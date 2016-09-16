4cf7ca7f72850cb64cd625a18a3e89e4d8c4e3ca
Dawid Komorowski <dawidkomorowski@outlook.com>
2016-09-16 21:34:55 +0200

    Resolved #2 Create basic Texture interface and implementation.

---
+++ Geisha/Framework/Rendering/Geisha.Framework.Rendering.Gdi/Texture.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geisha.Framework.Rendering.Gdi
+{
+    public class Texture : ITexture
+    {
+        private readonly Bitmap _bitmap;
+
+        public Texture(Stream stream)
+        {
+            _bitmap = new Bitmap(stream);
+        }
+    }
+}
