c62856f4b89b458cf6a9280632bd35d15ec5c0ef
Dawid Komorowski <dawidkomorowski@outlook.com>
2016-09-17 11:13:41 +0200

    Resolved #3 Extend IRenderer with method to create textures.

--- Geisha/Framework/Rendering/Geisha.Framework.Rendering.Gdi/Renderer.cs
+++ Geisha/Framework/Rendering/Geisha.Framework.Rendering.Gdi/Renderer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,9 @@ namespace Geisha.Framework.Rendering.Gdi
 {
     public class Renderer : IRenderer
     {
+        public ITexture CreateTexture(Stream stream)
+        {
+            return new Texture(stream);
+        }
     }
 }
--- Geisha/Framework/Rendering/Geisha.Framework.Rendering/IRenderer.cs
+++ Geisha/Framework/Rendering/Geisha.Framework.Rendering/IRenderer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,6 @@ namespace Geisha.Framework.Rendering
 {
     public interface IRenderer
     {
+        ITexture CreateTexture(Stream stream);
     }
 }
