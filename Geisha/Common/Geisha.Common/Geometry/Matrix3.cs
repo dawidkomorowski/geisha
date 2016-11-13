6961c3d5984b247643b32cfcb73208cdda5f079d
Dawid Komorowski <dawidkomorowski@outlook.com>
2016-11-13 21:45:12 +0100

    #17 Refactoring after performance tests.

--- Geisha/Common/Geisha.Common/Geometry/Matrix3.cs
+++ Geisha/Common/Geisha.Common/Geometry/Matrix3.cs
@@ -2,51 +2,71 @@
 
 namespace Geisha.Common.Geometry
 {
-    // TODO Default parameterless constructor does not work correctly because it cannot create internal array
     public struct Matrix3 : IMatrix<Matrix3>
     {
-        // TODO Refactor to be nine double readonly fields, but test the performance first
-        private readonly double[] _matrix;
+        public double M11 { get; }
+        public double M12 { get; }
+        public double M13 { get; }
+        public double M21 { get; }
+        public double M22 { get; }
+        public double M23 { get; }
+        public double M31 { get; }
+        public double M32 { get; }
+        public double M33 { get; }
 
         public static Matrix3 Zero => new Matrix3(0, 0, 0, 0, 0, 0, 0, 0, 0);
         public static Matrix3 Identity => new Matrix3(1, 0, 0, 0, 1, 0, 0, 0, 1);
 
-        public Matrix3 Opposite
-            =>
-            new Matrix3(-this[0, 0], -this[0, 1], -this[0, 2], -this[1, 0], -this[1, 1], -this[1, 2], -this[2, 0],
-                -this[2, 1], -this[2, 2]);
+        public Matrix3 Opposite => new Matrix3(-M11, -M12, -M13, -M21, -M22, -M23, -M31, -M32, -M33);
 
-        public double[] Array
-            =>
-            new[]
-            {
-                _matrix[0], _matrix[1], _matrix[2],
-                _matrix[3], _matrix[4], _matrix[5],
-                _matrix[6], _matrix[7], _matrix[8]
-            };
+        public double[] Array => new[] {M11, M12, M13, M21, M22, M23, M31, M32, M33};
 
         public double this[int row, int column]
         {
-            get { return _matrix[row*3 + column]; }
-            private set { _matrix[row*3 + column] = value; }
+            get
+            {
+                var index = row*3 + column;
+
+                switch (index)
+                {
+                    case 0:
+                        return M11;
+                    case 1:
+                        return M12;
+                    case 2:
+                        return M13;
+                    case 3:
+                        return M21;
+                    case 4:
+                        return M22;
+                    case 5:
+                        return M23;
+                    case 6:
+                        return M31;
+                    case 7:
+                        return M32;
+                    case 8:
+                        return M33;
+                    default:
+                        throw new IndexOutOfRangeException();
+                }
+            }
         }
 
         public Matrix3(double m11, double m12, double m13, double m21, double m22, double m23, double m31, double m32,
             double m33)
         {
-            _matrix = new double[9];
+            M11 = m11;
+            M12 = m12;
+            M13 = m13;
 
-            this[0, 0] = m11;
-            this[0, 1] = m12;
-            this[0, 2] = m13;
+            M21 = m21;
+            M22 = m22;
+            M23 = m23;
 
-            this[1, 0] = m21;
-            this[1, 1] = m22;
-            this[1, 2] = m23;
-
-            this[2, 0] = m31;
-            this[2, 1] = m32;
-            this[2, 2] = m33;
+            M31 = m31;
+            M32 = m32;
+            M33 = m33;
         }
 
         public Matrix3(double[] matrix)
@@ -56,101 +76,99 @@ namespace Geisha.Common.Geometry
                 throw new ArgumentException("Array must be the length of 9 elements.");
             }
 
-            _matrix = new double[9];
+            M11 = matrix[0];
+            M12 = matrix[1];
+            M13 = matrix[2];
 
-            this[0, 0] = matrix[0];
-            this[0, 1] = matrix[1];
-            this[0, 2] = matrix[2];
+            M21 = matrix[3];
+            M22 = matrix[4];
+            M23 = matrix[5];
 
-            this[1, 0] = matrix[3];
-            this[1, 1] = matrix[4];
-            this[1, 2] = matrix[5];
-
-            this[2, 0] = matrix[6];
-            this[2, 1] = matrix[7];
-            this[2, 2] = matrix[8];
+            M31 = matrix[6];
+            M32 = matrix[7];
+            M33 = matrix[8];
         }
 
         public Matrix3 Add(Matrix3 other)
         {
             return new Matrix3(
-                this[0, 0] + other[0, 0],
-                this[0, 1] + other[0, 1],
-                this[0, 2] + other[0, 2],
-                this[1, 0] + other[1, 0],
-                this[1, 1] + other[1, 1],
-                this[1, 2] + other[1, 2],
-                this[2, 0] + other[2, 0],
-                this[2, 1] + other[2, 1],
-                this[2, 2] + other[2, 2]
+                M11 + other.M11,
+                M12 + other.M12,
+                M13 + other.M13,
+                M21 + other.M21,
+                M22 + other.M22,
+                M23 + other.M23,
+                M31 + other.M31,
+                M32 + other.M32,
+                M33 + other.M33
             );
         }
 
         public Matrix3 Subtract(Matrix3 other)
         {
             return new Matrix3(
-                this[0, 0] - other[0, 0],
-                this[0, 1] - other[0, 1],
-                this[0, 2] - other[0, 2],
-                this[1, 0] - other[1, 0],
-                this[1, 1] - other[1, 1],
-                this[1, 2] - other[1, 2],
-                this[2, 0] - other[2, 0],
-                this[2, 1] - other[2, 1],
-                this[2, 2] - other[2, 2]
+                M11 - other.M11,
+                M12 - other.M12,
+                M13 - other.M13,
+                M21 - other.M21,
+                M22 - other.M22,
+                M23 - other.M23,
+                M31 - other.M31,
+                M32 - other.M32,
+                M33 - other.M33
             );
         }
 
         public Matrix3 Multiply(double scalar)
         {
             return new Matrix3(
-                this[0, 0]*scalar,
-                this[0, 1]*scalar,
-                this[0, 2]*scalar,
-                this[1, 0]*scalar,
-                this[1, 1]*scalar,
-                this[1, 2]*scalar,
-                this[2, 0]*scalar,
-                this[2, 1]*scalar,
-                this[2, 2]*scalar
+                M11*scalar,
+                M12*scalar,
+                M13*scalar,
+                M21*scalar,
+                M22*scalar,
+                M23*scalar,
+                M31*scalar,
+                M32*scalar,
+                M33*scalar
             );
         }
 
         public Matrix3 Multiply(Matrix3 other)
         {
             return new Matrix3(
-                this[0, 0]*other[0, 0] + this[0, 1]*other[1, 0] + this[0, 2]*other[2, 0],
-                this[0, 0]*other[0, 1] + this[0, 1]*other[1, 1] + this[0, 2]*other[2, 1],
-                this[0, 0]*other[0, 2] + this[0, 1]*other[1, 2] + this[0, 2]*other[2, 2],
-                this[1, 0]*other[0, 0] + this[1, 1]*other[1, 0] + this[1, 2]*other[2, 0],
-                this[1, 0]*other[0, 1] + this[1, 1]*other[1, 1] + this[1, 2]*other[2, 1],
-                this[1, 0]*other[0, 2] + this[1, 1]*other[1, 2] + this[1, 2]*other[2, 2],
-                this[2, 0]*other[0, 0] + this[2, 1]*other[1, 0] + this[2, 2]*other[2, 0],
-                this[2, 0]*other[0, 1] + this[2, 1]*other[1, 1] + this[2, 2]*other[2, 1],
-                this[2, 0]*other[0, 2] + this[2, 1]*other[1, 2] + this[2, 2]*other[2, 2]
+                M11*other.M11 + M12*other.M21 + M13*other.M31,
+                M11*other.M12 + M12*other.M22 + M13*other.M32,
+                M11*other.M13 + M12*other.M23 + M13*other.M33,
+                M21*other.M11 + M22*other.M21 + M23*other.M31,
+                M21*other.M12 + M22*other.M22 + M23*other.M32,
+                M21*other.M13 + M22*other.M23 + M23*other.M33,
+                M31*other.M11 + M32*other.M21 + M33*other.M31,
+                M31*other.M12 + M32*other.M22 + M33*other.M32,
+                M31*other.M13 + M32*other.M23 + M33*other.M33
             );
         }
 
         public Matrix3 Divide(double scalar)
         {
             return new Matrix3(
-                this[0, 0]/scalar,
-                this[0, 1]/scalar,
-                this[0, 2]/scalar,
-                this[1, 0]/scalar,
-                this[1, 1]/scalar,
-                this[1, 2]/scalar,
-                this[2, 0]/scalar,
-                this[2, 1]/scalar,
-                this[2, 2]/scalar
+                M11/scalar,
+                M12/scalar,
+                M13/scalar,
+                M21/scalar,
+                M22/scalar,
+                M23/scalar,
+                M31/scalar,
+                M32/scalar,
+                M33/scalar
             );
         }
 
         public bool Equals(Matrix3 other)
         {
-            return this[0, 0].Equals(other[0, 0]) && this[0, 1].Equals(other[0, 1]) && this[0, 2].Equals(other[0, 2]) &&
-                   this[1, 0].Equals(other[1, 0]) && this[1, 1].Equals(other[1, 1]) && this[1, 2].Equals(other[1, 2]) &&
-                   this[2, 0].Equals(other[2, 0]) && this[2, 1].Equals(other[2, 1]) && this[2, 2].Equals(other[2, 2]);
+            return M11.Equals(other.M11) && M12.Equals(other.M12) && M13.Equals(other.M13) &&
+                   M21.Equals(other.M21) && M22.Equals(other.M22) && M23.Equals(other.M23) &&
+                   M31.Equals(other.M31) && M32.Equals(other.M32) && M33.Equals(other.M33);
         }
 
         public override bool Equals(object obj)
@@ -163,15 +181,15 @@ namespace Geisha.Common.Geometry
         {
             unchecked
             {
-                var hashCode = this[0, 0].GetHashCode();
-                hashCode = (hashCode*397) ^ this[0, 1].GetHashCode();
-                hashCode = (hashCode*397) ^ this[0, 2].GetHashCode();
-                hashCode = (hashCode*397) ^ this[1, 0].GetHashCode();
-                hashCode = (hashCode*397) ^ this[1, 1].GetHashCode();
-                hashCode = (hashCode*397) ^ this[1, 2].GetHashCode();
-                hashCode = (hashCode*397) ^ this[2, 0].GetHashCode();
-                hashCode = (hashCode*397) ^ this[2, 1].GetHashCode();
-                hashCode = (hashCode*397) ^ this[2, 2].GetHashCode();
+                var hashCode = M11.GetHashCode();
+                hashCode = (hashCode*397) ^ M12.GetHashCode();
+                hashCode = (hashCode*397) ^ M13.GetHashCode();
+                hashCode = (hashCode*397) ^ M21.GetHashCode();
+                hashCode = (hashCode*397) ^ M22.GetHashCode();
+                hashCode = (hashCode*397) ^ M23.GetHashCode();
+                hashCode = (hashCode*397) ^ M31.GetHashCode();
+                hashCode = (hashCode*397) ^ M32.GetHashCode();
+                hashCode = (hashCode*397) ^ M33.GetHashCode();
                 return hashCode;
             }
         }
---
+++ Geisha/Geisha.ManualPerformanceTests/FixedMatrix3.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace Geisha.ManualPerformanceTests
+{
+    internal struct FixedMatrix3
+    {
+        private readonly double _m11;
+        private readonly double _m12;
+        private readonly double _m13;
+        private readonly double _m21;
+        private readonly double _m22;
+        private readonly double _m23;
+        private readonly double _m31;
+        private readonly double _m32;
+        private readonly double _m33;
+
+        public double this[int row, int column]
+        {
+            get
+            {
+                var index = row*3 + column;
+
+                switch (index)
+                {
+                    case 0:
+                        return _m11;
+                    case 1:
+                        return _m12;
+                    case 2:
+                        return _m13;
+                    case 3:
+                        return _m21;
+                    case 4:
+                        return _m22;
+                    case 5:
+                        return _m23;
+                    case 6:
+                        return _m31;
+                    case 7:
+                        return _m32;
+                    case 8:
+                        return _m33;
+                    default:
+                        throw new IndexOutOfRangeException();
+                }
+            }
+        }
+
+        public FixedMatrix3(double m11, double m12, double m13, double m21, double m22, double m23, double m31,
+            double m32,
+            double m33)
+        {
+            _m11 = m11;
+            _m12 = m12;
+            _m13 = m13;
+            _m21 = m21;
+            _m22 = m22;
+            _m23 = m23;
+            _m31 = m31;
+            _m32 = m32;
+            _m33 = m33;
+        }
+
+        public FixedMatrix3(double[] matrix)
+        {
+            if (matrix.Length != 9)
+            {
+                throw new ArgumentException("Array must be the length of 9 elements.");
+            }
+
+            _m11 = matrix[0];
+            _m12 = matrix[1];
+            _m13 = matrix[2];
+            _m21 = matrix[3];
+            _m22 = matrix[4];
+            _m23 = matrix[5];
+            _m31 = matrix[6];
+            _m32 = matrix[7];
+            _m33 = matrix[8];
+        }
+
+        public FixedMatrix3 MultiplyWithIndexer(FixedMatrix3 other)
+        {
+            return new FixedMatrix3(
+                this[0, 0]*other[0, 0] + this[0, 1]*other[1, 0] + this[0, 2]*other[2, 0],
+                this[0, 0]*other[0, 1] + this[0, 1]*other[1, 1] + this[0, 2]*other[2, 1],
+                this[0, 0]*other[0, 2] + this[0, 1]*other[1, 2] + this[0, 2]*other[2, 2],
+                this[1, 0]*other[0, 0] + this[1, 1]*other[1, 0] + this[1, 2]*other[2, 0],
+                this[1, 0]*other[0, 1] + this[1, 1]*other[1, 1] + this[1, 2]*other[2, 1],
+                this[1, 0]*other[0, 2] + this[1, 1]*other[1, 2] + this[1, 2]*other[2, 2],
+                this[2, 0]*other[0, 0] + this[2, 1]*other[1, 0] + this[2, 2]*other[2, 0],
+                this[2, 0]*other[0, 1] + this[2, 1]*other[1, 1] + this[2, 2]*other[2, 1],
+                this[2, 0]*other[0, 2] + this[2, 1]*other[1, 2] + this[2, 2]*other[2, 2]
+            );
+        }
+
+        public FixedMatrix3 Multiply(FixedMatrix3 other)
+        {
+            return new FixedMatrix3(
+                _m11*other._m11 + _m12*other._m21 + _m13*other._m31,
+                _m11*other._m12 + _m12*other._m22 + _m13*other._m32,
+                _m11*other._m13 + _m12*other._m23 + _m13*other._m33,
+                _m21*other._m11 + _m22*other._m21 + _m23*other._m31,
+                _m21*other._m12 + _m22*other._m22 + _m23*other._m32,
+                _m21*other._m13 + _m22*other._m23 + _m23*other._m33,
+                _m31*other._m11 + _m32*other._m21 + _m33*other._m31,
+                _m31*other._m12 + _m32*other._m22 + _m33*other._m32,
+                _m31*other._m13 + _m32*other._m23 + _m33*other._m33
+            );
+        }
+    }
+}
