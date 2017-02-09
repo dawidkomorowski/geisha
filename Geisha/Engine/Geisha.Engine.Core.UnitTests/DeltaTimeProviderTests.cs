5a3ec8c168a246d5048af751e4242070e456aa0c
Dawid Komorowski <dawidkomorowski@outlook.com>
2017-02-09 23:16:53 +0100

    Resolved #28 UnitTest failing on Jenkins.

--- Geisha/Engine/Geisha.Engine.Core.UnitTests/DeltaTimeProviderTests.cs
+++ Geisha/Engine/Geisha.Engine.Core.UnitTests/DeltaTimeProviderTests.cs
@@ -19,13 +19,12 @@ namespace Geisha.Engine.Core.UnitTests
             Assert.That(deltaTime, Is.Zero);
         }
 
-        [TestCase(0.001, 1)]
         [TestCase(0.01, 10)]
+        [TestCase(0.05, 50)]
         [TestCase(0.1, 100)]
         public void
             GetDeltaTime_ShouldReturnAtLeastDeltaTimeButLessThanTwiceDeltaTime_WhenCalledAfterDeltaTimeMillisecondsFromPreviousCall
-            (
-                double expected, int sleep)
+            (double expected, int sleep)
         {
             // Arrange
             var deltaTimeProvider = new DeltaTimeProvider();
@@ -37,7 +36,7 @@ namespace Geisha.Engine.Core.UnitTests
 
             // Assert
             Assert.That(deltaTime, Is.GreaterThan(expected));
-            Assert.That(deltaTime, Is.LessThan(2*expected));
+            Assert.That(deltaTime, Is.LessThan(2 * expected));
         }
     }
 }
