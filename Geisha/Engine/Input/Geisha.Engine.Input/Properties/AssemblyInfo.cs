58fd47f69dfa32637083bffcc235493bcbc67ca0
WorkServer Automation <workserver.automation@gmail.com>
2017-04-11 22:29:30 +0200

    Incrementing version to 0.0.6.41.

--- Geisha/Engine/Input/Geisha.Engine.Input/Properties/AssemblyInfo.cs
+++ Geisha/Engine/Input/Geisha.Engine.Input/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.5.32")]
-[assembly: AssemblyFileVersion("0.0.5.32")]
+[assembly: AssemblyVersion("0.0.6.41")]
+[assembly: AssemblyFileVersion("0.0.6.41")]
 
 // UnitTests
 [assembly: InternalsVisibleTo("Geisha.Engine.Input.UnitTests")]
