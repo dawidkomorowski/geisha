ff99b22de8e868f89ce2b9afa63ad21cc5398167
Dawid Komorowski <dawidkomorowski@outlook.com>
2017-10-25 00:41:41 +0200

    Added angle common helper methods.

---
+++ Geisha/Common/Geisha.Common.UnitTests/Math/AngleTests.cs
@@ -0,0 +1,27 @@
+﻿using Geisha.Common.Math;
+using NUnit.Framework;
+
+namespace Geisha.Common.UnitTests.Math
+{
+    [TestFixture]
+    public class AngleTests
+    {
+        [TestCase(0, 0)]
+        [TestCase(90, System.Math.PI / 2)]
+        [TestCase(180, System.Math.PI)]
+        [TestCase(360, 2 * System.Math.PI)]
+        [TestCase(720, 4 * System.Math.PI)]
+        [TestCase(37.375612, 0.65232748934790286)]
+        public void Deg2Rad_And_Rag2Deg(double degrees, double radians)
+        {
+            // Arrange
+            // Act
+            var actualRadians = Angle.Deg2Rad(degrees);
+            var actualDegrees = Angle.Rad2Deg(radians);
+
+            // Assert
+            Assert.That(actualRadians, Is.EqualTo(radians));
+            Assert.That(actualDegrees, Is.EqualTo(degrees));
+        }
+    }
+}
