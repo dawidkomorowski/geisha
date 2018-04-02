93fd6b270da1f1848aa9b4be1bceaf1aed855e90
Dawid Komorowski <dawidkomorowski@outlook.com>
2018-04-02 14:30:24 +0200

    #74 First idea how could InputMappingFile look like.

---
+++ Geisha/Engine/Input/Geisha.Engine.Input/Assets/InputMappingFile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Geisha.Framework.Input;
+
+namespace Geisha.Engine.Input.Assets
+{
+    // TODO Add docs to all Mapping classes and all Assets classes
+    public class InputMappingFile
+    {
+        public Dictionary<string, ActionMappingDefinition[]> ActionMappings { get; set; }
+        public Dictionary<string, AxisMappingDefinition[]> AxisMappings { get; set; }
+    }
+
+    public class ActionMappingDefinition
+    {
+        public Key Key { get; set; }
+    }
+
+    public class AxisMappingDefinition
+    {
+        public Key Key { get; set; }
+        public double Scale { get; set; }
+    }
+}
---
+++ Geisha/Engine/Input/Geisha.Engine.Input/Assets/InputMappingFileLoader.cs
@@ -0,0 +1,12 @@
+﻿using Geisha.Engine.Core.Assets;
+
+namespace Geisha.Engine.Input.Assets
+{
+    public class InputMappingFileLoader : AssetLoaderAdapter<InputMappingFile>
+    {
+        public override object Load(string filePath)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
