c31b9652eebd16b963aed73bc852dabd9f8392c8
Dawid Komorowski <dawidkomorowski@outlook.com>
2018-04-11 07:46:48 +0200

    #75 Implemented InputComponentDefinition and InputComponentDefinitionMapper.

---
+++ Geisha/Engine/Input/Geisha.Engine.Input/Components/Definition/InputComponentDefinition.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Geisha.Engine.Core.SceneModel.Definition;
+
+namespace Geisha.Engine.Input.Components.Definition
+{
+    public class InputComponentDefinition : IComponentDefinition
+    {
+        public Guid InputMappingAssetId { get; set; }
+    }
+}
---
+++ Geisha/Engine/Input/Geisha.Engine.Input/Components/Definition/InputComponentDefinitionMapper.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.Composition;
+using Geisha.Engine.Core.Assets;
+using Geisha.Engine.Core.SceneModel.Definition;
+using Geisha.Engine.Input.Mapping;
+
+namespace Geisha.Engine.Input.Components.Definition
+{
+    [Export(typeof(IComponentDefinitionMapper))]
+    internal class InputComponentDefinitionMapper : ComponentDefinitionMapperAdapter<InputComponent, InputComponentDefinition>
+    {
+        private readonly IAssetStore _assetStore;
+
+        [ImportingConstructor]
+        public InputComponentDefinitionMapper(IAssetStore assetStore)
+        {
+            _assetStore = assetStore;
+        }
+
+        protected override InputComponentDefinition ToDefinition(InputComponent component)
+        {
+            return new InputComponentDefinition
+            {
+                InputMappingAssetId = _assetStore.GetAssetId(component.InputMapping)
+            };
+        }
+
+        protected override InputComponent FromDefinition(InputComponentDefinition componentDefinition)
+        {
+            return new InputComponent
+            {
+                InputMapping = _assetStore.GetAsset<InputMapping>(componentDefinition.InputMappingAssetId)
+            };
+        }
+    }
+}
