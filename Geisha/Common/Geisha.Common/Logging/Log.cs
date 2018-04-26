2b85135cede100bc24d55b17e66539d24fde7989
Dawid Komorowski <dawidkomorowski@outlook.com>
2018-04-26 23:10:29 +0200

    #81 Added missing docs to ILog interface.

--- Geisha/Common/Geisha.Common/Logging/Log.cs
+++ Geisha/Common/Geisha.Common/Logging/Log.cs
@@ -2,13 +2,45 @@
 
 namespace Geisha.Common.Logging
 {
+    /// <summary>
+    ///     Provides logging interface.
+    /// </summary>
     public interface ILog
     {
+        /// <summary>
+        ///     Writes the diagnostic message at the Trace level.
+        /// </summary>
+        /// <param name="message">Log message.</param>
         void Trace(string message);
+
+        /// <summary>
+        ///     Writes the diagnostic message at the Debug level.
+        /// </summary>
+        /// <param name="message">Log message.</param>
         void Debug(string message);
+
+        /// <summary>
+        ///     Writes the diagnostic message at the Info level.
+        /// </summary>
+        /// <param name="message">Log message.</param>
         void Info(string message);
+
+        /// <summary>
+        ///     Writes the diagnostic message at the Warn level.
+        /// </summary>
+        /// <param name="message">Log message.</param>
         void Warn(string message);
+
+        /// <summary>
+        ///     Writes the diagnostic message at the Error level.
+        /// </summary>
+        /// <param name="message">Log message.</param>
         void Error(string message);
+
+        /// <summary>
+        ///     Writes the diagnostic message at the Fatal level.
+        /// </summary>
+        /// <param name="message">Log message.</param>
         void Fatal(string message);
     }
 
