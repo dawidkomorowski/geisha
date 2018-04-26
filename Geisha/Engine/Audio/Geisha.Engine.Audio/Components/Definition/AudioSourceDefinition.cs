46a14ceed6e58491d754430315af95ee84c51199
Dawid Komorowski <dawidkomorowski@outlook.com>
2018-04-26 21:56:05 +0200

    #81 Created AudioSourceDefinition class..

---
+++ Geisha/Engine/Audio/Geisha.Engine.Audio/Components/Definition/AudioSourceDefinition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Geisha.Engine.Core.SceneModel.Definition;
+using Geisha.Framework.Audio;
+
+namespace Geisha.Engine.Audio.Components.Definition
+{
+    /// <summary>
+    ///     Represents serializable <see cref="AudioSource" /> that is used in a scene file content.
+    /// </summary>
+    public sealed class AudioSourceDefinition : IComponentDefinition
+    {
+        /// <summary>
+        ///     Asset id of <see cref="ISound" /> asset.
+        /// </summary>
+        public Guid SoundAssetId { get; set; }
+
+        /// <summary>
+        ///     Defines <see cref="AudioSource.IsPlaying" /> property of <see cref="AudioSource" />.
+        /// </summary>
+        public bool IsPlaying { get; set; }
+    }
+}
---
+++ Geisha/Engine/Audio/Geisha.Engine.Audio/Components/Definition/AudioSourceDefinitionMapper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Geisha.Engine.Core.SceneModel.Definition;
+
+namespace Geisha.Engine.Audio.Components.Definition
+{
+    internal sealed class AudioSourceDefinitionMapper : ComponentDefinitionMapperAdapter<AudioSource, AudioSourceDefinition>
+    {
+        protected override AudioSourceDefinition ToDefinition(AudioSource component)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override AudioSource FromDefinition(AudioSourceDefinition componentDefinition)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
