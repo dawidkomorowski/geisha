906b3b538acb4174e89a69eef8bca8cf8c8cbde5
Dawid Komorowski <dawidkomorowski@outlook.com>
2018-04-26 22:29:50 +0200

    #81 Implemented AudioSourceDefinitionMapper..

---
+++ Geisha/Engine/Audio/Geisha.Engine.Audio.UnitTests/Components/Definition/AudioSourceDefinitionMapperTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Geisha.Engine.Audio.Components;
+using Geisha.Engine.Audio.Components.Definition;
+using Geisha.Engine.Core.Assets;
+using Geisha.Framework.Audio;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Geisha.Engine.Audio.UnitTests.Components.Definition
+{
+    [TestFixture]
+    public class AudioSourceDefinitionMapperTests
+    {
+        private IAssetStore _assetStore;
+        private AudioSourceDefinitionMapper _mapper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _assetStore = Substitute.For<IAssetStore>();
+            _mapper = new AudioSourceDefinitionMapper(_assetStore);
+        }
+
+        [Test]
+        public void ToDefinition()
+        {
+            // Arrange
+            var sound = Substitute.For<ISound>();
+            var soundAssetId = Guid.NewGuid();
+
+            var audioSource = new AudioSource
+            {
+                Sound = sound,
+                IsPlaying = true
+            };
+
+            _assetStore.GetAssetId(sound).Returns(soundAssetId);
+
+            // Act
+            var actual = (AudioSourceDefinition) _mapper.ToDefinition(audioSource);
+
+            // Assert
+            Assert.That(actual.SoundAssetId, Is.EqualTo(soundAssetId));
+            Assert.That(actual.IsPlaying, Is.True);
+        }
+
+        [Test]
+        public void FromDefinition()
+        {
+            // Arrange
+            var sound = Substitute.For<ISound>();
+            var soundAssetId = Guid.NewGuid();
+
+            var audioSourceDefinition = new AudioSourceDefinition
+            {
+                SoundAssetId = soundAssetId,
+                IsPlaying = true
+            };
+
+            _assetStore.GetAsset<ISound>(soundAssetId).Returns(sound);
+
+            // Act
+            var actual = (AudioSource) _mapper.FromDefinition(audioSourceDefinition);
+
+            // Assert
+            Assert.That(actual.Sound, Is.EqualTo(sound));
+            Assert.That(actual.IsPlaying, Is.True);
+        }
+    }
+}
--- Geisha/Engine/Audio/Geisha.Engine.Audio/Components/Definition/AudioSourceDefinitionMapper.cs
+++ Geisha/Engine/Audio/Geisha.Engine.Audio/Components/Definition/AudioSourceDefinitionMapper.cs
@@ -1,18 +1,37 @@
-﻿using System;
+﻿using System.ComponentModel.Composition;
+using Geisha.Engine.Core.Assets;
 using Geisha.Engine.Core.SceneModel.Definition;
+using Geisha.Framework.Audio;
 
 namespace Geisha.Engine.Audio.Components.Definition
 {
+    [Export(typeof(IComponentDefinitionMapper))]
     internal sealed class AudioSourceDefinitionMapper : ComponentDefinitionMapperAdapter<AudioSource, AudioSourceDefinition>
     {
+        private readonly IAssetStore _assetStore;
+
+        [ImportingConstructor]
+        public AudioSourceDefinitionMapper(IAssetStore assetStore)
+        {
+            _assetStore = assetStore;
+        }
+
         protected override AudioSourceDefinition ToDefinition(AudioSource component)
         {
-            throw new NotImplementedException();
+            return new AudioSourceDefinition
+            {
+                SoundAssetId = _assetStore.GetAssetId(component.Sound),
+                IsPlaying = component.IsPlaying
+            };
         }
 
         protected override AudioSource FromDefinition(AudioSourceDefinition componentDefinition)
         {
-            throw new NotImplementedException();
+            return new AudioSource
+            {
+                Sound = _assetStore.GetAsset<ISound>(componentDefinition.SoundAssetId),
+                IsPlaying = componentDefinition.IsPlaying
+            };
         }
     }
 }
