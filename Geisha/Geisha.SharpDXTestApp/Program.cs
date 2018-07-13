9fca107da5a9f7d664d73f80f37b1b4e09a158fd
Dawid Komorowski <dawidkomorowski@outlook.com>
2018-07-13 21:56:43 +0200

    #91 Conversion from ARGB to BGRA reimplemented using System.Drawing.Color.

--- Geisha/Geisha.SharpDXTestApp/Program.cs
+++ Geisha/Geisha.SharpDXTestApp/Program.cs
@@ -96,7 +96,8 @@ namespace Geisha.SharpDXTestApp
                                                     d2D1RenderTarget.FillRectangle(new RawRectangleF(x, y, x + width, y + height), brush);
 
                                                     //d2D1RenderTarget.Transform = new RawMatrix3x2(1.0f, 0.0f, 0.0f, 1.0f, 400.0f, 100.0f);
-                                                    d2D1RenderTarget.Transform = new RawMatrix3x2((float)Math.Sin(totalTime.TotalSeconds), 0.0f, 0.0f, 1.0f, 400.0f, 100.0f);
+                                                    d2D1RenderTarget.Transform = new RawMatrix3x2((float) Math.Sin(totalTime.TotalSeconds), 0.0f, 0.0f, 1.0f,
+                                                        400.0f, 100.0f);
                                                     d2D1RenderTarget.DrawBitmap(d2D1Bitmap, 1.0f, BitmapInterpolationMode.Linear);
 
                                                     d2D1RenderTarget.EndDraw();
@@ -133,39 +134,20 @@ namespace Geisha.SharpDXTestApp
 
             using (var gdiBitmap = new System.Drawing.Bitmap(filePath))
             {
-                var color = gdiBitmap.GetPixel(300, 200);
-
                 var gdiBitmapData = gdiBitmap.LockBits(new Rectangle(0, 0, gdiBitmap.Width, gdiBitmap.Height), ImageLockMode.ReadOnly,
                     System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
-
-                var alpha = (byte)((Color.FromArgb(byte.MaxValue, byte.MinValue, byte.MinValue, byte.MinValue).ToArgb() & 0b11111111_00000000_00000000_00000000) >> 24);
-                var red = (byte)((Color.Red.ToArgb() & 0b00000000_11111111_00000000_00000000) >> 16);
-                var green = (byte)((Color.Green.ToArgb() & 0b00000000_00000000_11111111_00000000) >> 8);
-                var blue = (byte)((Color.Blue.ToArgb() & 0b00000000_00000000_00000000_11111111) >> 0);
-
 
                 var stride = Math.Abs(gdiBitmapData.Stride);
                 using (var convertedBitmapDataStream = new DataStream(gdiBitmap.Height * stride, true, true))
                 {
-                    var byteAlpha = Marshal.ReadByte(gdiBitmapData.Scan0, 200 * stride + 300 * sizeof(int));
-                    var byteRed = Marshal.ReadByte(gdiBitmapData.Scan0, 200 * stride + 300 * sizeof(int) + 1);
-                    var byteGreen = Marshal.ReadByte(gdiBitmapData.Scan0, 200 * stride + 300 * sizeof(int) + 2);
-                    var byteBlue = Marshal.ReadByte(gdiBitmapData.Scan0, 200 * stride + 300 * sizeof(int) + 3);
-
-                    var pixelInt = Marshal.ReadInt32(gdiBitmapData.Scan0, 200 * stride + 300 * sizeof(int));
-                    var pixelAlpha = (byte) ((pixelInt & 0b11111111_00000000_00000000_00000000) >> 24);
-                    var pixelRed = (byte) ((pixelInt & 0b00000000_11111111_00000000_00000000) >> 16);
-                    var pixelGreen = (byte) ((pixelInt & 0b00000000_00000000_11111111_00000000) >> 8);
-                    var pixelBlue = (byte) ((pixelInt & 0b00000000_00000000_00000000_11111111) >> 0);
-
-                    for (var i = 0; i < gdiBitmap.Height*stride; i+=4)
+                    for (var i = 0; i < gdiBitmap.Height * stride; i += sizeof(int))
                     {
-                        var pixel = Marshal.ReadInt32(gdiBitmapData.Scan0, i);
-                        convertedBitmapDataStream.WriteByte((byte) ((pixel & 0b00000000_00000000_00000000_11111111) >> 0));
-                        convertedBitmapDataStream.WriteByte((byte) ((pixel & 0b00000000_00000000_11111111_00000000) >> 8));
-                        convertedBitmapDataStream.WriteByte((byte) ((pixel & 0b00000000_11111111_00000000_00000000) >> 16));
-                        convertedBitmapDataStream.WriteByte((byte) ((pixel & 0b11111111_00000000_00000000_00000000) >> 24));
-                        //convertedBitmapDataStream.WriteByte(Marshal.ReadByte(gdiBitmapData.Scan0, i));
+                        var pixelValue = Marshal.ReadInt32(gdiBitmapData.Scan0, i);
+                        var pixelColor = Color.FromArgb(pixelValue);
+                        convertedBitmapDataStream.WriteByte(pixelColor.B);
+                        convertedBitmapDataStream.WriteByte(pixelColor.G);
+                        convertedBitmapDataStream.WriteByte(pixelColor.R);
+                        convertedBitmapDataStream.WriteByte(pixelColor.A);
                     }
 
                     convertedBitmapDataStream.Position = 0;
