f7bef9dc1e45e8329ccb1ef76761908e554a1f15
Dawid Komorowski <dawidkomorowski@outlook.com>
2018-08-24 14:08:58 +0200

    #93 Inroduced ColorExtensions to have common code for converting from Geisha Color structure to DirectX RawColor4.

---
+++ Geisha/Framework/Rendering/Geisha.Framework.Rendering.DirectX/ColorExtensions.cs
@@ -0,0 +1,12 @@
+﻿using SharpDX.Mathematics.Interop;
+
+namespace Geisha.Framework.Rendering.DirectX
+{
+    internal static class ColorExtensions
+    {
+        public static RawColor4 ToRawColor4(this Color color)
+        {
+            return new RawColor4((float) color.ScR, (float) color.ScG, (float) color.ScB, (float) color.ScA);
+        }
+    }
+}
