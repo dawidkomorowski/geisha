adb6ac63dd5825876e4166aa0a4e3847ec49dace
Dawid Komorowski <dawidkomorowski@outlook.com>
2018-09-06 19:15:12 +0200

    #103 Created Geisha.Framework.Rendering.DirectX.IntegrationTests project exporting test implementation of IWindow to fix failing SceneLoader integration test.

---
+++ Geisha/Framework/Rendering/Geisha.Framework.Rendering.DirectX.IntegrationTests/IntegrationTestsWindow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Drawing;
+using SharpDX.Windows;
+
+namespace Geisha.Framework.Rendering.DirectX.IntegrationTests
+{
+    [Export(typeof(IWindow))]
+    public class IntegrationTestsWindow : IWindow
+    {
+        private readonly RenderForm _renderForm;
+        public int ClientAreaWidth => _renderForm.ClientSize.Width;
+        public int ClientAreaHeight => _renderForm.ClientSize.Height;
+        public IntPtr Handle => _renderForm.Handle;
+
+        public IntegrationTestsWindow()
+        {
+            _renderForm = new RenderForm($"{typeof(IntegrationTestsWindow).FullName}") {ClientSize = new Size(1280, 720)};
+        }
+    }
+}
