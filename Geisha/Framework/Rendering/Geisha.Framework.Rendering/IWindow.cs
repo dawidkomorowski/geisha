8045e294caf617e4e0d1d8d587502a12b56091bb
Dawid Komorowski <dawidkomorowski@outlook.com>
2018-09-08 23:03:00 +0200

    #102 Added documentation to IWindow.

--- Geisha/Framework/Rendering/Geisha.Framework.Rendering/IWindow.cs
+++ Geisha/Framework/Rendering/Geisha.Framework.Rendering/IWindow.cs
@@ -2,10 +2,24 @@
 
 namespace Geisha.Framework.Rendering
 {
+    /// <summary>
+    ///     Provides access to application window.
+    /// </summary>
     public interface IWindow
     {
+        /// <summary>
+        ///     Width of client area in the window (excluding window frame).
+        /// </summary>
         int ClientAreaWidth { get; }
+
+        /// <summary>
+        ///     Height of client area in the window (excluding window frame).
+        /// </summary>
         int ClientAreaHeight { get; }
+
+        /// <summary>
+        ///     Native handle to the window.
+        /// </summary>
         IntPtr Handle { get; }
     }
 }
