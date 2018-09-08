2eea61539bc384d0cff518004bc0a525ed667331
Dawid Komorowski <dawidkomorowski@outlook.com>
2018-09-08 22:03:39 +0200

    #102 Added documentation to ITexture.

--- Geisha/Framework/Rendering/Geisha.Framework.Rendering/ITexture.cs
+++ Geisha/Framework/Rendering/Geisha.Framework.Rendering/ITexture.cs
@@ -3,8 +3,14 @@ using Geisha.Common.Math;
 
 namespace Geisha.Framework.Rendering
 {
+    /// <summary>
+    ///     Represents texture that is pixel data and attributes of 2D image.
+    /// </summary>
     public interface ITexture : IDisposable
     {
+        /// <summary>
+        ///     Dimension of texture in pixels.
+        /// </summary>
         Vector2 Dimension { get; }
     }
 }
