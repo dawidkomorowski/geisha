76d9b38b99181c78b1a229f374a479db26cff4f1
Dawid Komorowski <dawidkomorowski@outlook.com>
2018-11-02 10:44:06 +0100

    #117 Added XML docs for ExtensionsHostContainer.

--- Geisha/Common/Geisha.Common/Extensibility/ExtensionsHostContainer.cs
+++ Geisha/Common/Geisha.Common/Extensibility/ExtensionsHostContainer.cs
@@ -4,19 +4,34 @@ using Geisha.Common.Logging;
 
 namespace Geisha.Common.Extensibility
 {
+    /// <summary>
+    ///     Implements host for application composed of components, services and extensions using Autofac dependency injection
+    ///     container and MEF extensions discovery.
+    /// </summary>
+    /// <typeparam name="TCompositionRoot">Type of composition root object, typically an entry point to hosted application.</typeparam>
     public sealed class ExtensionsHostContainer<TCompositionRoot> : IDisposable
     {
         private static readonly ILog Log = LogFactory.Create(typeof(ExtensionsHostContainer<TCompositionRoot>));
-        private readonly ExtensionsManager _extensionsManager;
         private readonly IContainer _container;
+        private readonly ExtensionsManager _extensionsManager;
         private readonly ILifetimeScope _lifetimeScope;
 
-        public TCompositionRoot CompositionRoot { get; }
-
+        /// <summary>
+        ///     Creates new instance of <see cref="ExtensionsHostContainer{TCompositionRoot}" /> with no services provided by the
+        ///     host.
+        /// </summary>
         public ExtensionsHostContainer() : this(new EmptyHostServices())
         {
         }
 
+        /// <summary>
+        ///     Creates new instance of <see cref="ExtensionsHostContainer{TCompositionRoot}" /> with specified services provided
+        ///     by the host.
+        /// </summary>
+        /// <param name="hostServices">
+        ///     Services provided by the host. Typically it includes services specific to particular host
+        ///     executable.
+        /// </param>
         public ExtensionsHostContainer(IHostServices hostServices)
         {
             _extensionsManager = new ExtensionsManager();
@@ -39,6 +54,15 @@ namespace Geisha.Common.Extensibility
             Log.Info("Dependency graph composed successfully.");
         }
 
+        /// <summary>
+        ///     Composition root object instance. It is automatically created after
+        ///     <see cref="ExtensionsHostContainer{TCompositionRoot}" /> is constructed.
+        /// </summary>
+        public TCompositionRoot CompositionRoot { get; }
+
+        /// <summary>
+        ///     Disposes Autofac dependency injection container and MEF extensions containers.
+        /// </summary>
         public void Dispose()
         {
             Log.Info("Disposing Autofac container.");
