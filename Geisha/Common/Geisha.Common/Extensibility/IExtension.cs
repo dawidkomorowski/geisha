89491db72a17426fd14cb9627e0b49548911ec9c
Dawid Komorowski <dawidkomorowski@outlook.com>
2018-11-13 23:49:05 +0100

    #117 Added XML docs for IExtension.

--- Geisha/Common/Geisha.Common/Extensibility/IExtension.cs
+++ Geisha/Common/Geisha.Common/Extensibility/IExtension.cs
@@ -4,20 +4,53 @@ using Autofac;
 
 namespace Geisha.Common.Extensibility
 {
+    /// <summary>
+    ///     Specifies interface of extension to Geisha Engine. Implement this interface to provide custom functionality within
+    ///     an extension.
+    /// </summary>
     [InheritedExport]
     public interface IExtension
     {
+        /// <summary>
+        ///     Name of extension.
+        /// </summary>
         string Name { get; }
+
+        /// <summary>
+        ///     Description of extension. Provides brief information about functionality an extension implements.
+        /// </summary>
         string Description { get; }
+
+        /// <summary>
+        ///     Category of extension. Allows identifying and grouping similar extensions with common functionality.
+        /// </summary>
         string Category { get; }
+
+        /// <summary>
+        ///     Author of extension.
+        /// </summary>
         string Author { get; }
+
+        /// <summary>
+        ///     Version of extension.
+        /// </summary>
         Version Version { get; }
 
+        /// <summary>
+        ///     Registers extension components in Autofac container.
+        /// </summary>
+        /// <remarks>\Implement this method to register components and services an extension provides in Autofac container.</remarks>
+        /// <param name="containerBuilder"></param>
         void Register(ContainerBuilder containerBuilder);
     }
 
     public static class ExtensionExtensions
     {
+        /// <summary>
+        ///     Returns human-readable string representation of <see cref="IExtension" /> object.
+        /// </summary>
+        /// <param name="extension">An extension object to be used.</param>
+        /// <returns>Human-readable string representation of <see cref="IExtension" /> object.</returns>
         public static string Format(this IExtension extension)
         {
             return $@"
