73e60f5af4eae446b8334fb12bc248d9649fbba2
Dawid Komorowski <dawidkomorowski@outlook.com>
2018-12-21 23:49:00 +0100

    #104 Implemented basic CircularBuffer and tests for it. It still lacks some functionality.

---
+++ Geisha/Common/Geisha.Common/CircularBuffer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Geisha.Common
+{
+    public sealed class CircularBuffer<T> : IEnumerable<T>
+    {
+        private readonly T[] _data;
+        private int _head = -1;
+
+        public CircularBuffer(int size)
+        {
+            if (size <= 0) throw new ArgumentOutOfRangeException(nameof(size), size, $"{nameof(size)} parameter must be positive.");
+            Size = size;
+            _data = new T[size];
+        }
+
+        public int Size { get; }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= Size) throw new IndexOutOfRangeException($"Index must be in range [0,{Size - 1}].");
+                return _data[(_head - (Size - 1 - index) + Size) % Size];
+            }
+        }
+
+        public void Add(T item)
+        {
+            _data[(++_head) % Size] = item;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
