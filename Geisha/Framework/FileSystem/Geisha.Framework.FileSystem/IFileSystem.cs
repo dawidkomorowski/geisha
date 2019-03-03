f25e774f64511c58a8b00f34c1b0255f3e4e0882
Dawid Komorowski <dawidkomorowski@outlook.com>
2019-03-03 20:00:15 +0100

    #127 Added xml docs to IFileSystem, IDirectory and IFile.

--- Geisha/Framework/FileSystem/Geisha.Framework.FileSystem/IFileSystem.cs
+++ Geisha/Framework/FileSystem/Geisha.Framework.FileSystem/IFileSystem.cs
@@ -1,10 +1,37 @@
 ﻿namespace Geisha.Framework.FileSystem
 {
-    // TODO Add xml documentation.
+    /// <summary>
+    ///     Provides access to file system.
+    /// </summary>
     public interface IFileSystem
     {
+        /// <summary>
+        ///     Creates or overwrites a file in the specified path.
+        /// </summary>
+        /// <param name="filePath">The path and name of the file to create.</param>
+        /// <returns>
+        ///     An <see cref="IFile" /> instance that represents the file specified in <paramref name="filePath" />.
+        /// </returns>
         IFile CreateFile(string filePath);
+
+        /// <summary>
+        ///     Gets an <see cref="IFile" /> instance that represents the file specified in <paramref name="filePath" />.
+        /// </summary>
+        /// <param name="filePath">The path and name of the existing file.</param>
+        /// <returns>
+        ///     An <see cref="IFile" /> instance that represents the file specified in <paramref name="filePath" />.
+        /// </returns>
         IFile GetFile(string filePath);
+
+        /// <summary>
+        ///     Gets an <see cref="IDirectory" /> instance that represents the directory specified in
+        ///     <paramref name="directoryPath" />.
+        /// </summary>
+        /// <param name="directoryPath">The path to existing directory.</param>
+        /// <returns>
+        ///     An <see cref="IDirectory" /> instance that represents the directory specified in
+        ///     <paramref name="directoryPath" />.
+        /// </returns>
         IDirectory GetDirectory(string directoryPath);
     }
 }
