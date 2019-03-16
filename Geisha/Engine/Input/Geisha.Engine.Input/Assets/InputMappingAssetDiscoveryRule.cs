fc55e39dd96b7f56e30328da36870ca49047c530
Dawid Komorowski <dawidkomorowski@outlook.com>
2019-03-16 16:18:23 +0100

    #127 Implemented asset discovery rule for Input Mapping.

---
+++ Geisha/Engine/Input/Geisha.Engine.Input/Assets/InputMappingAssetDiscoveryRule.cs
@@ -0,0 +1,36 @@
+﻿using Geisha.Common;
+using Geisha.Common.Serialization;
+using Geisha.Engine.Core.Assets;
+using Geisha.Engine.Input.Mapping;
+using Geisha.Framework.FileSystem;
+
+namespace Geisha.Engine.Input.Assets
+{
+    public class InputMappingAssetDiscoveryRule : IAssetDiscoveryRule
+    {
+        private const string InputMappingFileExtension = ".input";
+        private readonly IJsonSerializer _jsonSerializer;
+
+        public InputMappingAssetDiscoveryRule(IJsonSerializer jsonSerializer)
+        {
+            _jsonSerializer = jsonSerializer;
+        }
+
+        public ISingleOrEmpty<AssetInfo> Discover(IFile file)
+        {
+            if (file.Extension == InputMappingFileExtension)
+            {
+                var inputMappingFileContent = _jsonSerializer.Deserialize<InputMappingFileContent>(file.ReadAllText());
+                var assetInfo = new AssetInfo(
+                    new AssetId(inputMappingFileContent.AssetId),
+                    typeof(InputMapping),
+                    file.Path);
+                return SingleOrEmpty.Single(assetInfo);
+            }
+            else
+            {
+                return SingleOrEmpty.Empty<AssetInfo>();
+            }
+        }
+    }
+}
