b48f056177b6256d86257925db1c3c384dd90eaa
Dawid Komorowski <dawidkomorowski@outlook.com>
2019-04-22 17:42:24 +0200

    Incrementing version to 0.3.0.

--- Geisha/Framework/FileSystem/Geisha.Framework.FileSystem.IntegrationTests/Properties/AssemblyInfo.cs
+++ Geisha/Framework/FileSystem/Geisha.Framework.FileSystem.IntegrationTests/Properties/AssemblyInfo.cs
@@ -1,16 +1,16 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Geisha.Engine.Input")]
+[assembly: AssemblyTitle("Geisha.Framework.FileSystem.IntegrationTests.Properties")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Geisha.Engine.Input")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("Geisha.Framework.FileSystem.IntegrationTests.Properties")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8385ce2a-33ae-4214-85ea-e2156a2d87d7")]
+[assembly: Guid("3b49b312-c002-444a-a02e-2c6baa993a92")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,12 +29,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.3.0")]
 [assembly: AssemblyFileVersion("0.3.0")]
 [assembly: AssemblyInformationalVersion("0.3.0")]
-
-// UnitTests
-[assembly: InternalsVisibleTo("Geisha.Engine.Input.UnitTests")]
