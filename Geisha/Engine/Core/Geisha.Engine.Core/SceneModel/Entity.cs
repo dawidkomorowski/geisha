ec69fe8103297f9fec0c4b50ae4d712d27ee1a1c
Dawid Komorowski <dawidkomorowski@outlook.com>
2019-05-12 18:29:58 +0200

    #122 Made Entity class sealed and added missing XML docs to it.

--- Geisha/Engine/Core/Geisha.Engine.Core/SceneModel/Entity.cs
+++ Geisha/Engine/Core/Geisha.Engine.Core/SceneModel/Entity.cs
@@ -3,16 +3,27 @@ using System.Linq;
 
 namespace Geisha.Engine.Core.SceneModel
 {
-    // TODO Add documentation.
-    public class Entity
+    /// <summary>
+    ///     Implements entity part of entity-component-system architecture. Entity represents any object in the game scene and
+    ///     it's behavior and interactions are defined by attached components processed by systems.
+    /// </summary>
+    public sealed class Entity
     {
         private readonly List<Entity> _children = new List<Entity>();
         private readonly List<IComponent> _components = new List<IComponent>();
         private Entity _parent;
         private Scene _scene;
 
+        /// <summary>
+        ///     Name of entity. Can be used to uniquely identify entities by <c>string</c> names or include some debugging
+        ///     information.
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        ///     Entity that is parent of this entity (this entity is child of parent entity). Returns <c>null</c> if entity is root
+        ///     of entity tree.
+        /// </summary>
         public Entity Parent
         {
             get => _parent;
@@ -26,6 +37,9 @@ namespace Geisha.Engine.Core.SceneModel
             }
         }
 
+        /// <summary>
+        ///     Scene that this entity is part of. Returns <c>null</c> if entity is not part of any scene.
+        /// </summary>
         public Scene Scene
         {
             get => _scene;
@@ -39,9 +53,24 @@ namespace Geisha.Engine.Core.SceneModel
             }
         }
 
+        /// <summary>
+        ///     Defines whether this entity is root of entity tree. True if entity is root of entity tree; false otherwise.
+        /// </summary>
         public bool IsRoot => Parent == null;
+
+        /// <summary>
+        ///     Root of entity tree that this entity is part of. Returns this entity if it is root of entity tree.
+        /// </summary>
         public Entity Root => IsRoot ? this : Parent.Root;
+
+        /// <summary>
+        ///     Entities that are children of this entity.
+        /// </summary>
         public IReadOnlyList<Entity> Children => _children.AsReadOnly();
+
+        /// <summary>
+        ///     Components attached to this entity.
+        /// </summary>
         public IReadOnlyList<IComponent> Components => _components.AsReadOnly();
 
         /// <summary>
@@ -49,26 +78,49 @@ namespace Geisha.Engine.Core.SceneModel
         /// </summary>
         public bool IsScheduledForDestruction { get; private set; }
 
+        /// <summary>
+        ///     Returns component of specified type. Throws exception if there are multiple or none requested components.
+        /// </summary>
+        /// <typeparam name="TComponent">Type of component to retrieve.</typeparam>
+        /// <returns>Component of specified type.</returns>
         public TComponent GetComponent<TComponent>() where TComponent : IComponent
         {
             return _components.OfType<TComponent>().Single();
         }
 
+        /// <summary>
+        ///     Returns components of specified type.
+        /// </summary>
+        /// <typeparam name="TComponent">Type of components to retrieve.</typeparam>
+        /// <returns>Components of specified type.</returns>
         public IEnumerable<TComponent> GetComponents<TComponent>() where TComponent : IComponent
         {
             return _components.OfType<TComponent>();
         }
 
+        /// <summary>
+        ///     Checks if component of specified type is attached to entity.
+        /// </summary>
+        /// <typeparam name="TComponent">Type of component to check.</typeparam>
+        /// <returns>True if component of specified type is attached to entity; false otherwise.</returns>
         public bool HasComponent<TComponent>() where TComponent : IComponent
         {
             return _components.OfType<TComponent>().Any();
         }
 
+        /// <summary>
+        ///     Attaches specified component instance to entity.
+        /// </summary>
+        /// <param name="component">Component instance to be attached.</param>
         public void AddComponent(IComponent component)
         {
             _components.Add(component);
         }
 
+        /// <summary>
+        ///     Removes specified component instance from entity.
+        /// </summary>
+        /// <param name="component">Component instance to be removed from entity.</param>
         public void RemoveComponent(IComponent component)
         {
             _components.Remove(component);
@@ -84,11 +136,20 @@ namespace Geisha.Engine.Core.SceneModel
             entity.Parent = this;
         }
 
+        /// <summary>
+        ///     Returns all children of entity including children of children effectively iterating through whole subtree.
+        /// </summary>
+        /// <returns>Entities that are all children of this entity including children of children.</returns>
         public IEnumerable<Entity> GetChildrenRecursively()
         {
             return Children.SelectMany(c => c.GetChildrenRecursively()).Concat(Children);
         }
 
+        /// <summary>
+        ///     Returns this entity and all its children including children of children effectively iterating through whole
+        ///     subtree.
+        /// </summary>
+        /// <returns>Entities collection that contains this entity and all its children including children of children.</returns>
         public IEnumerable<Entity> GetChildrenRecursivelyIncludingRoot()
         {
             return Children.SelectMany(c => c.GetChildrenRecursivelyIncludingRoot()).Concat(new[] {this});
