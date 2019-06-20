aa5bdff327f79e207c22a95dc1121669634ac5e6
Dawid Komorowski <dawidkomorowski@outlook.com>
2019-06-20 11:58:51 +0200

    #126 Implemented EllipseRendererComponent but it is not yet supported by rendering system.

---
+++ src/Geisha.Engine/Rendering/Components/EllipseRendererComponent.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Geisha.Framework.Rendering;
+
+namespace Geisha.Engine.Rendering.Components
+{
+    /// <summary>
+    ///     Ellipse renderer component enables entity with ellipse rendering functionality.
+    /// </summary>
+    public sealed class EllipseRendererComponent : Renderer2DComponent
+    {
+        /// <summary>
+        ///     Radius of the circle.
+        /// </summary>
+        /// <remarks>
+        ///     If assigned changes ellipse to circle by modifying <see cref="RadiusX" /> and <see cref="RadiusY" />. When
+        ///     accessed returns circle radius if ellipse is circle; otherwise throws <see cref="EllipseIsNotCircleException" />.
+        /// </remarks>
+        /// <exception cref="EllipseIsNotCircleException">Thrown when ellipse does not represent a circle.</exception>
+        public double Radius
+        {
+            get => RadiusX == RadiusY ? RadiusX : throw new EllipseIsNotCircleException(RadiusX, RadiusY);
+            set
+            {
+                RadiusX = value;
+                RadiusY = value;
+            }
+        }
+
+        /// <summary>
+        ///     X radius of the ellipse.
+        /// </summary>
+        public double RadiusX { get; set; }
+
+        /// <summary>
+        ///     Y radius of the ellipse.
+        /// </summary>
+        public double RadiusY { get; set; }
+
+        /// <summary>
+        ///     Color of the ellipse.
+        /// </summary>
+        public Color Color { get; set; }
+
+        /// <summary>
+        ///     Specifies whether to fill interior of ellipse or draw only border. If true interior is filled with color.
+        /// </summary>
+        public bool FillInterior { get; set; }
+    }
+
+    /// <summary>
+    ///     The exception that is thrown when accessing <see cref="EllipseRendererComponent.Radius" /> when ellipse does not
+    ///     represent a circle.
+    /// </summary>
+    public sealed class EllipseIsNotCircleException : Exception
+    {
+        public EllipseIsNotCircleException(double radiusX, double radiusY)
+            : base($"Ellipse is not a circle. Circle radius cannot be calculated. RadiusX = {radiusX}, RadiusY = {radiusY}.")
+        {
+            RadiusX = radiusX;
+            RadiusY = radiusY;
+        }
+
+        /// <summary>
+        ///     X radius of the ellipse.
+        /// </summary>
+        public double RadiusX { get; }
+
+        /// <summary>
+        ///     Y radius of the ellipse.
+        /// </summary>
+        public double RadiusY { get; }
+    }
+}
---
+++ test/Geisha.Engine.UnitTests/Rendering/Components/EllipseRendererComponentTests.cs
@@ -0,0 +1,65 @@
+﻿using Geisha.Engine.Rendering.Components;
+using NUnit.Framework;
+
+namespace Geisha.Engine.UnitTests.Rendering.Components
+{
+    [TestFixture]
+    public class EllipseRendererComponentTests
+    {
+        [Test]
+        public void Radius_ConvertsEllipseToCircle_WhenSet()
+        {
+            // Arrange
+            const double radius = 1.23;
+
+            var ellipseRendererComponent = new EllipseRendererComponent
+            {
+                RadiusX = 4.56,
+                RadiusY = 7.89
+            };
+
+            // Act
+            ellipseRendererComponent.Radius = radius;
+
+            // Assert
+            Assert.That(ellipseRendererComponent.Radius, Is.EqualTo(radius));
+            Assert.That(ellipseRendererComponent.RadiusX, Is.EqualTo(radius));
+            Assert.That(ellipseRendererComponent.RadiusY, Is.EqualTo(radius));
+        }
+
+        [Test]
+        public void Radius_ReturnsValue_WhenEllipseIsCircle()
+        {
+            // Arrange
+            var ellipseRendererComponent = new EllipseRendererComponent
+            {
+                RadiusX = 1.23,
+                RadiusY = 1.23
+            };
+
+            // Act
+            var actual = ellipseRendererComponent.Radius;
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(1.23));
+        }
+
+        [Test]
+        public void Radius_ThrowsException_WhenEllipseIsNotCircle()
+        {
+            // Arrange
+            var ellipseRendererComponent = new EllipseRendererComponent
+            {
+                RadiusX = 4.56,
+                RadiusY = 7.89
+            };
+
+            // Act
+            // Assert
+            Assert.That(() =>
+            {
+                var radius = ellipseRendererComponent.Radius;
+            }, Throws.TypeOf<EllipseIsNotCircleException>());
+        }
+    }
+}
