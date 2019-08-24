158bc85a8d7552f22f75974d7aa870f259e5ece7
Dawid Komorowski <dawidkomorowski@outlook.com>
2019-08-25 01:41:41 +0200

    #55 Simplified setup of Geisha.Engine.IntegrationTests.

--- test/Geisha.Engine.IntegrationTests/IntegrationTests.cs
+++ test/Geisha.Engine.IntegrationTests/IntegrationTests.cs
@@ -1,6 +1,5 @@
 ﻿using System.Drawing;
 using Autofac;
-using Geisha.Common.Extensibility;
 using Geisha.Engine.Audio;
 using Geisha.Engine.Audio.CSCore;
 using Geisha.Engine.Input;
@@ -14,43 +13,40 @@ namespace Geisha.Engine.IntegrationTests
 {
     public abstract class IntegrationTests<TSystemUnderTest>
     {
-        // TODO Replace with raw autofac container when Geisha.Framework will no longer be as extension.
-        private ExtensionsHostContainer<TSystemUnderTest> _extensionsHostContainer;
         private RenderForm _renderForm;
+        private IContainer _container;
+        private ILifetimeScope _lifetimeScope;
         protected TSystemUnderTest SystemUnderTest { get; private set; }
 
         [SetUp]
         public virtual void SetUp()
         {
-            _renderForm = new RenderForm($"IntegrationTestsWindow") {ClientSize = new Size(1280, 720)};
-            _extensionsHostContainer = new ExtensionsHostContainer<TSystemUnderTest>(new HostServices());
-            SystemUnderTest = _extensionsHostContainer.CompositionRoot;
+            _renderForm = new RenderForm("IntegrationTestsWindow") {ClientSize = new Size(1280, 720)};
+            var containerBuilder = new ContainerBuilder();
+
+            // Register engine back-ends
+            containerBuilder.RegisterInstance(new CSCoreAudioBackend()).As<IAudioBackend>().SingleInstance();
+            containerBuilder.RegisterInstance(new WindowsInputBackend(_renderForm)).As<IInputBackend>().SingleInstance();
+            containerBuilder.RegisterInstance(new DirectXRenderingBackend(_renderForm)).As<IRenderingBackend>().SingleInstance();
+
+            // Register engine modules
+            EngineModules.RegisterAll(containerBuilder);
+
+            // Register test components
+            containerBuilder.RegisterType<TSystemUnderTest>().AsSelf().SingleInstance();
+
+            _container = containerBuilder.Build();
+            _lifetimeScope = _container.BeginLifetimeScope();
+
+            SystemUnderTest = _lifetimeScope.Resolve<TSystemUnderTest>();
         }
 
         [TearDown]
         public virtual void TearDown()
         {
-            _extensionsHostContainer.Dispose();
+            _lifetimeScope.Dispose();
+            _container.Dispose();
             _renderForm.Dispose();
-        }
-
-        private sealed class HostServices : IHostServices
-        {
-            public void Register(ContainerBuilder containerBuilder)
-            {
-                // TODO Get rid of this not disposed form after removing HostServices.
-                var renderForm = new RenderForm($"IntegrationTestsWindow") {ClientSize = new Size(1280, 720)};
-                // Register engine back-ends
-                containerBuilder.RegisterInstance(new CSCoreAudioBackend()).As<IAudioBackend>().SingleInstance();
-                containerBuilder.RegisterInstance(new WindowsInputBackend(renderForm)).As<IInputBackend>().SingleInstance();
-                containerBuilder.RegisterInstance(new DirectXRenderingBackend(renderForm)).As<IRenderingBackend>().SingleInstance();
-
-                // Register engine modules
-                EngineModules.RegisterAll(containerBuilder);
-
-                // Register test components
-                containerBuilder.RegisterType<TSystemUnderTest>().AsSelf().SingleInstance();
-            }
         }
     }
 }
