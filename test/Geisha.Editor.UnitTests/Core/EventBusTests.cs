7a3da702ef0491dac59472a639d425907f310325
Dawid Komorowski <dawidkomorowski@outlook.com>
2019-09-13 01:18:35 +0200

    #67 Added basic test scenarios for EventBus.

---
+++ test/Geisha.Editor.UnitTests/Core/EventBusTests.cs
@@ -0,0 +1,31 @@
+﻿using Geisha.Editor.Core;
+using NUnit.Framework;
+
+namespace Geisha.Editor.UnitTests.Core
+{
+    [TestFixture]
+    public class EventBusTests
+    {
+        [Test]
+        public void SendEvent_ShouldExecuteRegisteredEventHandler()
+        {
+            // Arrange
+            var eventBus = new EventBus();
+
+            TestEvent eventReceivedInHandler = null;
+            eventBus.RegisterEventHandler<TestEvent>(e => eventReceivedInHandler = e);
+
+            var @event = new TestEvent();
+
+            // Act
+            eventBus.SendEvent(@event);
+
+            // Assert
+            Assert.That(eventReceivedInHandler, Is.EqualTo(@event));
+        }
+
+        private sealed class TestEvent : IEvent
+        {
+        }
+    }
+}
