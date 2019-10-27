cb47446a786fa190eccc4f050aea703d79a8c979
Dawid Komorowski <dawidkomorowski@outlook.com>
2019-10-27 21:49:37 +0100

    #151 Implemented missing properties in TransformComponentModel.

--- src/Geisha.Editor/SceneEditor/Model/Components/TransformComponentModel.cs
+++ src/Geisha.Editor/SceneEditor/Model/Components/TransformComponentModel.cs
@@ -18,5 +18,53 @@ namespace Geisha.Editor.SceneEditor.Model.Components
             get => _component.Translation.X;
             set => _component.Translation = _component.Translation.WithX(value);
         }
+
+        public double TranslationY
+        {
+            get => _component.Translation.Y;
+            set => _component.Translation = _component.Translation.WithY(value);
+        }
+
+        public double TranslationZ
+        {
+            get => _component.Translation.Z;
+            set => _component.Translation = _component.Translation.WithZ(value);
+        }
+
+        public double RotationX
+        {
+            get => _component.Rotation.X;
+            set => _component.Rotation = _component.Rotation.WithX(value);
+        }
+
+        public double RotationY
+        {
+            get => _component.Rotation.Y;
+            set => _component.Rotation = _component.Rotation.WithY(value);
+        }
+
+        public double RotationZ
+        {
+            get => _component.Rotation.Z;
+            set => _component.Rotation = _component.Rotation.WithZ(value);
+        }
+
+        public double ScaleX
+        {
+            get => _component.Scale.X;
+            set => _component.Scale = _component.Scale.WithX(value);
+        }
+
+        public double ScaleY
+        {
+            get => _component.Scale.Y;
+            set => _component.Scale = _component.Scale.WithY(value);
+        }
+
+        public double ScaleZ
+        {
+            get => _component.Scale.Z;
+            set => _component.Scale = _component.Scale.WithZ(value);
+        }
     }
 }
--- test/Geisha.Editor.UnitTests/SceneEditor/Model/Components/TransformComponentModelTests.cs
+++ test/Geisha.Editor.UnitTests/SceneEditor/Model/Components/TransformComponentModelTests.cs
@@ -37,5 +37,117 @@ namespace Geisha.Editor.UnitTests.SceneEditor.Model.Components
             Assert.That(_transformComponentModel.TranslationX, Is.EqualTo(123));
             Assert.That(_transformComponent.Translation.X, Is.EqualTo(123));
         }
+
+        [Test]
+        public void TranslationY_ShouldUpdateTransformComponentTranslation()
+        {
+            // Assume
+            Assume.That(_transformComponentModel.TranslationY, Is.EqualTo(2));
+
+            // Act
+            _transformComponentModel.TranslationY = 123;
+
+            // Assert
+            Assert.That(_transformComponentModel.TranslationY, Is.EqualTo(123));
+            Assert.That(_transformComponent.Translation.Y, Is.EqualTo(123));
+        }
+
+        [Test]
+        public void TranslationZ_ShouldUpdateTransformComponentTranslation()
+        {
+            // Assume
+            Assume.That(_transformComponentModel.TranslationZ, Is.EqualTo(3));
+
+            // Act
+            _transformComponentModel.TranslationZ = 123;
+
+            // Assert
+            Assert.That(_transformComponentModel.TranslationZ, Is.EqualTo(123));
+            Assert.That(_transformComponent.Translation.Z, Is.EqualTo(123));
+        }
+
+        [Test]
+        public void RotationX_ShouldUpdateTransformComponentRotation()
+        {
+            // Assume
+            Assume.That(_transformComponentModel.RotationX, Is.EqualTo(4));
+
+            // Act
+            _transformComponentModel.RotationX = 123;
+
+            // Assert
+            Assert.That(_transformComponentModel.RotationX, Is.EqualTo(123));
+            Assert.That(_transformComponent.Rotation.X, Is.EqualTo(123));
+        }
+
+        [Test]
+        public void RotationY_ShouldUpdateTransformComponentRotation()
+        {
+            // Assume
+            Assume.That(_transformComponentModel.RotationY, Is.EqualTo(5));
+
+            // Act
+            _transformComponentModel.RotationY = 123;
+
+            // Assert
+            Assert.That(_transformComponentModel.RotationY, Is.EqualTo(123));
+            Assert.That(_transformComponent.Rotation.Y, Is.EqualTo(123));
+        }
+
+        [Test]
+        public void RotationZ_ShouldUpdateTransformComponentRotation()
+        {
+            // Assume
+            Assume.That(_transformComponentModel.RotationZ, Is.EqualTo(6));
+
+            // Act
+            _transformComponentModel.RotationZ = 123;
+
+            // Assert
+            Assert.That(_transformComponentModel.RotationZ, Is.EqualTo(123));
+            Assert.That(_transformComponent.Rotation.Z, Is.EqualTo(123));
+        }
+
+        [Test]
+        public void ScaleX_ShouldUpdateTransformComponentScale()
+        {
+            // Assume
+            Assume.That(_transformComponentModel.ScaleX, Is.EqualTo(7));
+
+            // Act
+            _transformComponentModel.ScaleX = 123;
+
+            // Assert
+            Assert.That(_transformComponentModel.ScaleX, Is.EqualTo(123));
+            Assert.That(_transformComponent.Scale.X, Is.EqualTo(123));
+        }
+
+        [Test]
+        public void ScaleY_ShouldUpdateTransformComponentScale()
+        {
+            // Assume
+            Assume.That(_transformComponentModel.ScaleY, Is.EqualTo(8));
+
+            // Act
+            _transformComponentModel.ScaleY = 123;
+
+            // Assert
+            Assert.That(_transformComponentModel.ScaleY, Is.EqualTo(123));
+            Assert.That(_transformComponent.Scale.Y, Is.EqualTo(123));
+        }
+
+        [Test]
+        public void ScaleZ_ShouldUpdateTransformComponentScale()
+        {
+            // Arrange
+            Assume.That(_transformComponentModel.ScaleZ, Is.EqualTo(9));
+
+            // Act
+            _transformComponentModel.ScaleZ = 123;
+
+            // Assert
+            Assert.That(_transformComponentModel.ScaleZ, Is.EqualTo(123));
+            Assert.That(_transformComponent.Scale.Z, Is.EqualTo(123));
+        }
     }
 }
