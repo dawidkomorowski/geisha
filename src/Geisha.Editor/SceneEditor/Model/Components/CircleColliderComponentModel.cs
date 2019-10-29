b802f84e473060808b34a61498a04b4fcea6eff9
Dawid Komorowski <dawidkomorowski@outlook.com>
2019-10-29 18:36:44 +0100

    #156 Implemented component model for circle collider component.

---
+++ src/Geisha.Editor/SceneEditor/Model/Components/CircleColliderComponentModel.cs
@@ -0,0 +1,22 @@
+﻿using Geisha.Engine.Physics.Components;
+
+namespace Geisha.Editor.SceneEditor.Model.Components
+{
+    public class CircleColliderComponentModel : IComponentModel
+    {
+        private readonly CircleColliderComponent _component;
+
+        public CircleColliderComponentModel(CircleColliderComponent component)
+        {
+            _component = component;
+        }
+
+        public string Name => "Circle Collider Component";
+
+        public double Radius
+        {
+            get => _component.Radius;
+            set => _component.Radius = value;
+        }
+    }
+}
---
+++ test/Geisha.Editor.UnitTests/SceneEditor/Model/Components/CircleColliderComponentModelTests.cs
@@ -0,0 +1,28 @@
+﻿using Geisha.Editor.SceneEditor.Model.Components;
+using Geisha.Engine.Physics.Components;
+using NUnit.Framework;
+
+namespace Geisha.Editor.UnitTests.SceneEditor.Model.Components
+{
+    [TestFixture]
+    public class CircleColliderComponentModelTests
+    {
+        [Test]
+        public void Radius_ShouldUpdateCircleColliderComponentRadius()
+        {
+            // Arrange
+            var circleColliderComponent = new CircleColliderComponent {Radius = 123};
+            var circleColliderComponentModel = new CircleColliderComponentModel(circleColliderComponent);
+
+            // Assume
+            Assume.That(circleColliderComponentModel.Radius, Is.EqualTo(123));
+
+            // Act
+            circleColliderComponentModel.Radius = 456;
+
+            // Assert
+            Assert.That(circleColliderComponentModel.Radius, Is.EqualTo(456));
+            Assert.That(circleColliderComponent.Radius, Is.EqualTo(456));
+        }
+    }
+}
