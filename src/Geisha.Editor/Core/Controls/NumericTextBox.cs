9e039d11cdc0cdf702a56f244f5ce7327a911f57
Dawid Komorowski <dawidkomorowski@outlook.com>
2019-10-29 01:18:25 +0100

    #151 Implemented numeric text box control that forbids most non digit characters and has auto select all on focus feature. Replaced regular TextBox with NumericTextBox in transform component properties editor view.

---
+++ src/Geisha.Editor/Core/Controls/NumericTextBox.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Geisha.Editor.Core.Controls
+{
+    public class NumericTextBox : TextBox
+    {
+        private bool _shouldSelectAll = true;
+
+        protected override void OnPreviewTextInput(TextCompositionEventArgs e)
+        {
+            base.OnPreviewTextInput(e);
+
+            var regex = new Regex("[^0-9.-]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        protected override void OnLostFocus(RoutedEventArgs e)
+        {
+            base.OnLostFocus(e);
+            _shouldSelectAll = true;
+        }
+
+        protected override void OnSelectionChanged(RoutedEventArgs e)
+        {
+            base.OnSelectionChanged(e);
+
+            if (_shouldSelectAll && IsFocused)
+            {
+                _shouldSelectAll = false;
+                SelectAll();
+            }
+        }
+    }
+}
