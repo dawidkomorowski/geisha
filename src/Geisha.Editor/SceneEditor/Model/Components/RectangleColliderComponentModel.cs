6730c06c11aa8e71a7c2ad1ca5f152e1cb1e9ae2
Dawid Komorowski <dawidkomorowski@outlook.com>
2019-10-30 19:50:08 +0100

    #157 Implemented rectangle collider component model.

---
+++ src/Geisha.Editor/SceneEditor/Model/Components/RectangleColliderComponentModel.cs
@@ -0,0 +1,28 @@
+﻿using Geisha.Engine.Physics.Components;
+
+namespace Geisha.Editor.SceneEditor.Model.Components
+{
+    public class RectangleColliderComponentModel : IComponentModel
+    {
+        private readonly RectangleColliderComponent _component;
+
+        public RectangleColliderComponentModel(RectangleColliderComponent component)
+        {
+            _component = component;
+        }
+
+        public string Name => "Rectangle Collider Component";
+
+        public double DimensionX
+        {
+            get => _component.Dimension.X;
+            set => _component.Dimension = _component.Dimension.WithX(value);
+        }
+
+        public double DimensionY
+        {
+            get => _component.Dimension.Y;
+            set => _component.Dimension = _component.Dimension.WithY(value);
+        }
+    }
+}
---
+++ test/Geisha.Editor.UnitTests/SceneEditor/Model/Components/RectangleColliderComponentModelTests.cs
@@ -0,0 +1,50 @@
+﻿using Geisha.Common.Math;
+using Geisha.Editor.SceneEditor.Model.Components;
+using Geisha.Engine.Physics.Components;
+using NUnit.Framework;
+
+namespace Geisha.Editor.UnitTests.SceneEditor.Model.Components
+{
+    [TestFixture]
+    public class RectangleColliderComponentModelTests
+    {
+        private RectangleColliderComponent _rectangleColliderComponent;
+        private RectangleColliderComponentModel _rectangleColliderComponentModel;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Arrange
+            _rectangleColliderComponent = new RectangleColliderComponent {Dimension = new Vector2(1, 2)};
+            _rectangleColliderComponentModel = new RectangleColliderComponentModel(_rectangleColliderComponent);
+        }
+
+        [Test]
+        public void DimensionX_ShouldUpdateRectangleColliderComponentDimension()
+        {
+            // Assume
+            Assume.That(_rectangleColliderComponentModel.DimensionX, Is.EqualTo(1));
+
+            // Act
+            _rectangleColliderComponentModel.DimensionX = 123;
+
+            // Assert
+            Assert.That(_rectangleColliderComponentModel.DimensionX, Is.EqualTo(123));
+            Assert.That(_rectangleColliderComponent.Dimension.X, Is.EqualTo(123));
+        }
+
+        [Test]
+        public void DimensionY_ShouldUpdateRectangleColliderComponentDimension()
+        {
+            // Assume
+            Assume.That(_rectangleColliderComponentModel.DimensionY, Is.EqualTo(2));
+
+            // Act
+            _rectangleColliderComponentModel.DimensionY = 123;
+
+            // Assert
+            Assert.That(_rectangleColliderComponentModel.DimensionY, Is.EqualTo(123));
+            Assert.That(_rectangleColliderComponent.Dimension.Y, Is.EqualTo(123));
+        }
+    }
+}
