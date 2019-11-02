379c2a8d6426553be002265d8826319d7fb1fb76
Dawid Komorowski <dawidkomorowski@outlook.com>
2019-11-02 19:16:31 +0100

    #153 Implemented Vector3Editor control providing standard editor of 3D vectors.

--- src/Geisha.Editor/Core/Controls/Vector3Editor.xaml.cs
+++ src/Geisha.Editor/Core/Controls/Vector3Editor.xaml.cs
@@ -5,31 +5,32 @@ using Geisha.Common.Math;
 namespace Geisha.Editor.Core.Controls
 {
     /// <summary>
-    /// Interaction logic for Vector2Editor.xaml
+    /// Interaction logic for Vector3Editor.xaml
     /// </summary>
-    public partial class Vector2Editor : UserControl
+    public partial class Vector3Editor : UserControl
     {
-        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(nameof(Value), typeof(Vector2), typeof(Vector2Editor),
-            new PropertyMetadata(default(Vector2), OnValueChanged));
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(nameof(Value), typeof(Vector3), typeof(Vector3Editor),
+            new PropertyMetadata(default(Vector3), OnValueChanged));
 
-        public Vector2Editor()
+        public Vector3Editor()
         {
             InitializeComponent();
         }
 
-        public Vector2 Value
+        public Vector3 Value
         {
-            get => (Vector2) GetValue(ValueProperty);
+            get => (Vector3) GetValue(ValueProperty);
             set => SetValue(ValueProperty, value);
         }
 
         private static void OnValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            var editor = (Vector2Editor) d;
-            var newValue = (Vector2) e.NewValue;
+            var editor = (Vector3Editor) d;
+            var newValue = (Vector3) e.NewValue;
 
             editor.X.Value = newValue.X;
             editor.Y.Value = newValue.Y;
+            editor.Z.Value = newValue.Z;
         }
 
         private void X_OnTextChanged(object sender, TextChangedEventArgs e)
@@ -40,6 +41,11 @@ namespace Geisha.Editor.Core.Controls
         private void Y_OnTextChanged(object sender, TextChangedEventArgs e)
         {
             Value = Value.WithY(Y.Value);
+        }
+
+        private void Z_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            Value = Value.WithZ(Z.Value);
         }
     }
 }
