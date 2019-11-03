c16dc635fc0e9e709448ecb58fced350e46ec998
Dawid Komorowski <dawidkomorowski@outlook.com>
2019-11-03 13:10:12 +0100

    #153 Implemented rectangle renderer component view model.

---
+++ src/Geisha.Editor/SceneEditor/UserInterface/EntityPropertiesEditor/Components/RectangleRendererComponent/RectangleRendererComponentPropertiesEditorViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System.Windows.Controls;
+using Geisha.Common.Math;
+using Geisha.Editor.Core;
+using Geisha.Editor.SceneEditor.Model.Components;
+using Geisha.Engine.Rendering;
+
+namespace Geisha.Editor.SceneEditor.UserInterface.EntityPropertiesEditor.Components.RectangleRendererComponent
+{
+    internal sealed class RectangleRendererComponentPropertiesEditorViewModel : ComponentPropertiesEditorViewModel
+    {
+        private readonly IProperty<Vector2> _dimension;
+        private readonly IProperty<Color> _color;
+        private readonly IProperty<bool> _fillInterior;
+        private readonly IProperty<bool> _visible;
+        private readonly IProperty<string> _sortingLayerName;
+        private readonly IProperty<int> _orderInLayer;
+
+        public RectangleRendererComponentPropertiesEditorViewModel(RectangleRendererComponentModel componentModel) : base(componentModel)
+        {
+            _dimension = CreateProperty(nameof(Dimension), componentModel.Dimension);
+            _color = CreateProperty(nameof(Color), componentModel.Color);
+            _fillInterior = CreateProperty(nameof(FillInterior), componentModel.FillInterior);
+            _visible = CreateProperty(nameof(Visible), componentModel.Visible);
+            _sortingLayerName = CreateProperty(nameof(SortingLayerName), componentModel.SortingLayerName);
+            _orderInLayer = CreateProperty(nameof(OrderInLayer), componentModel.OrderInLayer);
+
+            _dimension.Subscribe(v => componentModel.Dimension = v);
+            _color.Subscribe(v => componentModel.Color = v);
+            _fillInterior.Subscribe(v => componentModel.FillInterior = v);
+            _visible.Subscribe(v => componentModel.Visible = v);
+            _sortingLayerName.Subscribe(v => componentModel.SortingLayerName = v);
+            _orderInLayer.Subscribe(v => componentModel.OrderInLayer = v);
+        }
+
+        public override Control View { get; }
+
+        public Vector2 Dimension
+        {
+            get => _dimension.Get();
+            set => _dimension.Set(value);
+        }
+
+        public Color Color
+        {
+            get => _color.Get();
+            set => _color.Set(value);
+        }
+
+        public bool FillInterior
+        {
+            get => _fillInterior.Get();
+            set => _fillInterior.Set(value);
+        }
+
+        public bool Visible
+        {
+            get => _visible.Get();
+            set => _visible.Set(value);
+        }
+
+        public string SortingLayerName
+        {
+            get => _sortingLayerName.Get();
+            set => _sortingLayerName.Set(value);
+        }
+
+        public int OrderInLayer
+        {
+            get => _orderInLayer.Get();
+            set => _orderInLayer.Set(value);
+        }
+    }
+}
---
+++ test/Geisha.Editor.UnitTests/SceneEditor/UserInterface/EntityPropertiesEditor/Components/RectangleRendererComponent/RectangleRendererComponentPropertiesEditorViewModelTests.cs
@@ -0,0 +1,116 @@
+﻿using Geisha.Common.Math;
+using Geisha.Editor.SceneEditor.Model.Components;
+using Geisha.Editor.SceneEditor.UserInterface.EntityPropertiesEditor.Components.RectangleRendererComponent;
+using Geisha.Engine.Rendering;
+using NUnit.Framework;
+
+namespace Geisha.Editor.UnitTests.SceneEditor.UserInterface.EntityPropertiesEditor.Components.RectangleRendererComponent
+{
+    [TestFixture]
+    public class RectangleRendererComponentPropertiesEditorViewModelTests
+    {
+        private RectangleRendererComponentModel _rectangleRendererComponentModel;
+        private RectangleRendererComponentPropertiesEditorViewModel _rectangleRendererComponentPropertiesEditorViewModel;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Arrange
+            var rectangleRendererComponent = new Engine.Rendering.Components.RectangleRendererComponent
+            {
+                Dimension = new Vector2(1, 2),
+                Color = Color.FromArgb(1, 2, 3, 4),
+                FillInterior = true,
+                Visible = true,
+                SortingLayerName = "Test Layer",
+                OrderInLayer = 1
+            };
+            _rectangleRendererComponentModel = new RectangleRendererComponentModel(rectangleRendererComponent);
+            _rectangleRendererComponentPropertiesEditorViewModel = new RectangleRendererComponentPropertiesEditorViewModel(_rectangleRendererComponentModel);
+        }
+
+        [Test]
+        public void Dimension_ShouldUpdateRectangleRendererComponentModelDimension()
+        {
+            // Assume
+            Assume.That(_rectangleRendererComponentPropertiesEditorViewModel.Dimension, Is.EqualTo(new Vector2(1, 2)));
+
+            // Act
+            _rectangleRendererComponentPropertiesEditorViewModel.Dimension = new Vector2(123, 456);
+
+            // Assert
+            Assert.That(_rectangleRendererComponentPropertiesEditorViewModel.Dimension, Is.EqualTo(new Vector2(123, 456)));
+            Assert.That(_rectangleRendererComponentModel.Dimension, Is.EqualTo(new Vector2(123, 456)));
+        }
+
+        [Test]
+        public void Color_ShouldUpdateRectangleRendererComponentModelColor()
+        {
+            // Assume
+            Assume.That(_rectangleRendererComponentPropertiesEditorViewModel.Color, Is.EqualTo(Color.FromArgb(1, 2, 3, 4)));
+
+            // Act
+            _rectangleRendererComponentPropertiesEditorViewModel.Color = Color.FromArgb(11, 22, 33, 44);
+
+            // Assert
+            Assert.That(_rectangleRendererComponentPropertiesEditorViewModel.Color, Is.EqualTo(Color.FromArgb(11, 22, 33, 44)));
+            Assert.That(_rectangleRendererComponentModel.Color, Is.EqualTo(Color.FromArgb(11, 22, 33, 44)));
+        }
+
+        [Test]
+        public void FillInterior_ShouldUpdateRectangleRendererComponentModelFillInterior()
+        {
+            // Assume
+            Assume.That(_rectangleRendererComponentPropertiesEditorViewModel.FillInterior, Is.True);
+
+            // Act
+            _rectangleRendererComponentPropertiesEditorViewModel.FillInterior = false;
+
+            // Assert
+            Assert.That(_rectangleRendererComponentPropertiesEditorViewModel.FillInterior, Is.False);
+            Assert.That(_rectangleRendererComponentModel.FillInterior, Is.False);
+        }
+
+        [Test]
+        public void Visible_ShouldUpdateRectangleRendererComponentModelVisible()
+        {
+            // Assume
+            Assume.That(_rectangleRendererComponentPropertiesEditorViewModel.Visible, Is.True);
+
+            // Act
+            _rectangleRendererComponentPropertiesEditorViewModel.Visible = false;
+
+            // Assert
+            Assert.That(_rectangleRendererComponentPropertiesEditorViewModel.Visible, Is.False);
+            Assert.That(_rectangleRendererComponentModel.Visible, Is.False);
+        }
+
+        [Test]
+        public void SortingLayerName_ShouldUpdateRectangleRendererComponentSortingLayerName()
+        {
+            // Assume
+            Assume.That(_rectangleRendererComponentPropertiesEditorViewModel.SortingLayerName, Is.EqualTo("Test Layer"));
+
+            // Act
+            _rectangleRendererComponentPropertiesEditorViewModel.SortingLayerName = "Other Layer";
+
+            // Assert
+            Assert.That(_rectangleRendererComponentPropertiesEditorViewModel.SortingLayerName, Is.EqualTo("Other Layer"));
+            Assert.That(_rectangleRendererComponentModel.SortingLayerName, Is.EqualTo("Other Layer"));
+        }
+
+        [Test]
+        public void OrderInLayer_ShouldUpdateRectangleRendererComponentOrderInLayer()
+        {
+            // Assume
+            Assume.That(_rectangleRendererComponentPropertiesEditorViewModel.OrderInLayer, Is.EqualTo(1));
+
+            // Act
+            _rectangleRendererComponentPropertiesEditorViewModel.OrderInLayer = 123;
+
+            // Assert
+            Assert.That(_rectangleRendererComponentPropertiesEditorViewModel.OrderInLayer, Is.EqualTo(123));
+            Assert.That(_rectangleRendererComponentModel.OrderInLayer, Is.EqualTo(123));
+        }
+    }
+}
