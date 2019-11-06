242dd285c64135dc1e4adea7c07931357ee644e2
Dawid Komorowski <dawidkomorowski@outlook.com>
2019-11-06 23:05:47 +0100

    #153 Implemented color picker user interface for rectangle renderer component editor.

---
+++ src/Geisha.Editor/Core/ColorConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using Geisha.Engine.Rendering;
+
+namespace Geisha.Editor.Core
+{
+    public sealed class ColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var color = (Color) value;
+            return System.Windows.Media.Color.FromArgb(color.A, color.R, color.G, color.B);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var wpfColor = (System.Windows.Media.Color) value;
+            return Color.FromArgb(wpfColor.A, wpfColor.R, wpfColor.G, wpfColor.B);
+        }
+    }
+}
