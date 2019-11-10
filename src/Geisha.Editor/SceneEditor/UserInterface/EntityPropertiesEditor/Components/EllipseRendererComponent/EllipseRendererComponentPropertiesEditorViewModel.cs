8c3d17a9848ad020568cd5be5097e5197a192651
Dawid Komorowski <dawidkomorowski@outlook.com>
2019-11-10 13:44:10 +0100

    #154 Implemented ellipse renderer component view model.

---
+++ src/Geisha.Editor/SceneEditor/UserInterface/EntityPropertiesEditor/Components/EllipseRendererComponent/EllipseRendererComponentPropertiesEditorViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System.Windows.Controls;
+using Geisha.Editor.Core;
+using Geisha.Editor.SceneEditor.Model.Components;
+using Geisha.Engine.Rendering;
+
+namespace Geisha.Editor.SceneEditor.UserInterface.EntityPropertiesEditor.Components.EllipseRendererComponent
+{
+    internal sealed class EllipseRendererComponentPropertiesEditorViewModel : ComponentPropertiesEditorViewModel
+    {
+        private readonly IProperty<double> _radiusX;
+        private readonly IProperty<double> _radiusY;
+        private readonly IProperty<Color> _color;
+        private readonly IProperty<bool> _fillInterior;
+        private readonly IProperty<bool> _visible;
+        private readonly IProperty<string> _sortingLayerName;
+        private readonly IProperty<int> _orderInLayer;
+
+        public EllipseRendererComponentPropertiesEditorViewModel(EllipseRendererComponentModel componentModel) : base(componentModel)
+        {
+            _radiusX = CreateProperty(nameof(RadiusX), componentModel.RadiusX);
+            _radiusY = CreateProperty(nameof(RadiusY), componentModel.RadiusY);
+            _color = CreateProperty(nameof(Color), componentModel.Color);
+            _fillInterior = CreateProperty(nameof(FillInterior), componentModel.FillInterior);
+            _visible = CreateProperty(nameof(Visible), componentModel.Visible);
+            _sortingLayerName = CreateProperty(nameof(SortingLayerName), componentModel.SortingLayerName);
+            _orderInLayer = CreateProperty(nameof(OrderInLayer), componentModel.OrderInLayer);
+
+            _radiusX.Subscribe(v => componentModel.RadiusX = v);
+            _radiusY.Subscribe(v => componentModel.RadiusY = v);
+            _color.Subscribe(v => componentModel.Color = v);
+            _fillInterior.Subscribe(v => componentModel.FillInterior = v);
+            _visible.Subscribe(v => componentModel.Visible = v);
+            _sortingLayerName.Subscribe(v => componentModel.SortingLayerName = v);
+            _orderInLayer.Subscribe(v => componentModel.OrderInLayer = v);
+        }
+
+        public override Control View { get; }
+
+        public double RadiusX
+        {
+            get => _radiusX.Get();
+            set => _radiusX.Set(value);
+        }
+
+        public double RadiusY
+        {
+            get => _radiusY.Get();
+            set => _radiusY.Set(value);
+        }
+
+        public Color Color
+        {
+            get => _color.Get();
+            set => _color.Set(value);
+        }
+
+        public bool FillInterior
+        {
+            get => _fillInterior.Get();
+            set => _fillInterior.Set(value);
+        }
+
+        public bool Visible
+        {
+            get => _visible.Get();
+            set => _visible.Set(value);
+        }
+
+        public string SortingLayerName
+        {
+            get => _sortingLayerName.Get();
+            set => _sortingLayerName.Set(value);
+        }
+
+        public int OrderInLayer
+        {
+            get => _orderInLayer.Get();
+            set => _orderInLayer.Set(value);
+        }
+    }
+}
---
+++ test/Geisha.Editor.UnitTests/SceneEditor/UserInterface/EntityPropertiesEditor/Components/EllipseRendererComponent/EllipseRendererComponentPropertiesEditorViewModelTests.cs
@@ -0,0 +1,130 @@
+﻿using Geisha.Editor.SceneEditor.Model.Components;
+using Geisha.Editor.SceneEditor.UserInterface.EntityPropertiesEditor.Components.EllipseRendererComponent;
+using Geisha.Engine.Rendering;
+using NUnit.Framework;
+
+namespace Geisha.Editor.UnitTests.SceneEditor.UserInterface.EntityPropertiesEditor.Components.EllipseRendererComponent
+{
+    [TestFixture]
+    public class EllipseRendererComponentPropertiesEditorViewModelTests
+    {
+        private EllipseRendererComponentModel _ellipseRendererComponentModel;
+        private EllipseRendererComponentPropertiesEditorViewModel _ellipseRendererComponentPropertiesEditorViewModel;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Arrange
+            var ellipseRendererComponent = new Engine.Rendering.Components.EllipseRendererComponent
+            {
+                RadiusX = 1,
+                RadiusY = 2,
+                Color = Color.FromArgb(1, 2, 3, 4),
+                FillInterior = true,
+                Visible = true,
+                SortingLayerName = "Test Layer",
+                OrderInLayer = 1
+            };
+            _ellipseRendererComponentModel = new EllipseRendererComponentModel(ellipseRendererComponent);
+            _ellipseRendererComponentPropertiesEditorViewModel = new EllipseRendererComponentPropertiesEditorViewModel(_ellipseRendererComponentModel);
+        }
+
+        [Test]
+        public void RadiusX_ShouldUpdateEllipseRendererComponentModelRadiusX()
+        {
+            // Assume
+            Assume.That(_ellipseRendererComponentPropertiesEditorViewModel.RadiusX, Is.EqualTo(1));
+
+            // Act
+            _ellipseRendererComponentPropertiesEditorViewModel.RadiusX = 123;
+
+            // Assert
+            Assert.That(_ellipseRendererComponentPropertiesEditorViewModel.RadiusX, Is.EqualTo(123));
+            Assert.That(_ellipseRendererComponentModel.RadiusX, Is.EqualTo(123));
+        }
+
+        [Test]
+        public void RadiusY_ShouldUpdateEllipseRendererComponentModelRadiusY()
+        {
+            // Assume
+            Assume.That(_ellipseRendererComponentPropertiesEditorViewModel.RadiusY, Is.EqualTo(2));
+
+            // Act
+            _ellipseRendererComponentPropertiesEditorViewModel.RadiusY = 123;
+
+            // Assert
+            Assert.That(_ellipseRendererComponentPropertiesEditorViewModel.RadiusY, Is.EqualTo(123));
+            Assert.That(_ellipseRendererComponentModel.RadiusY, Is.EqualTo(123));
+        }
+
+        [Test]
+        public void Color_ShouldUpdateEllipseRendererComponentModelColor()
+        {
+            // Assume
+            Assume.That(_ellipseRendererComponentPropertiesEditorViewModel.Color, Is.EqualTo(Color.FromArgb(1, 2, 3, 4)));
+
+            // Act
+            _ellipseRendererComponentPropertiesEditorViewModel.Color = Color.FromArgb(11, 22, 33, 44);
+
+            // Assert
+            Assert.That(_ellipseRendererComponentPropertiesEditorViewModel.Color, Is.EqualTo(Color.FromArgb(11, 22, 33, 44)));
+            Assert.That(_ellipseRendererComponentModel.Color, Is.EqualTo(Color.FromArgb(11, 22, 33, 44)));
+        }
+
+        [Test]
+        public void FillInterior_ShouldUpdateEllipseRendererComponentModelFillInterior()
+        {
+            // Assume
+            Assume.That(_ellipseRendererComponentPropertiesEditorViewModel.FillInterior, Is.True);
+
+            // Act
+            _ellipseRendererComponentPropertiesEditorViewModel.FillInterior = false;
+
+            // Assert
+            Assert.That(_ellipseRendererComponentPropertiesEditorViewModel.FillInterior, Is.False);
+            Assert.That(_ellipseRendererComponentModel.FillInterior, Is.False);
+        }
+
+        [Test]
+        public void Visible_ShouldUpdateEllipseRendererComponentModelVisible()
+        {
+            // Assume
+            Assume.That(_ellipseRendererComponentPropertiesEditorViewModel.Visible, Is.True);
+
+            // Act
+            _ellipseRendererComponentPropertiesEditorViewModel.Visible = false;
+
+            // Assert
+            Assert.That(_ellipseRendererComponentPropertiesEditorViewModel.Visible, Is.False);
+            Assert.That(_ellipseRendererComponentModel.Visible, Is.False);
+        }
+
+        [Test]
+        public void SortingLayerName_ShouldUpdateEllipseRendererComponentModelSortingLayerName()
+        {
+            // Assume
+            Assume.That(_ellipseRendererComponentPropertiesEditorViewModel.SortingLayerName, Is.EqualTo("Test Layer"));
+
+            // Act
+            _ellipseRendererComponentPropertiesEditorViewModel.SortingLayerName = "Other Layer";
+
+            // Assert
+            Assert.That(_ellipseRendererComponentPropertiesEditorViewModel.SortingLayerName, Is.EqualTo("Other Layer"));
+            Assert.That(_ellipseRendererComponentModel.SortingLayerName, Is.EqualTo("Other Layer"));
+        }
+
+        [Test]
+        public void OrderInLayer_ShouldUpdateEllipseRendererComponentModelOrderInLayer()
+        {
+            // Assume
+            Assume.That(_ellipseRendererComponentPropertiesEditorViewModel.OrderInLayer, Is.EqualTo(1));
+
+            // Act
+            _ellipseRendererComponentPropertiesEditorViewModel.OrderInLayer = 123;
+
+            // Assert
+            Assert.That(_ellipseRendererComponentPropertiesEditorViewModel.OrderInLayer, Is.EqualTo(123));
+            Assert.That(_ellipseRendererComponentModel.OrderInLayer, Is.EqualTo(123));
+        }
+    }
+}
