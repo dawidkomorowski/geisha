fe4c5509a68c290f432ac0ab86a6b6de9021d605
Dawid Komorowski <dawidkomorowski@outlook.com>
2019-11-24 15:01:09 +0100

    #155 Implemented text renderer component model.

---
+++ src/Geisha.Editor/SceneEditor/Model/Components/TextRendererComponentModel.cs
@@ -0,0 +1,53 @@
+﻿using Geisha.Engine.Rendering;
+using Geisha.Engine.Rendering.Components;
+
+namespace Geisha.Editor.SceneEditor.Model.Components
+{
+    internal sealed class TextRendererComponentModel : IComponentModel
+    {
+        private readonly TextRendererComponent _component;
+
+        public TextRendererComponentModel(TextRendererComponent component)
+        {
+            _component = component;
+        }
+
+        public string Name => "Text Renderer Component";
+
+        public string Text
+        {
+            get => _component.Text;
+            set => _component.Text = value;
+        }
+
+        public FontSize FontSize
+        {
+            get => _component.FontSize;
+            set => _component.FontSize = value;
+        }
+
+        public Color Color
+        {
+            get => _component.Color;
+            set => _component.Color = value;
+        }
+
+        public bool Visible
+        {
+            get => _component.Visible;
+            set => _component.Visible = value;
+        }
+
+        public string SortingLayerName
+        {
+            get => _component.SortingLayerName;
+            set => _component.SortingLayerName = value;
+        }
+
+        public int OrderInLayer
+        {
+            get => _component.OrderInLayer;
+            set => _component.OrderInLayer = value;
+        }
+    }
+}
---
+++ test/Geisha.Editor.UnitTests/SceneEditor/Model/Components/TextRendererComponentModelTests.cs
@@ -0,0 +1,114 @@
+﻿using Geisha.Editor.SceneEditor.Model.Components;
+using Geisha.Engine.Rendering;
+using Geisha.Engine.Rendering.Components;
+using NUnit.Framework;
+
+namespace Geisha.Editor.UnitTests.SceneEditor.Model.Components
+{
+    [TestFixture]
+    public class TextRendererComponentModelTests
+    {
+        private TextRendererComponent _textRendererComponent;
+        private TextRendererComponentModel _textRendererComponentModel;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Arrange
+            _textRendererComponent = new TextRendererComponent
+            {
+                Text = "Some Text",
+                FontSize = FontSize.FromDips(1),
+                Color = Color.FromArgb(1, 2, 3, 4),
+                Visible = true,
+                SortingLayerName = "Test Layer",
+                OrderInLayer = 1
+            };
+            _textRendererComponentModel = new TextRendererComponentModel(_textRendererComponent);
+        }
+
+        [Test]
+        public void Text_ShouldUpdateTextRendererComponentText()
+        {
+            // Assume
+            Assume.That(_textRendererComponentModel.Text, Is.EqualTo("Some Text"));
+
+            // Act
+            _textRendererComponentModel.Text = "Other Text";
+
+            // Assert
+            Assert.That(_textRendererComponentModel.Text, Is.EqualTo("Other Text"));
+            Assert.That(_textRendererComponent.Text, Is.EqualTo("Other Text"));
+        }
+
+        [Test]
+        public void FontSize_ShouldUpdateTextRendererComponentFontSize()
+        {
+            // Assume
+            Assume.That(_textRendererComponentModel.FontSize, Is.EqualTo(FontSize.FromDips(1)));
+
+            // Act
+            _textRendererComponentModel.FontSize = FontSize.FromDips(123);
+
+            // Assert
+            Assert.That(_textRendererComponentModel.FontSize, Is.EqualTo(FontSize.FromDips(123)));
+            Assert.That(_textRendererComponent.FontSize, Is.EqualTo(FontSize.FromDips(123)));
+        }
+
+        [Test]
+        public void Color_ShouldUpdateTextRendererComponentColor()
+        {
+            // Assume
+            Assume.That(_textRendererComponentModel.Color, Is.EqualTo(Color.FromArgb(1, 2, 3, 4)));
+
+            // Act
+            _textRendererComponentModel.Color = Color.FromArgb(11, 22, 33, 44);
+
+            // Assert
+            Assert.That(_textRendererComponentModel.Color, Is.EqualTo(Color.FromArgb(11, 22, 33, 44)));
+            Assert.That(_textRendererComponent.Color, Is.EqualTo(Color.FromArgb(11, 22, 33, 44)));
+        }
+
+        [Test]
+        public void Visible_ShouldUpdateTextRendererComponentVisible()
+        {
+            // Assume
+            Assume.That(_textRendererComponentModel.Visible, Is.True);
+
+            // Act
+            _textRendererComponentModel.Visible = false;
+
+            // Assert
+            Assert.That(_textRendererComponentModel.Visible, Is.False);
+            Assert.That(_textRendererComponent.Visible, Is.False);
+        }
+
+        [Test]
+        public void SortingLayerName_ShouldUpdateTextRendererComponentSortingLayerName()
+        {
+            // Assume
+            Assume.That(_textRendererComponentModel.SortingLayerName, Is.EqualTo("Test Layer"));
+
+            // Act
+            _textRendererComponentModel.SortingLayerName = "Other Layer";
+
+            // Assert
+            Assert.That(_textRendererComponentModel.SortingLayerName, Is.EqualTo("Other Layer"));
+            Assert.That(_textRendererComponent.SortingLayerName, Is.EqualTo("Other Layer"));
+        }
+
+        [Test]
+        public void OrderInLayer_ShouldUpdateTextRendererComponentOrderInLayer()
+        {
+            // Assume
+            Assume.That(_textRendererComponentModel.OrderInLayer, Is.EqualTo(1));
+
+            // Act
+            _textRendererComponentModel.OrderInLayer = 123;
+
+            // Assert
+            Assert.That(_textRendererComponentModel.OrderInLayer, Is.EqualTo(123));
+            Assert.That(_textRendererComponent.OrderInLayer, Is.EqualTo(123));
+        }
+    }
+}
