0e797c1cf1c9887336546e9e4c24a923cabd7ac4
Dawid Komorowski <dawidkomorowski@outlook.com>
2019-12-29 00:34:39 +0100

    #123 Implemented CreateTextureCommand.

--- src/Geisha.Editor/CreateTexture/UserInterface/CreateTextureCommand.cs
+++ src/Geisha.Editor/CreateTexture/UserInterface/CreateTextureCommand.cs
@@ -1,18 +1,29 @@
 ﻿using System;
 using System.Windows.Input;
+using Geisha.Editor.CreateTexture.Model;
+using Geisha.Editor.ProjectHandling.Model;
 
 namespace Geisha.Editor.CreateTexture.UserInterface
 {
     internal sealed class CreateTextureCommand : ICommand
     {
+        private readonly ICreateTextureService _createTextureService;
+        private readonly IProjectFile _projectFile;
+
+        public CreateTextureCommand(ICreateTextureService createTextureService, IProjectFile projectFile)
+        {
+            _createTextureService = createTextureService;
+            _projectFile = projectFile;
+        }
+
         public bool CanExecute(object parameter)
         {
-            throw new NotImplementedException();
+            return true;
         }
 
         public void Execute(object parameter)
         {
-            throw new NotImplementedException();
+            _createTextureService.CreateTexture(_projectFile);
         }
 
         public event EventHandler CanExecuteChanged;
---
+++ test/Geisha.Editor.UnitTests/CreateTexture/UserInterface/CreateTextureCommandTests.cs
@@ -0,0 +1,46 @@
+﻿using Geisha.Editor.CreateTexture.Model;
+using Geisha.Editor.CreateTexture.UserInterface;
+using Geisha.Editor.ProjectHandling.Model;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Geisha.Editor.UnitTests.CreateTexture.UserInterface
+{
+    [TestFixture]
+    public class CreateTextureCommandTests
+    {
+        private ICreateTextureService _createTextureService;
+        private IProjectFile _projectFile;
+        private CreateTextureCommand _command;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _createTextureService = Substitute.For<ICreateTextureService>();
+            _projectFile = Substitute.For<IProjectFile>();
+            _command = new CreateTextureCommand(_createTextureService, _projectFile);
+        }
+
+        [Test]
+        public void CanExecute_ShouldReturnTrue()
+        {
+            // Arrange
+            // Act
+            var actual = _command.CanExecute(null);
+
+            // Assert
+            Assert.That(actual, Is.True);
+        }
+
+        [Test]
+        public void Execute_ShouldRunCreateTextureServiceWithProjectFile()
+        {
+            // Arrange
+            // Act
+            _command.Execute(null);
+
+            // Assert
+            _createTextureService.Received().CreateTexture(_projectFile);
+        }
+    }
+}
