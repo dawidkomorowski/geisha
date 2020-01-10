a00eee39de85774204b835fbacfb9df5b2ff2b2b
Dawid Komorowski <dawidkomorowski@outlook.com>
2020-01-10 22:42:54 +0100

    #123 Implemented boilerplate code of CreateSpriteService.

--- src/Geisha.Editor/CreateSprite/Model/CreateSpriteService.cs
+++ src/Geisha.Editor/CreateSprite/Model/CreateSpriteService.cs
@@ -1,4 +1,10 @@
-﻿using Geisha.Editor.ProjectHandling.Model;
+﻿using System.IO;
+using System.Text;
+using Geisha.Common.Serialization;
+using Geisha.Editor.ProjectHandling.Model;
+using Geisha.Engine.Core.Assets;
+using Geisha.Engine.Rendering.Assets;
+using Geisha.Engine.Rendering.Assets.Serialization;
 
 namespace Geisha.Editor.CreateSprite.Model
 {
@@ -9,9 +15,35 @@ namespace Geisha.Editor.CreateSprite.Model
 
     internal sealed class CreateSpriteService : ICreateSpriteService
     {
+        private readonly IJsonSerializer _jsonSerializer;
+
+        public CreateSpriteService(IJsonSerializer jsonSerializer)
+        {
+            _jsonSerializer = jsonSerializer;
+        }
+
         public void CreateSprite(IProjectFile textureFile)
         {
-            throw new System.NotImplementedException();
+            var spriteFileName = $"{Path.GetFileNameWithoutExtension(textureFile.Name)}{RenderingFileExtensions.Sprite}";
+            var folder = textureFile.ParentFolder;
+
+            var spriteFileContent = new SpriteFileContent
+            {
+                AssetId = AssetId.CreateUnique().Value
+            };
+
+            var json = _jsonSerializer.Serialize(spriteFileContent);
+
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var streamWriter = new StreamWriter(memoryStream, Encoding.UTF8, 512, true))
+                {
+                    streamWriter.Write(json);
+                }
+
+                memoryStream.Position = 0;
+                folder.AddFile(spriteFileName, memoryStream);
+            }
         }
     }
 }
--- test/Geisha.Editor.UnitTests/CreateSprite/Model/CreateSpriteServiceTests.cs
+++ test/Geisha.Editor.UnitTests/CreateSprite/Model/CreateSpriteServiceTests.cs
@@ -1,9 +1,54 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.IO;
+using Geisha.Common.Serialization;
+using Geisha.Editor.CreateSprite.Model;
+using Geisha.Editor.ProjectHandling.Model;
+using Geisha.Engine.Rendering.Assets;
+using Geisha.Engine.Rendering.Assets.Serialization;
+using NSubstitute;
+using NSubstitute.ReturnsExtensions;
+using NUnit.Framework;
 
 namespace Geisha.Editor.UnitTests.CreateSprite.Model
 {
     [TestFixture]
     public class CreateSpriteServiceTests
     {
+        [Test]
+        public void CreateSprite_ShouldCreateSpriteAssetFileInTheSameFolderAsTextureFile_GivenTextureFile()
+        {
+            // Arrange
+            const string json = "some json";
+            var jsonSerializer = Substitute.For<IJsonSerializer>();
+            var parentFolder = Substitute.For<IProjectFolder>();
+            var textureFile = Substitute.For<IProjectFile>();
+
+            textureFile.Name.Returns($"TextureForSprite{RenderingFileExtensions.Texture}");
+            textureFile.ParentFolder.Returns(parentFolder);
+
+            var createSpriteService = new CreateSpriteService(jsonSerializer);
+
+            // Define assertions
+            jsonSerializer.Serialize(Arg.Do<SpriteFileContent>(content =>
+            {
+                Assert.That(content.AssetId, Is.Not.EqualTo(Guid.Empty));
+                // TODO Assert rest of sprite file content properties.
+            })).Returns(json);
+
+            parentFolder.AddFile(Arg.Any<string>(), Arg.Do<Stream>(stream =>
+            {
+                using (var streamReader = new StreamReader(stream))
+                {
+                    var content = streamReader.ReadToEnd();
+                    Assert.That(content, Is.EqualTo(json));
+                }
+            })).ReturnsNull();
+
+            // Act
+            createSpriteService.CreateSprite(textureFile);
+
+            // Assert
+            parentFolder.Received().AddFile($"TextureForSprite{RenderingFileExtensions.Sprite}", Arg.Any<Stream>());
+        }
     }
 }
