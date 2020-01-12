2262415055b1b84da59ce2fe1e78147b52665466
Dawid Komorowski <dawidkomorowski@outlook.com>
2020-01-12 03:00:37 +0100

    #123 Implemented CreateSpriteService so it creates .gsprite file based on .gtexture file.

--- src/Geisha.Editor/CreateSprite/Model/CreateSpriteService.cs
+++ src/Geisha.Editor/CreateSprite/Model/CreateSpriteService.cs
@@ -1,5 +1,9 @@
-﻿using System.IO;
+﻿using System.Drawing;
+using System.IO;
 using System.Text;
+using Geisha.Common.FileSystem;
+using Geisha.Common.Math;
+using Geisha.Common.Math.Serialization;
 using Geisha.Common.Serialization;
 using Geisha.Editor.ProjectHandling.Model;
 using Geisha.Engine.Core.Assets;
@@ -24,12 +28,27 @@ namespace Geisha.Editor.CreateSprite.Model
 
         public void CreateSprite(IProjectFile textureFile)
         {
+            var serializedTextureFileContent = File.ReadAllText(textureFile.Path);
+            var textureFileContent = _jsonSerializer.Deserialize<TextureFileContent>(serializedTextureFileContent);
+
+            var textureImageFilePath = PathUtils.GetSiblingPath(textureFile.Path, textureFileContent.TextureFilePath);
+            Vector2 spriteDimension;
+            using (var bitmapImage = Image.FromFile(textureImageFilePath))
+            {
+                spriteDimension = new Vector2(bitmapImage.Width, bitmapImage.Height);
+            }
+
             var spriteFileName = $"{Path.GetFileNameWithoutExtension(textureFile.Name)}{RenderingFileExtensions.Sprite}";
             var folder = textureFile.ParentFolder;
 
             var spriteFileContent = new SpriteFileContent
             {
-                AssetId = AssetId.CreateUnique().Value
+                AssetId = AssetId.CreateUnique().Value,
+                TextureAssetId = textureFileContent.AssetId,
+                SourceUV = SerializableVector2.FromVector2(Vector2.Zero),
+                SourceDimension = SerializableVector2.FromVector2(spriteDimension),
+                SourceAnchor = SerializableVector2.FromVector2(spriteDimension / 2),
+                PixelsPerUnit = 1
             };
 
             var json = _jsonSerializer.Serialize(spriteFileContent);
--- test/Geisha.Editor.IntegrationTests/CreateSprite/Model/CreateSpriteServiceIntegrationTests.cs
+++ test/Geisha.Editor.IntegrationTests/CreateSprite/Model/CreateSpriteServiceIntegrationTests.cs
@@ -8,6 +8,7 @@ using Geisha.Editor.IntegrationTests.ProjectHandling.Model;
 using Geisha.Editor.ProjectHandling.Model;
 using Geisha.Engine.Rendering.Assets;
 using Geisha.Engine.Rendering.Assets.Serialization;
+using Geisha.IntegrationTestsData;
 using NUnit.Framework;
 
 namespace Geisha.Editor.IntegrationTests.CreateSprite.Model
@@ -34,7 +35,7 @@ namespace Geisha.Editor.IntegrationTests.CreateSprite.Model
             File.Copy(textureFilePathToCopy, textureFilePathInProject);
 
             project = Project.Open(projectFilePath);
-            var textureProjectFile = project.Files.Single(f => Path.GetExtension(f.Name) == RenderingFileExtensions.Texture);
+            var textureProjectFile = project.Files.Single(f => f.Extension == RenderingFileExtensions.Texture);
 
             var jsonSerializer = new JsonSerializer();
             var createSpriteService = new CreateSpriteService(jsonSerializer);
@@ -49,7 +50,14 @@ namespace Geisha.Editor.IntegrationTests.CreateSprite.Model
             var json = File.ReadAllText(spriteFilePath);
             var spriteFileContent = jsonSerializer.Deserialize<SpriteFileContent>(json);
             Assert.That(spriteFileContent.AssetId, Is.Not.EqualTo(Guid.Empty));
-            // TODO Assert rest of sprite file content properties.
+            Assert.That(spriteFileContent.TextureAssetId, Is.EqualTo(AssetsIds.TestTexture.Value));
+            Assert.That(spriteFileContent.SourceUV.X, Is.Zero);
+            Assert.That(spriteFileContent.SourceUV.Y, Is.Zero);
+            Assert.That(spriteFileContent.SourceDimension.X, Is.EqualTo(10));
+            Assert.That(spriteFileContent.SourceDimension.Y, Is.EqualTo(10));
+            Assert.That(spriteFileContent.SourceAnchor.X, Is.EqualTo(5));
+            Assert.That(spriteFileContent.SourceAnchor.Y, Is.EqualTo(5));
+            Assert.That(spriteFileContent.PixelsPerUnit, Is.EqualTo(1));
         }
     }
 }
