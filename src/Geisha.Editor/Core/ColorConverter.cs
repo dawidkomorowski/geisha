dc2eb5c2e7cebb6b2d5b9c4181aae99535bc6534
Dawid Komorowski <dawidkomorowski@outlook.com>
2020-04-23 21:49:19 +0200

    Fix for WPF designer in latest VS.

--- src/Geisha.Editor/Core/ColorConverter.cs
+++ src/Geisha.Editor/Core/ColorConverter.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
+using Geisha.Common.Logging;
 using Geisha.Engine.Rendering;
 
 namespace Geisha.Editor.Core
 {
     public sealed class ColorConverter : IValueConverter
     {
+        private static readonly ILog Log = LogFactory.Create(typeof(ColorConverter));
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            var color = (Color) value;
-            return System.Windows.Media.Color.FromArgb(color.A, color.R, color.G, color.B);
+            if (value is Color color)
+            {
+                return System.Windows.Media.Color.FromArgb(color.A, color.R, color.G, color.B);
+            }
+            else
+            {
+                Log.Error(
+                    $"Color could not be converted. Expected instance of type {typeof(Color).FullName} but received {value?.GetType().FullName}.");
+                return DependencyProperty.UnsetValue;
+            }
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
