90e569293daa3441448bfbe5af53716d9bbea63f
Dawid Komorowski <dawidkomorowski@outlook.com>
2020-09-12 16:52:12 +0200

    Created prototype of IPlayback interface.

---
+++ src/Geisha.Engine/Audio/Backend/IPlayback.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Geisha.Engine.Audio.Backend
+{
+    public interface IPlayback : IDisposable
+    {
+        event EventHandler Played;
+        event EventHandler Paused;
+        event EventHandler Stopped;
+        event EventHandler Disposed;
+
+        void Play();
+        void Pause();
+        void Stop();
+    }
+}
