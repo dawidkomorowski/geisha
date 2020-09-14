0e1ab8052a35469ce0417615a6ceb38c62199ffe
Dawid Komorowski <dawidkomorowski@outlook.com>
2020-09-14 21:42:23 +0200

    Added documentation to IPlayback interface.

--- src/Geisha.Engine/Audio/Backend/IPlayback.cs
+++ src/Geisha.Engine/Audio/Backend/IPlayback.cs
@@ -2,16 +2,46 @@
 
 namespace Geisha.Engine.Audio.Backend
 {
-    // TODO Add docs
+    /// <summary>
+    ///     Represents ongoing sound playback and allows to control its behavior.
+    /// </summary>
+    /// <remarks>
+    ///     When particular sound is no longer planned to be played then corresponding <see cref="IPlayback" /> object
+    ///     should be disposed to release internal resources of <see cref="IAudioPlayer" />.
+    /// </remarks>
     public interface IPlayback : IDisposable
     {
+        /// <summary>
+        ///     Indicates if <see cref="IPlayback" /> is in playing state and corresponding sound is being played.
+        /// </summary>
         bool IsPlaying { get; }
 
+        /// <summary>
+        ///     Invoked when <see cref="IPlayback" /> is being stopped.
+        /// </summary>
         event EventHandler Stopped;
+
+        /// <summary>
+        ///     Invoked when <see cref="IPlayback" /> is being disposed.
+        /// </summary>
         event EventHandler Disposed;
 
+        /// <summary>
+        ///     Starts playback of sound from the current position.
+        /// </summary>
+        /// <remarks>Calling this method sets <see cref="IsPlaying" /> to <c>true</c>.</remarks>
         void Play();
+
+        /// <summary>
+        ///     Pauses playback of sound and keeps current position.
+        /// </summary>
+        /// <remarks>Calling this method sets <see cref="IsPlaying" /> to <c>false</c>.</remarks>
         void Pause();
+
+        /// <summary>
+        ///     Stops playback of sound and sets current position to the beginning.
+        /// </summary>
+        /// <remarks>Calling this method sets <see cref="IsPlaying" /> to <c>false</c>.</remarks>
         void Stop();
     }
 }
