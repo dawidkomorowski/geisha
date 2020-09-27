a8fb8b2bceccf164fd798fdc133057de50652949
Dawid Komorowski <dawidkomorowski@outlook.com>
2020-09-27 18:05:48 +0200

    Created SpriteAnimation class that allow to specify list of sprites as animation frames.

---
+++ src/Geisha.Engine/Animation/SpriteAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Geisha.Engine.Rendering;
+
+namespace Geisha.Engine.Animation
+{
+    public sealed class SpriteAnimation
+    {
+        public SpriteAnimation(IEnumerable<SpriteAnimationFrame> frames)
+        {
+            Frames = frames.ToList().AsReadOnly();
+        }
+
+        public IReadOnlyList<SpriteAnimationFrame> Frames { get; }
+    }
+
+    public sealed class SpriteAnimationFrame
+    {
+        public SpriteAnimationFrame(Sprite sprite)
+        {
+            Sprite = sprite;
+        }
+
+        public Sprite Sprite { get; }
+    }
+}
