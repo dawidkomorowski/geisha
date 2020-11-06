ca6c7642c8726be6a7963f781310da431fa4515f
Dawid Komorowski <dawidkomorowski@outlook.com>
2020-11-06 21:27:57 +0100

    TODO converted into issue #245.

--- src/Geisha.Engine/Core/Systems/EntityDestructionSystem.cs
+++ src/Geisha.Engine/Core/Systems/EntityDestructionSystem.cs
@@ -3,8 +3,6 @@ using Geisha.Engine.Core.SceneModel;
 
 namespace Geisha.Engine.Core.Systems
 {
-    // TODO It seems inefficient. If final destruction logic would be part of Scene itself then it could be optimized.
-    // TODO Lists for entities scheduled for destruction could be internally maintained.
     internal class EntityDestructionSystem : IEntityDestructionSystem
     {
         public void DestroyEntitiesAfterFixedTimeStep(Scene scene)
