39d7473c2530653eac6d454494f56da2cab52362
Dawid Komorowski <dawidkomorowski@outlook.com>
2021-02-21 14:17:46 +0100

    Implemented StartUpSceneBehavior logic.

--- test/Geisha.Engine.UnitTests/Core/StartUpTasks/LoadStartUpSceneStartUpTaskTests.cs
+++ test/Geisha.Engine.UnitTests/Core/StartUpTasks/LoadStartUpSceneStartUpTaskTests.cs
@@ -10,7 +10,7 @@ namespace Geisha.Engine.UnitTests.Core.StartUpTasks
     public class LoadStartUpSceneStartUpTaskTests
     {
         [Test]
-        public void Run_ShouldLoadSceneFromStartUpScenePathInConfiguration()
+        public void Run_ShouldLoadSceneFromStartUpScenePathInConfiguration_WhenStartUpSceneIsNonEmpty()
         {
             // Arrange
             const string startUpScene = "start up scene";
@@ -25,6 +25,24 @@ namespace Geisha.Engine.UnitTests.Core.StartUpTasks
 
             // Assert
             sceneManager.Received().LoadScene(startUpScene);
+        }
+
+        [Test]
+        public void Run_ShouldLoadEmptySceneWithSceneBehaviorFromStartUpSceneBehaviorInConfiguration_WhenStartUpSceneIsEmpty()
+        {
+            // Arrange
+            const string startUpSceneBehavior = "start up scene behavior";
+
+            var sceneManager = Substitute.For<ISceneManager>();
+            var coreConfiguration = CoreConfiguration.CreateBuilder().WithStartUpSceneBehavior(startUpSceneBehavior).Build();
+
+            var startUpTask = new LoadStartUpSceneStartUpTask(sceneManager, coreConfiguration);
+
+            // Act
+            startUpTask.Run();
+
+            // Assert
+            sceneManager.Received().LoadEmptyScene(startUpSceneBehavior);
         }
     }
 }
