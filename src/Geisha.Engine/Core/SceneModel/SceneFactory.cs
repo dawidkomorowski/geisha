91bc64c072b25a21719b1e98cda06d7d6c49323e
Dawid Komorowski <dawidkomorowski@outlook.com>
2021-03-02 00:31:08 +0100

    Added documentation to ISceneFactory.

--- src/Geisha.Engine/Core/SceneModel/SceneFactory.cs
+++ src/Geisha.Engine/Core/SceneModel/SceneFactory.cs
@@ -2,8 +2,15 @@
 
 namespace Geisha.Engine.Core.SceneModel
 {
+    /// <summary>
+    /// Factory creating new instances of empty <see cref="Scene"/>.
+    /// </summary>
     public interface ISceneFactory
     {
+        /// <summary>
+        /// Creates new instance of empty <see cref="Scene"/>.
+        /// </summary>
+        /// <returns>New instance of empty <see cref="Scene"/>.</returns>
         public Scene Create();
     }
 
