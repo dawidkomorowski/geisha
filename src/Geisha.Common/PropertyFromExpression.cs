a53404d8ead9d38b54230ae4611609fb5bd461d5
Dawid Komorowski <dawidkomorowski@outlook.com>
2021-03-19 23:20:21 +0100

    Add documentation to PropertyFromExpression class.

--- src/Geisha.Common/PropertyFromExpression.cs
+++ src/Geisha.Common/PropertyFromExpression.cs
@@ -4,8 +4,18 @@ using System.Reflection;
 
 namespace Geisha.Common
 {
+    /// <summary>
+    ///     Utility class for inspecting property in property expression.
+    /// </summary>
     public static class PropertyFromExpression
     {
+        /// <summary>
+        ///     Gets name of property selected in <paramref name="propertyExpression" />.
+        /// </summary>
+        /// <param name="propertyExpression">Expression selecting property.</param>
+        /// <typeparam name="T">Type of object from which property is to be selected.</typeparam>
+        /// <typeparam name="TProperty">Type of property.</typeparam>
+        /// <returns>Name of selected property.</returns>
         public static string GetPropertyName<T, TProperty>(Expression<Func<T, TProperty>> propertyExpression)
         {
             return propertyExpression.Body switch
