5f94bfefe27f5f716b8f83186672302aea2e464b
Dawid Komorowski <dawidkomorowski@outlook.com>
2021-05-04 17:35:24 +0200

    Implement serialization and deserialization of Scene.SceneBehavior.

--- src/Geisha.Engine/Core/SceneModel/Serialization/SceneSerializer.cs
+++ src/Geisha.Engine/Core/SceneModel/Serialization/SceneSerializer.cs
@@ -1,4 +1,7 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.Json;
 
 namespace Geisha.Engine.Core.SceneModel.Serialization
 {
@@ -12,6 +15,11 @@ namespace Geisha.Engine.Core.SceneModel.Serialization
 
     internal sealed class SceneSerializer : ISceneSerializer
     {
+        private static class PropertyName
+        {
+            public const string SceneBehaviorName = "SceneBehaviorName";
+        }
+
         private readonly ISceneFactory _sceneFactory;
         private readonly ISceneBehaviorFactoryProvider _sceneBehaviorFactoryProvider;
 
@@ -23,24 +31,43 @@ namespace Geisha.Engine.Core.SceneModel.Serialization
 
         public void Serialize(Scene scene, Stream stream)
         {
+            using var jsonWriter = new Utf8JsonWriter(stream);
+            jsonWriter.WriteStartObject();
+
+            jsonWriter.WriteString(PropertyName.SceneBehaviorName, scene.SceneBehavior.Name);
+
+            jsonWriter.WriteEndObject();
+            jsonWriter.Flush();
         }
 
         public string Serialize(Scene scene)
         {
-            return string.Empty;
+            using var memoryStream = new MemoryStream();
+            Serialize(scene, memoryStream);
+            return Encoding.UTF8.GetString(memoryStream.ToArray());
         }
 
         public Scene Deserialize(Stream stream)
         {
-            var scene = _sceneFactory.Create();
-            scene.SceneBehavior = _sceneBehaviorFactoryProvider.Get(string.Empty).Create(scene);
-            return scene;
+            using var jsonDocument = JsonDocument.Parse(stream);
+            return DeserializeInternal(jsonDocument);
         }
 
         public Scene Deserialize(string json)
         {
+            using var jsonDocument = JsonDocument.Parse(json);
+            return DeserializeInternal(jsonDocument);
+        }
+
+        private Scene DeserializeInternal(JsonDocument jsonDocument)
+        {
+            var rootElement = jsonDocument.RootElement;
             var scene = _sceneFactory.Create();
-            scene.SceneBehavior = _sceneBehaviorFactoryProvider.Get(string.Empty).Create(scene);
+
+            var sceneBehaviorName = rootElement.GetProperty(PropertyName.SceneBehaviorName).GetString() ??
+                                    throw new InvalidOperationException($"Cannot deserialize scene. {PropertyName.SceneBehaviorName} property cannot be null.");
+            scene.SceneBehavior = _sceneBehaviorFactoryProvider.Get(sceneBehaviorName).Create(scene);
+
             return scene;
         }
     }
