5902846be07fc3f710449205f572713e75f91269
Dawid Komorowski <dawidkomorowski@outlook.com>
2021-07-03 20:22:15 +0200

    Create prototype of API for common asset file handling.

---
+++ src/Geisha.Engine/Core/Assets/AssetFile.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+
+namespace Geisha.Engine.Core.Assets
+{
+    public sealed class AssetFile
+    {
+        public static AssetFile Create(AssetId assetId, AssetType assetType, Stream binaryContent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static AssetFile Create(AssetId assetId, AssetType assetType, string stringContent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static AssetFile Create<T>(AssetId assetId, AssetType assetType, T jsonContent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static AssetFile Load(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static AssetFile Load(Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AssetId AssetId { get; }
+        public AssetType AssetType { get; }
+
+        public Stream ReadBinaryContent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ReadStringContent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T ReadJsonContent<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+
+        private AssetFile(AssetId assetId, AssetType assetType)
+        {
+            AssetId = assetId;
+            AssetType = assetType;
+        }
+    }
+}
---
+++ src/Geisha.Engine/Core/Assets/AssetType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Geisha.Engine.Core.Assets
+{
+    public readonly struct AssetType : IEquatable<AssetType>
+    {
+        private readonly string? _value;
+
+        public AssetType(string value)
+        {
+            _value = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public string Value => _value ?? string.Empty;
+
+        public override string ToString() => Value;
+
+        public bool Equals(AssetType other) => Value == other.Value;
+
+        public override bool Equals(object? obj) => obj is AssetType other && Equals(other);
+
+        public override int GetHashCode() => Value.GetHashCode();
+
+        public static bool operator ==(AssetType left, AssetType right) => left.Equals(right);
+
+        public static bool operator !=(AssetType left, AssetType right) => !left.Equals(right);
+    }
+}
