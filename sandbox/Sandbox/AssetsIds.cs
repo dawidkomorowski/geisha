4686d124b6734c1c981a7cff619d9109265ccc5b
Dawid Komorowski <dawidkomorowski@outlook.com>
2021-09-08 21:22:35 +0200

    Convert sandbox assets to new format.

--- sandbox/Sandbox/AssetsIds.cs
+++ sandbox/Sandbox/AssetsIds.cs
@@ -5,13 +5,13 @@ namespace Sandbox
 {
     public static class AssetsIds
     {
-        public static AssetId BoxSprite { get; } = new AssetId(new Guid("72D0650C-996F-4E61-904C-617E940326DE"));
-        public static AssetId CompassSprite { get; } = new AssetId(new Guid("09400BA1-A7AB-4752-ADC2-C6535898685C"));
+        public static AssetId BoxSprite { get; } = new AssetId(new Guid("56f5c040-dc60-46c7-b0ed-0353488d85f8"));
+        public static AssetId CompassSprite { get; } = new AssetId(new Guid("df819387-5162-4ce5-85be-68aa35ad0faf"));
 
         public static AssetId CampfireAnimation { get; } = new AssetId(new Guid("72B4483A-4E0E-45B9-BDD0-C4A265976CD8"));
 
-        public static AssetId MusicSound { get; } = new AssetId(new Guid("E23098D1-CE13-4C13-91E0-3CF545EFDFC2"));
-        public static AssetId SfxSound { get; } = new AssetId(new Guid("205F7A78-E8FA-49D5-BCF4-3174EBB728FF"));
+        public static AssetId MusicSound { get; } = new AssetId(new Guid("4f7b501d-2fd7-47f9-888a-5bc514a6ed9a"));
+        public static AssetId SfxSound { get; } = new AssetId(new Guid("ab1de87e-cad4-4ab8-8982-b33560faa76d"));
 
         public static AssetId PlayerInput { get; } = new AssetId(new Guid("4D5E957B-6176-4FFA-966D-5C3403909D9A"));
     }
