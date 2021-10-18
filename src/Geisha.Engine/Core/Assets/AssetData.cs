8828f647d0997f22d3e8ae53075d3cc135c62040
Dawid Komorowski <dawidkomorowski@outlook.com>
2021-10-18 02:45:45 +0200

    Document part of AssetData members.

--- src/Geisha.Engine/Core/Assets/AssetData.cs
+++ src/Geisha.Engine/Core/Assets/AssetData.cs
@@ -4,31 +4,93 @@ using System.Text.Json;
 
 namespace Geisha.Engine.Core.Assets
 {
+    // TODO Rename AssetContentType to AssetContentFormant and do the same for related names and existing asset files.
+    /// <summary>
+    ///     Represents an asset in the form of asset metadata and actual asset content. It also provides an API to load and
+    ///     save asset data to and from stream or file.
+    /// </summary>
     public sealed class AssetData
     {
-        private readonly Content _content;
-
+        /// <summary>
+        ///     Defines format of asset content stored in <see cref="AssetData" />.
+        /// </summary>
         public enum AssetContentType
         {
+            /// <summary>
+            ///     Specifies that format of asset content is binary.
+            /// </summary>
             Binary,
+
+            /// <summary>
+            ///     Specifies that format of asset content is <see cref="string" />.
+            /// </summary>
             String,
+
+            /// <summary>
+            ///     Specifies that format of asset content is JSON.
+            /// </summary>
             Json
         }
 
-        public static AssetData CreateWithBinaryContent(AssetId assetId, AssetType assetType, Stream content)
+        private readonly Content _content;
+
+        private AssetData(AssetId assetId, AssetType assetType, Content content)
         {
-            return new AssetData(assetId, assetType, new BinaryContent(content));
+            AssetId = assetId;
+            AssetType = assetType;
+            _content = content;
         }
 
-        public static AssetData CreateWithStringContent(AssetId assetId, AssetType assetType, string content)
-        {
-            return new AssetData(assetId, assetType, new StringContent(content));
-        }
+        /// <summary>
+        ///     Gets <see cref="Assets.AssetId" /> of this <see cref="AssetData" />.
+        /// </summary>
+        public AssetId AssetId { get; }
 
-        public static AssetData CreateWithJsonContent<T>(AssetId assetId, AssetType assetType, T content) where T : notnull
-        {
-            return new AssetData(assetId, assetType, new JsonContent(content));
-        }
+        /// <summary>
+        ///     Gets <see cref="Assets.AssetType" /> of this <see cref="AssetData" />.
+        /// </summary>
+        public AssetType AssetType { get; }
+
+        /// <summary>
+        ///     Gets <see cref="AssetContentType" /> of this <see cref="AssetData" />.
+        /// </summary>
+        public AssetContentType ContentType => _content.Type;
+
+        /// <summary>
+        ///     Creates new instance of <see cref="AssetData" /> with specified <see cref="Assets.AssetId" />,
+        ///     <see cref="Assets.AssetType" /> and content in binary format.
+        /// </summary>
+        /// <param name="assetId"><see cref="Assets.AssetId" /> of <see cref="AssetData" />.</param>
+        /// <param name="assetType"><see cref="Assets.AssetType" /> of <see cref="AssetData" />.</param>
+        /// <param name="content"><see cref="Stream" /> to read from the binary content of <see cref="AssetData" />.</param>
+        /// <returns>New instance of <see cref="AssetData" /> with binary content.</returns>
+        public static AssetData CreateWithBinaryContent(AssetId assetId, AssetType assetType, Stream content) =>
+            new AssetData(assetId, assetType, new BinaryContent(content));
+
+        /// <summary>
+        ///     Creates new instance of <see cref="AssetData" /> with specified <see cref="Assets.AssetId" />,
+        ///     <see cref="Assets.AssetType" /> and content in <see cref="string" /> format.
+        /// </summary>
+        /// <param name="assetId"><see cref="Assets.AssetId" /> of <see cref="AssetData" />.</param>
+        /// <param name="assetType"><see cref="Assets.AssetType" /> of <see cref="AssetData" />.</param>
+        /// <param name="content"><see cref="string" /> to be used as content of <see cref="AssetData" />.</param>
+        /// <returns>New instance of <see cref="AssetData" /> with <see cref="string" /> content.</returns>
+        public static AssetData CreateWithStringContent(AssetId assetId, AssetType assetType, string content) =>
+            new AssetData(assetId, assetType, new StringContent(content));
+
+        /// <summary>
+        ///     Creates new instance of <see cref="AssetData" /> with specified <see cref="Assets.AssetId" />,
+        ///     <see cref="Assets.AssetType" /> and content in JSON format.
+        /// </summary>
+        /// <param name="assetId"><see cref="Assets.AssetId" /> of <see cref="AssetData" />.</param>
+        /// <param name="assetType"><see cref="Assets.AssetType" /> of <see cref="AssetData" />.</param>
+        /// <param name="content">
+        ///     Instance of <typeparamref name="T" /> to be serialized as JSON content of
+        ///     <see cref="AssetData" />.
+        /// </param>
+        /// <returns>New instance of <see cref="AssetData" /> with <see cref="string" /> content.</returns>
+        public static AssetData CreateWithJsonContent<T>(AssetId assetId, AssetType assetType, T content) where T : notnull =>
+            new AssetData(assetId, assetType, new JsonContent(content));
 
         public static AssetData Load(string filePath)
         {
@@ -57,24 +119,11 @@ namespace Geisha.Engine.Core.Assets
             return new AssetData(assetId, assetType, content);
         }
 
-        public AssetId AssetId { get; }
-        public AssetType AssetType { get; }
-        public AssetContentType ContentType => _content.Type;
+        public Stream ReadBinaryContent() => _content.ReadBinary();
 
-        public Stream ReadBinaryContent()
-        {
-            return _content.ReadBinary();
-        }
+        public string ReadStringContent() => _content.ReadString();
 
-        public string ReadStringContent()
-        {
-            return _content.ReadString();
-        }
-
-        public T ReadJsonContent<T>() where T : notnull
-        {
-            return _content.ReadJson<T>();
-        }
+        public T ReadJsonContent<T>() where T : notnull => _content.ReadJson<T>();
 
         public void Save(string filePath)
         {
@@ -92,13 +141,6 @@ namespace Geisha.Engine.Core.Assets
             jsonWriter.WritePropertyName("Content");
             _content.WriteTo(jsonWriter);
             jsonWriter.WriteEndObject();
-        }
-
-        private AssetData(AssetId assetId, AssetType assetType, Content content)
-        {
-            AssetId = assetId;
-            AssetType = assetType;
-            _content = content;
         }
 
         private abstract class Content
