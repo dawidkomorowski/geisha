dcf86e0a76d85a14ac33277421a262ead8915324
Dawid Komorowski <dawidkomorowski@outlook.com>
2021-10-24 15:05:46 +0200

    Create project for Geisha CLI tool.

---
+++ src/Geisha.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Geisha.Cli
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
