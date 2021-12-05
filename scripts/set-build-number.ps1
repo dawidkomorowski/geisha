18f0eaeaa8cc7bd0d16e317f88bd90d5b1d71571
Dawid Komorowski <dawidkomorowski@outlook.com>
2021-12-05 18:32:31 +0100

    Simplify setting version of published packages.

--- scripts/set-build-number.ps1
+++
@@ -1,8 +0,0 @@
-Set-Location -Path $PSScriptRoot
-$ErrorActionPreference = "Stop"
-$buildNumber = $args[0]
-
-$projectVersion = Get-Content -Path "..\project.version" -Raw
-$versionToSet = "$projectVersion+$buildNumber"
-
-.\set-version.ps1 $versionToSet
