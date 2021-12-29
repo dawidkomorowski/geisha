a6c83e119212f3a373d13564370fa3c664a523d2
Dawid Komorowski <dawidkomorowski@outlook.com>
2021-12-29 18:34:05 +0100

    Check if markdown is supported in github actions annotations.

--- scripts/publish-benchmark-results.ps1
+++ scripts/publish-benchmark-results.ps1
@@ -4,7 +4,7 @@ $ErrorActionPreference = "Stop"
 $rawResults = Get-Content -Path "..\benchmark\Benchmark\bin\Release\netcoreapp3.1\BenchmarkResults-*" -Raw
 $jsonResults = ConvertFrom-Json -InputObject $rawResults
 
-$markdownOutput = "| Benchmark | Fixed frames | Frames |$([Environment]::NewLine)"
+$markdownOutput = "# ~~asd~~ | Benchmark | Fixed frames | Frames |$([Environment]::NewLine)"
 $markdownOutput = "$markdownOutput :- | -: | -:$([Environment]::NewLine)" 
 
 foreach($result in $jsonResults) {
