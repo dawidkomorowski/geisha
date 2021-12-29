341c66151a597be10a2b8533581b6111b410f62b
Dawid Komorowski <dawidkomorowski@outlook.com>
2021-12-29 19:14:42 +0100

    Test long line reported as github actions annotation.

--- scripts/publish-benchmark-results.ps1
+++ scripts/publish-benchmark-results.ps1
@@ -4,7 +4,7 @@ $ErrorActionPreference = "Stop"
 $rawResults = Get-Content -Path "..\benchmark\Benchmark\bin\Release\netcoreapp3.1\BenchmarkResults-*" -Raw
 $jsonResults = ConvertFrom-Json -InputObject $rawResults
 
-$markdownOutput = "# ~~asd~~ | Benchmark | Fixed frames | Frames |$([Environment]::NewLine)"
+$markdownOutput = "# ~~asd~~ | Benchmark | Fixed frames | Frames |..\benchmark\Benchmark\bin\Release\netcoreapp3.1\BenchmarkResults-*..\benchmark\Benchmark\bin\Release\netcoreapp3.1\BenchmarkResults-*..\benchmark\Benchmark\bin\Release\netcoreapp3.1\BenchmarkResults-*..\benchmark\Benchmark\bin\Release\netcoreapp3.1\BenchmarkResults-*..\benchmark\Benchmark\bin\Release\netcoreapp3.1\BenchmarkResults-*..\benchmark\Benchmark\bin\Release\netcoreapp3.1\BenchmarkResults-*..\benchmark\Benchmark\bin\Release\netcoreapp3.1\BenchmarkResults-*..\benchmark\Benchmark\bin\Release\netcoreapp3.1\BenchmarkResults-*..\benchmark\Benchmark\bin\Release\netcoreapp3.1\BenchmarkResults-*..\benchmark\Benchmark\bin\Release\netcoreapp3.1\BenchmarkResults-*"
 $markdownOutput = "$markdownOutput :- | -: | -:$([Environment]::NewLine)" 
 
 foreach($result in $jsonResults) {
