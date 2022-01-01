313f5b47d16cc7c554404b27e7cd352e81639c82
Dawid Komorowski <dawidkomorowski@outlook.com>
2022-01-01 16:27:25 +0100

    Try publishing benchmark results as github check run.

--- scripts/publish-benchmark-results.ps1
+++ scripts/publish-benchmark-results.ps1
@@ -1,6 +1,20 @@
 Set-Location -Path $PSScriptRoot
 $ErrorActionPreference = "Stop"
 
+if($args[0]) {
+    $headSha = $args[0]
+}
+else {
+    throw "Missing argument: commit SHA."
+}
+
+if($args[1]) {
+    $githubToken = $args[1]
+}
+else {
+    throw "Missing argument: github token."
+}
+
 $rawResults = Get-Content -Path "..\benchmark\Benchmark\bin\Release\netcoreapp3.1\BenchmarkResults--*" -Raw
 $jsonResults = ConvertFrom-Json -InputObject $rawResults
 
@@ -11,3 +25,21 @@ foreach($result in $jsonResults) {
 }
 
 Write-Host "::notice title=Performance Benchmark Results::$markdownOutput"
+
+$url = "https://api.github.com/repos/dawidkomorowski/geisha/check-runs"
+$headers = @{
+    Accept = 'application/vnd.github.antiope-preview+json'
+    Authorization = "token $githubToken"
+}
+$body = @{
+    name       = "Benchmark Results"
+    head_sha   = $headSha
+    status     = "completed"
+    conclusion = "success"
+    output     = @{
+        title   = "Benchmark Results"
+        summary = "This run completed at ``$([datetime]::Now)``"
+        text    = "Markdown tests ___XYZ___"
+    }
+}
+Invoke-WebRequest -Headers $headers $url -Method Post -Body ($body | ConvertTo-Json)
