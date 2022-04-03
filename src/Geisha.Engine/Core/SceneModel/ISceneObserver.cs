6923d130fc54dc7ece251cb0f333d2c47e695d18
Dawid Komorowski <dawidkomorowski@outlook.com>
2022-04-04 01:07:56 +0200

    Create ISceneObserver interface.

---
+++ src/Geisha.Engine/Core/SceneModel/ISceneObserver.cs
@@ -0,0 +1,12 @@
+﻿namespace Geisha.Engine.Core.SceneModel
+{
+    // TODO Add documentation.
+    public interface ISceneObserver
+    {
+        void OnEntityCreated(Entity entity);
+        void OnEntityRemoved(Entity entity);
+        void OnEntityParentChanged(Entity entity, Entity? oldParent, Entity? newParent);
+        void OnComponentCreated(Component component);
+        void OnComponentRemoved(Component component);
+    }
+}
