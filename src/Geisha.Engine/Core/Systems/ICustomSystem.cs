c0ac2c69beff1d5cb30c0912f7e6e4f0568f1556
Dawid Komorowski <dawidkomorowski@outlook.com>
2022-05-08 19:55:18 +0200

    Update documentation of ICustomSystem.

--- src/Geisha.Engine/Core/Systems/ICustomSystem.cs
+++ src/Geisha.Engine/Core/Systems/ICustomSystem.cs
@@ -2,15 +2,23 @@
 
 namespace Geisha.Engine.Core.Systems
 {
-    // TODO Review the documentation in this file.
     /// <summary>
     ///     <see cref="ICustomSystem" /> interface defines API for implementing custom game systems that are run by the engine
     ///     in the game loop.
     /// </summary>
     /// <remarks>
     ///     <p>
+    ///         <see cref="ICustomSystem" /> interface is a shorthand for implementing both <see cref="ICustomGameLoopStep" />
+    ///         and <see cref="ISceneObserver" /> as it is the most typical approach to implement custom system.
+    ///         <see cref="ICustomGameLoopStep" /> defines API for periodic updates in game loop while
+    ///         <see cref="ISceneObserver" /> defines API for watching scene structure.
+    ///     </p>
+    ///     <p>
     ///         To implement custom system create class implementing <see cref="ICustomSystem" /> interface and register it in
     ///         <see cref="IGame.RegisterComponents" /> using <see cref="IComponentsRegistry.RegisterSystem{TCustomSystem}" />.
+    ///         Implement <see cref="ISceneObserver" /> API to track entities and components relevant to the system. Implement
+    ///         <see cref="ICustomGameLoopStep" /> API to execute custom processing of entities and components relevant to the
+    ///         system.
     ///     </p>
     ///     <p>
     ///         Custom systems are executed in between main engine systems. First engine runs input and behavior systems, then
@@ -24,39 +32,39 @@ namespace Geisha.Engine.Core.Systems
     }
 
     /// <summary>
-    ///     <see cref="ICustomSystem" /> interface defines API for implementing custom game systems that are run by the engine
-    ///     in the game loop.
+    ///     <see cref="ICustomGameLoopStep" /> interface defines API for implementing custom game loop steps that are run by
+    ///     the engine in the game loop.
     /// </summary>
     /// <remarks>
     ///     <p>
-    ///         To implement custom system create class implementing <see cref="ICustomSystem" /> interface and register it in
-    ///         <see cref="IGame.RegisterComponents" /> using <see cref="IComponentsRegistry.RegisterSystem{TCustomSystem}" />.
+    ///         To implement custom game loop step create class implementing <see cref="ICustomGameLoopStep" /> interface and
+    ///         register it in <see cref="IGame.RegisterComponents" />.
     ///     </p>
     ///     <p>
-    ///         Custom systems are executed in between main engine systems. First engine runs input and behavior systems, then
-    ///         custom systems are run, finally rest of main engine systems are run (physics, rendering, etc.). Exact execution
-    ///         order depends on which processing method is used. Fixed time step processing is done before variable time step
-    ///         processing.
+    ///         Custom game loop steps are executed in between main engine systems. First the engine runs input and behavior
+    ///         systems, then custom game loop steps are run, finally rest of main engine systems are run (physics, rendering,
+    ///         etc.). Exact execution order depends on which processing method is used. Fixed time step processing is done
+    ///         before variable time step processing.
     ///     </p>
     /// </remarks>
     public interface ICustomGameLoopStep
     {
         /// <summary>
-        ///     Name of custom system. It is used to uniquely identify custom system in configuration.
+        ///     Name of custom game loop step. It is used to uniquely identify custom game loop step in configuration.
         /// </summary>
         string Name { get; }
 
         /// <summary>
-        ///     Implement system processing logic in this method. This method is executed at constant rate of fixed updates per
-        ///     second. This method may be executed zero, one or multiple times per frame depending on how long time elapsed since
-        ///     previous frame. This method is suitable for implementing logic that requires determinism, stability and strong
-        ///     frame rate independence.
+        ///     Implement game loop step processing logic in this method. This method is executed at constant rate of fixed updates
+        ///     per second. This method may be executed zero, one or multiple times per frame depending on how long time elapsed
+        ///     since previous frame. This method is suitable for implementing logic that requires determinism, stability and
+        ///     strong frame rate independence.
         /// </summary>
         void ProcessFixedUpdate();
 
         /// <summary>
-        ///     Implement system processing logic in this method. This method is executed at variable rate of updates per second as
-        ///     it is directly tied to frame rate. It is executed exactly once per frame.
+        ///     Implement game loop step processing logic in this method. This method is executed at variable rate of updates per
+        ///     second as it is directly tied to frame rate. It is executed exactly once per frame.
         /// </summary>
         /// <param name="gameTime">Game time elapsed since previous frame.</param>
         void ProcessUpdate(GameTime gameTime);
