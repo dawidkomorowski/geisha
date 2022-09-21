9ae5604404864d45fdfea977fb640686bef0ead8
Dawid Komorowski <dawidkomorowski@outlook.com>
2022-09-21 21:46:31 +0200

    Move InternalsVisibleTo configuration to .csproj.

--- src/Geisha.Engine.Audio.CSCore/InternalsVisibleToConfiguration.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Geisha.Engine.Audio.CSCore.UnitTests")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
--- src/Geisha.Engine/InternalsVisibleToConfiguration.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Geisha.Engine.UnitTests")]
-[assembly: InternalsVisibleTo("Geisha.Engine.IntegrationTests")]
-[assembly: InternalsVisibleTo("Geisha.TestUtils")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
