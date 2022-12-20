558f65c60f8cb3a84fe89a7a293d976f3c6451b9
Dawid Komorowski <dawidkomorowski@outlook.com>
2022-12-20 22:47:17 +0100

    Implement Hello screen

--- demo/Geisha.Demo/Screens/Hello/HelloSceneBehaviorFactory.cs
+++ demo/Geisha.Demo/Screens/Hello/HelloSceneBehaviorFactory.cs
@@ -1,5 +1,9 @@
 using Geisha.Demo.Common;
+using Geisha.Engine.Core.Components;
+using Geisha.Engine.Core.Math;
 using Geisha.Engine.Core.SceneModel;
+using Geisha.Engine.Rendering;
+using Geisha.Engine.Rendering.Components;
 
 namespace Geisha.Demo.Screens.Hello
 {
@@ -29,7 +33,48 @@ namespace Geisha.Demo.Screens.Hello
 
             protected override void OnLoaded()
             {
-                _commonScreenFactory.CreateCommonScreen(Scene, "https://github.com/dawidkomorowski/geisha/blob/master/src/Geisha.Engine/Engine.cs");
+                // Create background and menu.
+                _commonScreenFactory.CreateCommonScreen(Scene,
+                    "https://github.com/dawidkomorowski/geisha/blob/master/demo/Geisha.Demo/Screens/Hello/HelloSceneBehaviorFactory.cs");
+
+                // Create entity representing first line of text.
+                var line1 = Scene.CreateEntity();
+                // Add Transform2DComponent to entity.
+                var line1Transform = line1.CreateComponent<Transform2DComponent>();
+                // Set position of the entity.
+                line1Transform.Translation = new Vector2(-250, 150);
+                // Add TextRendererComponent to entity.
+                var line1TextRenderer = line1.CreateComponent<TextRendererComponent>();
+                // Set text properties.
+                line1TextRenderer.Color = Color.FromArgb(255, 0, 0, 0);
+                line1TextRenderer.FontSize = FontSize.FromDips(40);
+                line1TextRenderer.Text = "Welcome to Geisha Engine.";
+
+                // Create entity representing second line of text.
+                var line2 = Scene.CreateEntity();
+                // Add Transform2DComponent to entity.
+                var line2Transform = line2.CreateComponent<Transform2DComponent>();
+                // Set position of the entity.
+                line2Transform.Translation = new Vector2(-750, 100);
+                // Add TextRendererComponent to entity.
+                var line2TextRenderer = line2.CreateComponent<TextRendererComponent>();
+                // Set text properties.
+                line2TextRenderer.Color = Color.FromArgb(255, 0, 0, 0);
+                line2TextRenderer.FontSize = FontSize.FromDips(40);
+                line2TextRenderer.Text = "This is a quick demo of engine features and what can be done with it.";
+
+                // Create entity representing third line of text.
+                var line3 = Scene.CreateEntity();
+                // Add Transform2DComponent to entity.
+                var line3Transform = line3.CreateComponent<Transform2DComponent>();
+                // Set position of the entity.
+                line3Transform.Translation = new Vector2(-400, 0);
+                // Add TextRendererComponent to entity.
+                var line3TextRenderer = line3.CreateComponent<TextRendererComponent>();
+                // Set text properties.
+                line3TextRenderer.Color = Color.FromArgb(255, 0, 0, 0);
+                line3TextRenderer.FontSize = FontSize.FromDips(40);
+                line3TextRenderer.Text = "Press [ENTER] to go to the next screen.";
             }
         }
     }
