7895cfeeb509ac75021b40bd44079f852060a958
Dawid Komorowski <dawidkomorowski@outlook.com>
2023-03-19 21:19:43 +0100

    Add documentation for CoroutineState, CoroutineUpdateMode and CoroutineInstruction

--- src/Geisha.Engine/Core/Coroutines/CoroutineState.cs
+++ src/Geisha.Engine/Core/Coroutines/CoroutineState.cs
@@ -1,11 +1,33 @@
 ﻿namespace Geisha.Engine.Core.Coroutines
 {
+    /// <summary>
+    ///     Specifies state of <see cref="Coroutine" />.
+    /// </summary>
     public enum CoroutineState
     {
+        /// <summary>
+        ///     <see cref="Coroutine" /> is ready to be started.
+        /// </summary>
         Pending,
+
+        /// <summary>
+        ///     <see cref="Coroutine" /> is running.
+        /// </summary>
         Running,
+
+        /// <summary>
+        ///     <see cref="Coroutine" /> is paused.
+        /// </summary>
         Paused,
+
+        /// <summary>
+        ///     <see cref="Coroutine" /> has completed.
+        /// </summary>
         Completed,
+
+        /// <summary>
+        ///     <see cref="Coroutine" /> was aborted.
+        /// </summary>
         Aborted
     }
 }
--- src/Geisha.Engine/Core/Coroutines/CoroutineUpdateMode.cs
+++ src/Geisha.Engine/Core/Coroutines/CoroutineUpdateMode.cs
@@ -1,8 +1,18 @@
 ﻿namespace Geisha.Engine.Core.Coroutines
 {
+    /// <summary>
+    ///     Specifies update mode of <see cref="Coroutine" />.
+    /// </summary>
     public enum CoroutineUpdateMode
     {
+        /// <summary>
+        ///     <see cref="Coroutine" /> is executed as part of fixed time step update in game loop.
+        /// </summary>
         FixedTimeStep,
+
+        /// <summary>
+        ///     <see cref="Coroutine" /> is executed as part of variable time step update in game loop.
+        /// </summary>
         VariableTimeStep
     }
 }
