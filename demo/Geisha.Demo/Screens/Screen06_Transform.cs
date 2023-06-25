228daf8eaa655471b8f1b87b8216cb03fa93c6b1
Dawid Komorowski <dawidkomorowski@outlook.com>
2023-06-26 00:54:51 +0200

    Create placeholder for transform demo screen

---
+++ demo/Geisha.Demo/Screens/Screen06_Transform.cs
@@ -0,0 +1,68 @@
+﻿using Geisha.Demo.Common;
+using Geisha.Engine.Core.Components;
+using Geisha.Engine.Core.Math;
+using Geisha.Engine.Core.SceneModel;
+using Geisha.Engine.Rendering;
+using Geisha.Engine.Rendering.Components;
+
+namespace Geisha.Demo.Screens
+{
+    internal sealed class TransformSceneBehaviorFactory : ISceneBehaviorFactory
+    {
+        private const string SceneBehaviorName = "Screen06_Transform";
+        private readonly CommonScreenFactory _commonScreenFactory;
+
+        public TransformSceneBehaviorFactory(CommonScreenFactory commonScreenFactory)
+        {
+            _commonScreenFactory = commonScreenFactory;
+        }
+
+        public string BehaviorName => SceneBehaviorName;
+        public SceneBehavior Create(Scene scene) => new TransformSceneBehavior(scene, _commonScreenFactory);
+
+        private sealed class TransformSceneBehavior : SceneBehavior
+        {
+            private readonly CommonScreenFactory _commonScreenFactory;
+
+            public TransformSceneBehavior(Scene scene, CommonScreenFactory commonScreenFactory) : base(scene)
+            {
+                _commonScreenFactory = commonScreenFactory;
+            }
+
+            public override string Name => SceneBehaviorName;
+
+            protected override void OnLoaded()
+            {
+                // Create background and menu.
+                _commonScreenFactory.CreateBackgroundAndMenu(Scene);
+
+                // Create entity representing camera.
+                var camera = Scene.CreateEntity();
+                // Add Transform2DComponent to entity to set position of the camera at origin.
+                camera.CreateComponent<Transform2DComponent>();
+                // Add CameraComponent to entity so we can control what is visible on the screen.
+                var cameraComponent = camera.CreateComponent<CameraComponent>();
+                // Set size of the camera to be 1600x900 units - in this case it corresponds to widow size in pixels.
+                cameraComponent.ViewRectangle = new Vector2(1600, 900);
+
+                // Create entity representing first text block.
+                var textBlock1 = Scene.CreateEntity();
+                // Add Transform2DComponent to entity so we can control its position.
+                var textBlock1Transform = textBlock1.CreateComponent<Transform2DComponent>();
+                // Set position of the entity.
+                textBlock1Transform.Translation = new Vector2(0, 250);
+                // Add TextRendererComponent to entity so it can show text on the screen.
+                var textRenderer1 = textBlock1.CreateComponent<TextRendererComponent>();
+                // Set text properties.
+                textRenderer1.Color = Color.Black;
+                textRenderer1.FontSize = FontSize.FromDips(40);
+                textRenderer1.TextAlignment = TextAlignment.Center;
+                textRenderer1.ParagraphAlignment = ParagraphAlignment.Center;
+                textRenderer1.MaxWidth = 1600;
+                textRenderer1.MaxHeight = 900;
+                textRenderer1.Pivot = new Vector2(800, 450);
+                textRenderer1.Text = "Geisha Engine provides transform components for controlling position, rotation and scale of entities.";
+            }
+        }
+    }
+}
