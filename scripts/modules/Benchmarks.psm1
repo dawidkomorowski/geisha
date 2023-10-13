71f0b0c30e39b903a8c8c4c5a66076fb4c3c9cf4
Dawid Komorowski <dawidkomorowski@outlook.com>
2023-10-13 02:28:13 +0200

    Expanding the benchmarks script

--- scripts/modules/Benchmarks.psm1
+++ scripts/modules/Benchmarks.psm1
@@ -1,9 +1,10 @@
-function Get-BenchmarkAppFromGH {
-    $artifactName = "benchmark-app"
-    $artifactsJson = Invoke-WebRequest -Uri "https://api.github.com/repos/dawidkomorowski/geisha/actions/artifacts?name=$artifactName"
-    $artifacts = ConvertFrom-Json $artifactsJson
+function Get-LinkToArtifactDownloadPage {
+    $workflowName = "Build/Test/Publish"
+    
+    $workflowsJson = Invoke-WebRequest -Uri "https://api.github.com/repos/dawidkomorowski/geisha/actions/runs?branch=master"
+    $workflows = ConvertFrom-Json $workflowsJson
 
-    $artifact = $artifacts.artifacts | Where-Object { $_.workflow_run.head_branch -EQ "master" } | Select-Object -First 1
+    $workflow = $workflows.workflow_runs | Where-Object { $_.Name -EQ $workflowName } | Select-Object -First 1
 
-    Write-Host "Downloading '$artifactName' from latest '$($artifact.workflow_run.head_branch)' build SHA '$($artifact.workflow_run.head_sha)'"
+    return $workflow.html_url
 }
--- scripts/run-benchmark-current-vs-master.ps1
+++ scripts/run-benchmark-current-vs-master.ps1
@@ -3,5 +3,21 @@ $ErrorActionPreference = "Stop"
 
 Import-Module -Name .\modules\Benchmarks.psm1 -Force
 
-Write-Host "Test"
-Get-BenchmarkAppFromGH
+$binPath = "bin"
+$benchmarkAppPackageName = "benchmark-app.zip"
+
+New-Item -ItemType Directory -Path $binPath -Force | Out-Null
+
+if (-not (Test-Path -Path "$binPath\$benchmarkAppPackageName")) {
+    Write-Warning "Benchmark app package is missing."
+
+    $downloadPageLink = Get-LinkToArtifactDownloadPage
+    Write-Host "Download '$benchmarkAppPackageName' from $downloadPageLink and place it in $(Resolve-Path -Path $binPath)"
+
+    Write-Warning "Skipping benchmarks execution."
+    return
+}
+
+if (-not (Test-Path -Path "$binPath\benchmark-app")) {
+    Expand-Archive -Path $benchmarkAppPackageName
+}
