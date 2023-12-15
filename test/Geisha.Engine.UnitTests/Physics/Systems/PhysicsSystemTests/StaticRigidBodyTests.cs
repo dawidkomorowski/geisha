0d8a78432e0985745db00e2c3d8b06c615337ab6
Dawid Komorowski <dawidkomorowski@outlook.com>
2023-12-15 01:47:19 +0100

    Add basic tests for static bodies

---
+++ test/Geisha.Engine.UnitTests/Physics/Systems/PhysicsSystemTests/StaticRigidBodyTests.cs
@@ -0,0 +1,83 @@
+﻿using Geisha.Engine.Physics.Components;
+using NUnit.Framework;
+
+namespace Geisha.Engine.UnitTests.Physics.Systems.PhysicsSystemTests;
+
+[TestFixture]
+public class StaticRigidBodyTests : PhysicsSystemTestsBase
+{
+    [Test]
+    public void ProcessPhysics_RectangleStaticBodyShouldNotCollideWithOtherRectangleStaticBody()
+    {
+        // Arrange
+        var physicsSystem = GetPhysicsSystem();
+        var staticBody1 = CreateRectangleStaticBody(0, 0, 10, 5);
+        var staticBody2 = CreateRectangleStaticBody(5, 0, 10, 5);
+
+        // Assume
+        Assume.That(staticBody1.GetComponent<RectangleColliderComponent>().IsColliding, Is.False);
+        Assume.That(staticBody2.GetComponent<RectangleColliderComponent>().IsColliding, Is.False);
+
+        // Act
+        physicsSystem.ProcessPhysics();
+
+        // Assert
+        var staticBody1Collider = staticBody1.GetComponent<RectangleColliderComponent>();
+        Assert.That(staticBody1Collider.IsColliding, Is.False);
+        Assert.That(staticBody1Collider.CollidingEntities, Has.Count.Zero);
+
+        var staticBody2Collider = staticBody2.GetComponent<RectangleColliderComponent>();
+        Assert.That(staticBody2Collider.IsColliding, Is.False);
+        Assert.That(staticBody2Collider.CollidingEntities, Has.Count.Zero);
+    }
+
+    [Test]
+    public void ProcessPhysics_CircleStaticBodyShouldNotCollideWithOtherCircleStaticBody()
+    {
+        // Arrange
+        var physicsSystem = GetPhysicsSystem();
+        var staticBody1 = CreateCircleStaticBody(0, 0, 10);
+        var staticBody2 = CreateCircleStaticBody(5, 0, 10);
+
+        // Assume
+        Assume.That(staticBody1.GetComponent<CircleColliderComponent>().IsColliding, Is.False);
+        Assume.That(staticBody2.GetComponent<CircleColliderComponent>().IsColliding, Is.False);
+
+        // Act
+        physicsSystem.ProcessPhysics();
+
+        // Assert
+        var staticBody1Collider = staticBody1.GetComponent<CircleColliderComponent>();
+        Assert.That(staticBody1Collider.IsColliding, Is.False);
+        Assert.That(staticBody1Collider.CollidingEntities, Has.Count.Zero);
+
+        var staticBody2Collider = staticBody2.GetComponent<CircleColliderComponent>();
+        Assert.That(staticBody2Collider.IsColliding, Is.False);
+        Assert.That(staticBody2Collider.CollidingEntities, Has.Count.Zero);
+    }
+
+    [Test]
+    public void ProcessPhysics_RectangleStaticBodyShouldNotCollideWithCircleStaticBody()
+    {
+        // Arrange
+        var physicsSystem = GetPhysicsSystem();
+        var staticBody1 = CreateRectangleStaticBody(0, 0, 10, 5);
+        var staticBody2 = CreateCircleStaticBody(5, 0, 10);
+
+        // Assume
+        Assume.That(staticBody1.GetComponent<RectangleColliderComponent>().IsColliding, Is.False);
+        Assume.That(staticBody2.GetComponent<CircleColliderComponent>().IsColliding, Is.False);
+
+        // Act
+        physicsSystem.ProcessPhysics();
+
+        // Assert
+        var staticBody1Collider = staticBody1.GetComponent<RectangleColliderComponent>();
+        Assert.That(staticBody1Collider.IsColliding, Is.False);
+        Assert.That(staticBody1Collider.CollidingEntities, Has.Count.Zero);
+
+        var staticBody2Collider = staticBody2.GetComponent<CircleColliderComponent>();
+        Assert.That(staticBody2Collider.IsColliding, Is.False);
+        Assert.That(staticBody2Collider.CollidingEntities, Has.Count.Zero);
+    }
+}
