d027f76a67bce8dbaaa2d03eed0527c0a8149e3d
Dawid Komorowski <dawidkomorowski@outlook.com>
2024-03-04 23:54:13 +0100

    Prototyping internal physics engine API

---
+++ src/Geisha.Engine/Physics/PhysicsEngine2D/PhysicsScene2D.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Geisha.Engine.Physics.PhysicsEngine2D;
+
+internal sealed class PhysicsScene2D
+{
+    public RigidBody2D CreateBody(BodyType bodyType)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveBody(RigidBody2D body)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Simulate(TimeSpan timeStep)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+internal enum BodyType
+{
+    Static,
+    Kinematic
+}
+
+internal sealed class RigidBody2D
+{
+}
