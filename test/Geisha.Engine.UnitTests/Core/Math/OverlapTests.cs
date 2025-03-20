206d24701ac21808462f691e1fd039397dfc1424
Dawid Komorowski <dawidkomorowski@outlook.com>
2025-03-21 00:00:31 +0100

    Refactor rectangle and rectangle overlap tests

--- test/Geisha.Engine.UnitTests/Core/Math/OverlapTests.cs
+++ test/Geisha.Engine.UnitTests/Core/Math/OverlapTests.cs
@@ -12,6 +12,133 @@ public class OverlapTests
     private const double Epsilon = 1e-6;
     private static IEqualityComparer<Vector2> Vector2Comparer => CommonEqualityComparer.Vector2(Epsilon);
 
+    // Horizontal
+    [TestCase( /*R1*/ 0, 0, 10, 5, 0, /*R2*/ 20, 0, 10, 5, 0, /*E*/ false, 0, 0, 0,
+        TestName = $"01_{nameof(RectangleAndRectangle)}")]
+    [TestCase( /*R1*/ 0, 0, 10, 5, 0, /*R2*/ 10, 0, 10, 5, 0, /*E*/ true, -1, 0, 0,
+        TestName = $"02_{nameof(RectangleAndRectangle)}")]
+    [TestCase( /*R1*/ 0, 0, 10, 5, 0, /*R2*/ 9, 0, 10, 5, 0, /*E*/ true, -1, 0, 1,
+        TestName = $"03_{nameof(RectangleAndRectangle)}")]
+    // Vertical
+    [TestCase( /*R1*/ 0, 0, 10, 5, 0, /*R2*/ 0, 10, 10, 5, 0, /*E*/ false, 0, 0, 0,
+        TestName = $"04_{nameof(RectangleAndRectangle)}")]
+    [TestCase( /*R1*/ 0, 0, 10, 5, 0, /*R2*/ 0, 5, 10, 5, 0, /*E*/ true, 0, -1, 0,
+        TestName = $"05_{nameof(RectangleAndRectangle)}")]
+    [TestCase( /*R1*/ 0, 0, 10, 5, 0, /*R2*/ 0, 4, 10, 5, 0, /*E*/ true, 0, -1, 1,
+        TestName = $"06_{nameof(RectangleAndRectangle)}")]
+    // Diagonal
+    [TestCase( /*R1*/ 0, 0, 10, 5, 0, /*R2*/ 20, 10, 10, 5, 0, /*E*/ false, 0, 0, 0,
+        TestName = $"07_{nameof(RectangleAndRectangle)}")]
+    [TestCase( /*R1*/ 0, 0, 10, 5, 0, /*R2*/ 10, 5, 10, 5, 0, /*E*/ true, -1, 0, 0,
+        TestName = $"08_{nameof(RectangleAndRectangle)}")]
+    [TestCase( /*R1*/ 0, 0, 10, 5, 0, /*R2*/ 9, 4, 10, 5, 0, /*E*/ true, -1, 0, 1,
+        TestName = $"09_{nameof(RectangleAndRectangle)}")]
+    [TestCase( /*R1*/ 0, 0, 10, 5, 0, /*R2*/ 8, 4, 10, 5, 0, /*E*/ true, 0, -1, 1,
+        TestName = $"10_{nameof(RectangleAndRectangle)}")]
+    [TestCase( /*R1*/ 0, 0, 10, 5, 0, /*R2*/ 9, 3, 10, 5, 0, /*E*/ true, -1, 0, 1,
+        TestName = $"11_{nameof(RectangleAndRectangle)}")]
+    // Rectangle inside Rectangle
+    [TestCase( /*R1*/ 0, 0, 10, 5, 0, /*R2*/ 3, 0, 2, 4, 0, /*E*/ true, -1, 0, 3,
+        TestName = $"12_{nameof(RectangleAndRectangle)}")]
+    [TestCase( /*R1*/ 0, 0, 10, 5, 0, /*R2*/ -3, 0, 2, 4, 0, /*E*/ true, 1, 0, 3,
+        TestName = $"13_{nameof(RectangleAndRectangle)}")]
+    [TestCase( /*R1*/ 0, 0, 10, 5, 0, /*R2*/ 2, 1, 4, 2, 0, /*E*/ true, 0, -1, 2.5,
+        TestName = $"14_{nameof(RectangleAndRectangle)}")]
+    [TestCase( /*R1*/ 0, 0, 10, 5, 0, /*R2*/ 2, -1, 4, 2, 0, /*E*/ true, 0, 1, 2.5,
+        TestName = $"15_{nameof(RectangleAndRectangle)}")]
+    // One rotated
+    [TestCase( /*R1*/ 0, 0, 10, 10, 0, /*R2*/ 12.5, 0, 10, 10, 45, /*E*/ false, 0, 0, 0,
+        TestName = $"16_{nameof(RectangleAndRectangle)}")]
+    [TestCase( /*R1*/ 0, 0, 10, 10, 0, /*R2*/ 12, 0, 10, 10, 45, /*E*/ true, -1, 0, 0.071067,
+        TestName = $"17_{nameof(RectangleAndRectangle)}")]
+    [TestCase( /*R1*/ 0, 0, 10, 10, 0, /*R2*/ 0, 12.5, 10, 10, 45, /*E*/ false, 0, 0, 0,
+        TestName = $"18_{nameof(RectangleAndRectangle)}")]
+    [TestCase( /*R1*/ 0, 0, 10, 10, 0, /*R2*/ 0, 12, 10, 10, 45, /*E*/ true, 0, -1, 0.071067,
+        TestName = $"19_{nameof(RectangleAndRectangle)}")]
+    // Two rotated
+    [TestCase( /*R1*/ 0, 0, 10, 10, 45, /*R2*/ 14.5, 0, 10, 10, 45, /*E*/ false, 0, 0, 0,
+        TestName = $"20_{nameof(RectangleAndRectangle)}")]
+    [TestCase( /*R1*/ 0, 0, 10, 10, 45, /*R2*/ 9, 0, 10, 10, 45, /*E*/ true, -0.707106, 0.707106, 3.636038,
+        TestName = $"21_{nameof(RectangleAndRectangle)}")]
+    [TestCase( /*R1*/ 0, 0, 10, 10, 45, /*R2*/ 10, 1, 10, 10, 45, /*E*/ true, -0.707106, -0.707106, 2.221825,
+        TestName = $"22_{nameof(RectangleAndRectangle)}")]
+    [TestCase( /*R1*/ 0, 0, 10, 10, 45, /*R2*/ 10, -1, 10, 10, 45, /*E*/ true, -0.707106, 0.707106, 2.221825,
+        TestName = $"23_{nameof(RectangleAndRectangle)}")]
+    [TestCase( /*R1*/ 0, 0, 10, 10, 45, /*R2*/ 9, 5.5, 10, 10, 45, /*E*/ false, 0, 0, 0,
+        TestName = $"24_{nameof(RectangleAndRectangle)}")]
+    [TestCase( /*R1*/ 174, 110, 100, 100, 102, /*R2*/ 271, 187, 100, 100, 44, /*E*/ false, 0, 0, 0,
+        TestName = $"25_{nameof(RectangleAndRectangle)}")]
+    [TestCase( /*R1*/ 174, 110, 100, 100, 102, /*R2*/ 271, 187, 100, 100, 56, /*E*/ true, -0.559192, -0.829037, 2.622303,
+        TestName = $"26_{nameof(RectangleAndRectangle)}")]
+    public void RectangleAndRectangle(
+        double x1, double y1, double w1, double h1, double rotation1,
+        double x2, double y2, double w2, double h2, double rotation2,
+        bool overlap, double mtvX, double mtvY, double mtvLength
+    )
+    {
+        // Arrange
+        var rotationMatrix1 = Matrix3x3.CreateTranslation(new Vector2(x1, y1)) *
+                              Matrix3x3.CreateRotation(Angle.Deg2Rad(rotation1)) *
+                              Matrix3x3.CreateTranslation(new Vector2(-x1, -y1));
+
+        var rotationMatrix2 = Matrix3x3.CreateTranslation(new Vector2(x2, y2)) *
+                              Matrix3x3.CreateRotation(Angle.Deg2Rad(rotation2)) *
+                              Matrix3x3.CreateTranslation(new Vector2(-x2, -y2));
+
+        var rectangle1 = new Rectangle(new Vector2(x1, y1), new Vector2(w1, h1)).Transform(rotationMatrix1);
+        var rectangle2 = new Rectangle(new Vector2(x2, y2), new Vector2(w2, h2)).Transform(rotationMatrix2);
+
+        using var visualOutput = TestKit.CreateVisualOutput(scale: 20, enabled: false);
+        visualOutput.DrawRectangle(rectangle1, Color.Red);
+        visualOutput.DrawRectangle(rectangle2, Color.Blue);
+        visualOutput.SaveToFile();
+
+        // Act
+        var actual1 = rectangle1.Overlaps(rectangle2);
+        var actual2 = rectangle2.Overlaps(rectangle1);
+        var actual3 = rectangle1.Overlaps(rectangle2, out var mtv1);
+        var actual4 = rectangle2.Overlaps(rectangle1, out var mtv2);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(actual1, Is.EqualTo(overlap));
+            Assert.That(actual2, Is.EqualTo(overlap));
+            Assert.That(actual3, Is.EqualTo(overlap));
+            Assert.That(actual4, Is.EqualTo(overlap));
+
+            Assert.That(mtv1.Direction, Is.EqualTo(new Vector2(mtvX, mtvY)).Using(Vector2Comparer));
+            Assert.That(mtv1.Length, Is.EqualTo(mtvLength));
+
+            Assert.That(mtv2.Direction, Is.EqualTo(new Vector2(mtvX, mtvY).Opposite).Using(Vector2Comparer));
+            Assert.That(mtv2.Length, Is.EqualTo(mtvLength));
+        });
+    }
+
+    [Test]
+    public void RectangleAndRectangle_WhenRectanglesHaveTheSameCenter()
+    {
+        // Arrange
+        var rectangle1 = new Rectangle(new Vector2(20, 20));
+        var rectangle2 = new Rectangle(new Vector2(10, 10));
+
+        // Act
+        var actual1 = rectangle1.Overlaps(rectangle2, out var mtv1);
+        var actual2 = rectangle2.Overlaps(rectangle1, out var mtv2);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(actual1, Is.True);
+            Assert.That(actual2, Is.True);
+
+            Assert.That(mtv1.Direction, Is.EqualTo(Vector2.UnitX.Opposite));
+            Assert.That(mtv1.Length, Is.EqualTo(15));
+
+            Assert.That(mtv2.Direction, Is.EqualTo(Vector2.UnitX.Opposite));
+            Assert.That(mtv2.Length, Is.EqualTo(15));
+        });
+    }
 
     // Circle outside of Rectangle
     [TestCase( /*R*/ 30, 20, 40, 20, 0, /*C*/ 70, 20, 10, /*E*/ false, 0, 0, 0,
@@ -78,6 +205,7 @@ public class OverlapTests
         TestName = $"28_{nameof(RectangleAndCircle)}")]
     [TestCase( /*R*/ 10, 20, 100, 50, 45, /*C*/ 50, -15, 25, /*E*/ false, 0, 0, 0,
         TestName = $"29_{nameof(RectangleAndCircle)}")]
+    // TODO Rectangle inside of Circle?
     public void RectangleAndCircle(
         double rx, double ry, double rw, double rh, double rotation,
         double cx, double cy, double cr,
