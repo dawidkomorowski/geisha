70001680105ecd04c13630beac5b0ff3730bb0c2
Dawid Komorowski <dawidkomorowski@outlook.com>
2025-03-25 02:43:55 +0100

    Prepare scaffolding for physics sandbox

---
+++ sandbox/Sandbox/Physics/PhysicsSandboxSceneBehavior.cs
@@ -0,0 +1,24 @@
+﻿using Geisha.Engine.Core.SceneModel;
+
+namespace Sandbox.Physics;
+
+public sealed class PhysicsSandboxSceneBehaviorFactory : ISceneBehaviorFactory
+{
+    private const string SceneBehaviorName = "PhysicsSandbox";
+
+    public string BehaviorName => SceneBehaviorName;
+    public SceneBehavior Create(Scene scene) => new PhysicsSandboxSceneBehavior(scene);
+
+    private sealed class PhysicsSandboxSceneBehavior : SceneBehavior
+    {
+        public PhysicsSandboxSceneBehavior(Scene scene) : base(scene)
+        {
+        }
+
+        public override string Name => SceneBehaviorName;
+
+        protected override void OnLoaded()
+        {
+        }
+    }
+}
---
+++ sandbox/Sandbox/SandboxApp.cs
@@ -0,0 +1,17 @@
+﻿using Geisha.Engine;
+using Sandbox.Physics;
+
+namespace Sandbox;
+
+public sealed class SandboxApp : Game
+{
+    public override string WindowTitle => "Geisha Engine Sandbox";
+
+    public override void RegisterComponents(IComponentsRegistry componentsRegistry)
+    {
+        componentsRegistry.RegisterSystem<SandboxSystem>();
+        componentsRegistry.RegisterSceneBehaviorFactory<SandboxSceneBehaviorFactory>();
+        componentsRegistry.RegisterSceneBehaviorFactory<PhysicsSandboxSceneBehaviorFactory>();
+        componentsRegistry.RegisterComponentFactory<EntityControllerComponentFactory>();
+    }
+}
