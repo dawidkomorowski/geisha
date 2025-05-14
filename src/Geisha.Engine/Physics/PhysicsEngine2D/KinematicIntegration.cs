e1ce87d4d59aec9a88479d375c7201d5c8cd575e
Dawid Komorowski <dawidkomorowski@outlook.com>
2025-05-14 21:53:12 +0200

    Test simple global sub stepping

--- src/Geisha.Engine/Physics/PhysicsEngine2D/KinematicIntegration.cs
+++ src/Geisha.Engine/Physics/PhysicsEngine2D/KinematicIntegration.cs
@@ -4,11 +4,11 @@ namespace Geisha.Engine.Physics.PhysicsEngine2D;
 
 internal static class KinematicIntegration
 {
-    public static void IntegrateKinematicMotion(IReadOnlyList<RigidBody2D> bodies, double deltaTimeSeconds)
+    public static void IntegrateKinematicMotion(IReadOnlyList<RigidBody2D> kinematicBodies, double deltaTimeSeconds)
     {
-        for (var i = 0; i < bodies.Count; i++)
+        for (var i = 0; i < kinematicBodies.Count; i++)
         {
-            var body = bodies[i];
+            var body = kinematicBodies[i];
 
             body.Position += body.LinearVelocity * deltaTimeSeconds;
             body.Rotation += body.AngularVelocity * deltaTimeSeconds;
--- src/Geisha.Engine/Physics/PhysicsEngine2D/PhysicsScene2D.cs
+++ src/Geisha.Engine/Physics/PhysicsEngine2D/PhysicsScene2D.cs
@@ -45,27 +45,35 @@ internal sealed class PhysicsScene2D
 
     public void Simulate(TimeSpan timeStep)
     {
-        ContactSolver.SolveVelocityConstraints(_kinematicBodies);
+        // TODO Make this configurable.
+        const int subSteps = 4;
+        var subStepTime = timeStep.TotalSeconds / subSteps;
 
-        KinematicIntegration.IntegrateKinematicMotion(_kinematicBodies, timeStep.TotalSeconds);
-
-        foreach (var kinematicBody in _kinematicBodies)
+        for (var s = 0; s < subSteps; s++)
         {
-            kinematicBody.RecomputeCollider();
-        }
+            // TODO Consider adding minimum velocity threshold to avoid solving constraints for very small velocities.
+            ContactSolver.SolveVelocityConstraints(_kinematicBodies);
 
-        CollisionDetection.DetectCollisions(_staticBodies, _kinematicBodies);
+            KinematicIntegration.IntegrateKinematicMotion(_kinematicBodies, subStepTime);
 
-        // TODO Make this more general and configurable.
-        FilterTileGhostCollisions(_kinematicBodies);
+            foreach (var kinematicBody in _kinematicBodies)
+            {
+                kinematicBody.RecomputeCollider();
+            }
 
-        // TODO Constant of 6 is how many times position constraints are iteratively solved. It is arbitrary value.
-        // TODO It may require further research to find optimal value. Also, it may require to be configurable.
-        // TODO SolvePositionConstraints could return a boolean value indicating whether the position constraints were solved. Then further iterations could be stopped.
-        // TODO Research it further when working on https://github.com/dawidkomorowski/geisha/issues/324.
-        for (var i = 0; i < 6; i++)
-        {
-            ContactSolver.SolvePositionConstraints(_kinematicBodies);
+            CollisionDetection.DetectCollisions(_staticBodies, _kinematicBodies);
+
+            // TODO Make this more general and configurable.
+            FilterTileGhostCollisions(_kinematicBodies);
+
+            // TODO Constant of 6 is how many times position constraints are iteratively solved. It is arbitrary value.
+            // TODO It may require further research to find optimal value. Also, it may require to be configurable.
+            // TODO SolvePositionConstraints could return a boolean value indicating whether the position constraints were solved. Then further iterations could be stopped.
+            // TODO Research it further when working on https://github.com/dawidkomorowski/geisha/issues/324.
+            for (var i = 0; i < 6; i++)
+            {
+                ContactSolver.SolvePositionConstraints(_kinematicBodies);
+            }
         }
     }
 
