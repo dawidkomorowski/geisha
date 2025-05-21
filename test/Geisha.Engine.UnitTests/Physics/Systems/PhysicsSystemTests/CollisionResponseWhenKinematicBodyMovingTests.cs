9831433e8e339a4200d88aa9be3afcec91719e60
Dawid Komorowski <dawidkomorowski@outlook.com>
2025-05-21 21:08:50 +0200

    Add tests for kinematic bodies collisions with enabled collision response

--- test/Geisha.Engine.UnitTests/Physics/Systems/PhysicsSystemTests/CollisionResponseWhenKinematicBodyMovingTests.cs
+++ test/Geisha.Engine.UnitTests/Physics/Systems/PhysicsSystemTests/CollisionResponseWhenKinematicBodyMovingTests.cs
@@ -22,7 +22,6 @@ public class CollisionResponseWhenKinematicBodyMovingTests : PhysicsSystemTestsB
         kinematicBody.GetComponent<KinematicRigidBody2DComponent>().LinearVelocity = new Vector2(20, 0);
         CreateRectangleStaticBody(5, 5, 10, 10);
 
-
         // Act
         SaveVisualOutput(physicsSystem, 0, 10);
         physicsSystem.ProcessPhysics();
@@ -50,7 +49,6 @@ public class CollisionResponseWhenKinematicBodyMovingTests : PhysicsSystemTestsB
         kinematicBody1.GetComponent<KinematicRigidBody2DComponent>().LinearVelocity = new Vector2(20, 0);
         var kinematicBody2 = CreateRectangleKinematicBody(5, 5, 10, 10);
         kinematicBody2.GetComponent<KinematicRigidBody2DComponent>().EnableCollisionResponse = false;
-
 
         // Act
         SaveVisualOutput(physicsSystem, 0, 10);
@@ -85,7 +83,6 @@ public class CollisionResponseWhenKinematicBodyMovingTests : PhysicsSystemTestsB
         kinematicBody.GetComponent<KinematicRigidBody2DComponent>().LinearVelocity = new Vector2(20, 0);
         CreateRectangleStaticBody(5, 5, 10, 10);
 
-
         // Act
         SaveVisualOutput(physicsSystem, 0, 10);
         physicsSystem.ProcessPhysics();
@@ -114,7 +111,6 @@ public class CollisionResponseWhenKinematicBodyMovingTests : PhysicsSystemTestsB
         CreateRectangleStaticBody(0, 0, 10, 10);
         CreateRectangleStaticBody(5, 5, 10, 10);
 
-
         // Act
         SaveVisualOutput(physicsSystem, 0, 10);
         physicsSystem.ProcessPhysics();
@@ -128,5 +124,73 @@ public class CollisionResponseWhenKinematicBodyMovingTests : PhysicsSystemTestsB
         Assert.That(kinematicBody.GetComponent<Transform2DComponent>().Rotation, Is.Zero);
         Assert.That(kinematicBody.GetComponent<KinematicRigidBody2DComponent>().LinearVelocity, Is.EqualTo(Vector2.Zero));
         Assert.That(kinematicBody.GetComponent<KinematicRigidBody2DComponent>().AngularVelocity, Is.EqualTo(0d));
+    }
+
+    [Test]
+    public void CollisionResponseEnabled_ForOneBody_KinematicBody_And_KinematicBody()
+    {
+        // Arrange
+        GameTime.FixedDeltaTime = TimeSpan.FromSeconds(0.1);
+
+        var physicsSystem = GetPhysicsSystem();
+        var kinematicBody1 = CreateRectangleKinematicBody(-6, 5, 10, 10);
+        kinematicBody1.GetComponent<KinematicRigidBody2DComponent>().EnableCollisionResponse = true;
+        kinematicBody1.GetComponent<KinematicRigidBody2DComponent>().LinearVelocity = new Vector2(20, 0);
+        var kinematicBody2 = CreateRectangleKinematicBody(5, 5, 10, 10);
+        kinematicBody2.GetComponent<KinematicRigidBody2DComponent>().EnableCollisionResponse = false;
+
+        // Act
+        SaveVisualOutput(physicsSystem, 0, 10);
+        physicsSystem.ProcessPhysics();
+        SaveVisualOutput(physicsSystem, 1, 10);
+        physicsSystem.ProcessPhysics();
+        SaveVisualOutput(physicsSystem, 2, 10);
+
+        // Assert
+        Assert.That(kinematicBody1.GetComponent<RectangleColliderComponent>().IsColliding, Is.True);
+        Assert.That(kinematicBody1.GetComponent<Transform2DComponent>().Translation, Is.EqualTo(new Vector2(-5, 5)));
+        Assert.That(kinematicBody1.GetComponent<Transform2DComponent>().Rotation, Is.Zero);
+        Assert.That(kinematicBody1.GetComponent<KinematicRigidBody2DComponent>().LinearVelocity, Is.EqualTo(Vector2.Zero));
+        Assert.That(kinematicBody1.GetComponent<KinematicRigidBody2DComponent>().AngularVelocity, Is.EqualTo(0d));
+
+        Assert.That(kinematicBody2.GetComponent<RectangleColliderComponent>().IsColliding, Is.True);
+        Assert.That(kinematicBody2.GetComponent<Transform2DComponent>().Translation, Is.EqualTo(new Vector2(5, 5)));
+        Assert.That(kinematicBody2.GetComponent<Transform2DComponent>().Rotation, Is.Zero);
+        Assert.That(kinematicBody2.GetComponent<KinematicRigidBody2DComponent>().LinearVelocity, Is.EqualTo(Vector2.Zero));
+        Assert.That(kinematicBody2.GetComponent<KinematicRigidBody2DComponent>().AngularVelocity, Is.EqualTo(0d));
+    }
+
+    [Test]
+    public void CollisionResponseEnabled_ForBothBodies_KinematicBody_And_KinematicBody()
+    {
+        // Arrange
+        GameTime.FixedDeltaTime = TimeSpan.FromSeconds(0.1);
+
+        var physicsSystem = GetPhysicsSystem();
+        var kinematicBody1 = CreateRectangleKinematicBody(-6, 5, 10, 10);
+        kinematicBody1.GetComponent<KinematicRigidBody2DComponent>().EnableCollisionResponse = true;
+        kinematicBody1.GetComponent<KinematicRigidBody2DComponent>().LinearVelocity = new Vector2(20, 0);
+        var kinematicBody2 = CreateRectangleKinematicBody(5, 5, 10, 10);
+        kinematicBody2.GetComponent<KinematicRigidBody2DComponent>().EnableCollisionResponse = true;
+
+        // Act
+        SaveVisualOutput(physicsSystem, 0, 10);
+        physicsSystem.ProcessPhysics();
+        SaveVisualOutput(physicsSystem, 1, 10);
+        physicsSystem.ProcessPhysics();
+        SaveVisualOutput(physicsSystem, 2, 10);
+
+        // Assert
+        Assert.That(kinematicBody1.GetComponent<RectangleColliderComponent>().IsColliding, Is.True);
+        Assert.That(kinematicBody1.GetComponent<Transform2DComponent>().Translation, Is.EqualTo(new Vector2(-3.5, 5)));
+        Assert.That(kinematicBody1.GetComponent<Transform2DComponent>().Rotation, Is.Zero);
+        Assert.That(kinematicBody1.GetComponent<KinematicRigidBody2DComponent>().LinearVelocity, Is.EqualTo(new Vector2(10, 0)));
+        Assert.That(kinematicBody1.GetComponent<KinematicRigidBody2DComponent>().AngularVelocity, Is.EqualTo(0d));
+
+        Assert.That(kinematicBody2.GetComponent<RectangleColliderComponent>().IsColliding, Is.True);
+        Assert.That(kinematicBody2.GetComponent<Transform2DComponent>().Translation, Is.EqualTo(new Vector2(6.5, 5)));
+        Assert.That(kinematicBody2.GetComponent<Transform2DComponent>().Rotation, Is.Zero);
+        Assert.That(kinematicBody2.GetComponent<KinematicRigidBody2DComponent>().LinearVelocity, Is.EqualTo(new Vector2(10, 0)));
+        Assert.That(kinematicBody2.GetComponent<KinematicRigidBody2DComponent>().AngularVelocity, Is.EqualTo(0d));
     }
 }
