272671030e8b6832a8d43945aafc193a01b4480d
Dawid Komorowski <dawidkomorowski@outlook.com>
2025-05-23 22:02:52 +0200

    Remove TODO for task #579

--- src/Geisha.Engine/Physics/PhysicsEngine2D/PhysicsScene2D.cs
+++ src/Geisha.Engine/Physics/PhysicsEngine2D/PhysicsScene2D.cs
@@ -69,10 +69,7 @@ internal sealed class PhysicsScene2D
             // TODO Make this more general and configurable.
             FilterTileGhostCollisions(_kinematicBodies);
 
-            // TODO Constant of 6 is how many times position constraints are iteratively solved. It is arbitrary value.
-            // TODO It may require further research to find optimal value. Also, it may require to be configurable.
             // TODO SolvePositionConstraints could return a boolean value indicating whether the position constraints were solved. Then further iterations could be stopped.
-            // TODO Research it further when working on https://github.com/dawidkomorowski/geisha/issues/324.
             for (var i = 0; i < 6; i++)
             {
                 ContactSolver.SolvePositionConstraints(_kinematicBodies);
