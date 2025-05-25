13144cd39511b1cf686d3a4f6f41880cc72ecaf1
Dawid Komorowski <dawidkomorowski@outlook.com>
2025-05-25 17:35:38 +0200

    Add documentation for Substeps configuration property

--- src/Geisha.Engine/Physics/PhysicsConfiguration.cs
+++ src/Geisha.Engine/Physics/PhysicsConfiguration.cs
@@ -5,7 +5,20 @@
     /// </summary>
     public sealed record PhysicsConfiguration
     {
-        // TODO Add documentation for Substeps.
+        /// <summary>
+        ///     Defines how many substeps are performed during physics simulation per each game loop fixed update.
+        /// </summary>
+        /// <remarks>
+        ///     <para>
+        ///         Increasing number of substeps will improve precision and stability of physics simulation, but it will be at
+        ///         cost of decreased performance.
+        ///     </para>
+        ///     <para>
+        ///         In general, increasing the number of substeps will behave in a similar way as increasing the number of fixed
+        ///         updates per second. However, substeps only affect physics simulation therefore it does not have an overhead of
+        ///         increased frequency of synchronization of Physics Engine state with Scene state.
+        ///     </para>
+        /// </remarks>
         public int Substeps { get; init; } = 1;
 
         /// <summary>
