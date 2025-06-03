d831b29f788c2b4153cc8fe9a0ef0d9f6532d764
Dawid Komorowski <dawidkomorowski@outlook.com>
2025-06-03 20:51:48 +0200

    Add documentation for Size and SizeD

--- src/Geisha.Engine/Core/Math/Size.cs
+++ src/Geisha.Engine/Core/Math/Size.cs
@@ -1,15 +1,34 @@
 ﻿namespace Geisha.Engine.Core.Math;
 
-// TODO Add XML documentation comments to this struct.
+/// <summary>
+///     Represents the dimensions of a 2D object, defined by integer values for its width and height.
+/// </summary>
+/// <seealso cref="SizeD" />
 public readonly record struct Size
 {
+    /// <summary>
+    ///     Gets a <see cref="Size" /> struct that has both width and height set to zero.
+    /// </summary>
+    public static Size Empty => new(0, 0);
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="Size" /> struct with the specified dimensions.
+    /// </summary>
+    /// <param name="width">The width component of the size.</param>
+    /// <param name="height">The height component of the size.</param>
     public Size(int width, int height)
     {
         Width = width;
         Height = height;
     }
 
+    /// <summary>
+    ///     Gets the width component of this <see cref="Size" />.
+    /// </summary>
     public int Width { get; init; }
+
+    /// <summary>
+    ///     Gets the height component of this <see cref="Size" />.
+    /// </summary>
     public int Height { get; init; }
-    public static Size Empty => new(0, 0);
 }
