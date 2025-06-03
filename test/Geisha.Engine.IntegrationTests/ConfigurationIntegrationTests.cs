ef0d42aea733003ab67b66976a157e408bf9fd56
Dawid Komorowski <dawidkomorowski@outlook.com>
2025-06-03 19:57:29 +0200

    Add support for TileSize property in configuration file

--- test/Geisha.Engine.IntegrationTests/ConfigurationIntegrationTests.cs
+++ test/Geisha.Engine.IntegrationTests/ConfigurationIntegrationTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using Geisha.Engine.Core.Logging;
+using Geisha.Engine.Core.Math;
 using Geisha.Engine.Rendering;
 using Geisha.TestUtils;
 using NUnit.Framework;
@@ -10,6 +11,48 @@ namespace Geisha.Engine.IntegrationTests;
 [TestFixture]
 public class ConfigurationIntegrationTests
 {
+    [Test]
+    public void LoadFromFile_ShouldCreateDefaultConfiguration_WhenConfigurationFileIsEmpty()
+    {
+        // Arrange
+        var path = Utils.GetPathUnderTestDirectory(Path.Combine("Configuration", "empty-configuration.json"));
+
+        // Act
+        var configuration = Configuration.LoadFromFile(path);
+
+        // Assert
+        Assert.That(configuration.Audio.EnableSound, Is.True);
+        Assert.That(configuration.Audio.Volume, Is.EqualTo(1.0));
+
+        Assert.That(configuration.Core.AssetsRootDirectoryPath, Is.EqualTo("Assets"));
+        Assert.That(configuration.Core.CustomGameLoopSteps, Is.EqualTo(Array.Empty<string>()));
+        Assert.That(configuration.Core.FixedUpdatesPerFrameLimit, Is.Zero);
+        Assert.That(configuration.Core.FixedUpdatesPerSecond, Is.EqualTo(60));
+        Assert.That(configuration.Core.LogLevel, Is.EqualTo(LogLevel.Info));
+        Assert.That(configuration.Core.ShowAllEntitiesCount, Is.False);
+        Assert.That(configuration.Core.ShowRootEntitiesCount, Is.False);
+        Assert.That(configuration.Core.ShowFps, Is.False);
+        Assert.That(configuration.Core.ShowFrameTime, Is.False);
+        Assert.That(configuration.Core.ShowTotalFrames, Is.False);
+        Assert.That(configuration.Core.ShowTotalTime, Is.False);
+        Assert.That(configuration.Core.ShowGameLoopStatistics, Is.False);
+        Assert.That(configuration.Core.StartUpScene, Is.Empty);
+        Assert.That(configuration.Core.StartUpSceneBehavior, Is.Empty);
+
+        Assert.That(configuration.Physics.Substeps, Is.EqualTo(1));
+        Assert.That(configuration.Physics.VelocityIterations, Is.EqualTo(4));
+        Assert.That(configuration.Physics.PositionIterations, Is.EqualTo(4));
+        Assert.That(configuration.Physics.PenetrationTolerance, Is.EqualTo(0.01));
+        Assert.That(configuration.Physics.TileSize, Is.EqualTo(new SizeD(1, 1)));
+        Assert.That(configuration.Physics.RenderCollisionGeometry, Is.False);
+
+        Assert.That(configuration.Rendering.EnableVSync, Is.False);
+        Assert.That(configuration.Rendering.ScreenHeight, Is.EqualTo(720));
+        Assert.That(configuration.Rendering.ScreenWidth, Is.EqualTo(1280));
+        Assert.That(configuration.Rendering.ShowRenderingStatistics, Is.False);
+        Assert.That(configuration.Rendering.SortingLayersOrder, Is.EqualTo(new[] { RenderingConfiguration.DefaultSortingLayerName }));
+    }
+
     [Test]
     public void LoadFromFile_ShouldCreateConfigurationCorrespondingToFileContent()
     {
@@ -42,6 +85,7 @@ public class ConfigurationIntegrationTests
         Assert.That(configuration.Physics.VelocityIterations, Is.EqualTo(34));
         Assert.That(configuration.Physics.PositionIterations, Is.EqualTo(56));
         Assert.That(configuration.Physics.PenetrationTolerance, Is.EqualTo(1.23));
+        Assert.That(configuration.Physics.TileSize, Is.EqualTo(new SizeD(1.2, 3.4)));
         Assert.That(configuration.Physics.RenderCollisionGeometry, Is.True);
 
         Assert.That(configuration.Rendering.EnableVSync, Is.True);
@@ -49,46 +93,5 @@ public class ConfigurationIntegrationTests
         Assert.That(configuration.Rendering.ScreenWidth, Is.EqualTo(3840));
         Assert.That(configuration.Rendering.ShowRenderingStatistics, Is.True);
         Assert.That(configuration.Rendering.SortingLayersOrder, Is.EqualTo(new[] { "Layer1", "Layer2", "Layer3" }));
-    }
-
-    [Test]
-    public void LoadFromFile_ShouldCreateConfigurationWithDefaultValuesWhenFileConfigurationIsEmpty()
-    {
-        // Arrange
-        var path = Utils.GetPathUnderTestDirectory(Path.Combine("Configuration", "empty-configuration.json"));
-
-        // Act
-        var configuration = Configuration.LoadFromFile(path);
-
-        // Assert
-        Assert.That(configuration.Audio.EnableSound, Is.True);
-        Assert.That(configuration.Audio.Volume, Is.EqualTo(1.0));
-
-        Assert.That(configuration.Core.AssetsRootDirectoryPath, Is.EqualTo("Assets"));
-        Assert.That(configuration.Core.CustomGameLoopSteps, Is.EqualTo(Array.Empty<string>()));
-        Assert.That(configuration.Core.FixedUpdatesPerFrameLimit, Is.Zero);
-        Assert.That(configuration.Core.FixedUpdatesPerSecond, Is.EqualTo(60));
-        Assert.That(configuration.Core.LogLevel, Is.EqualTo(LogLevel.Info));
-        Assert.That(configuration.Core.ShowAllEntitiesCount, Is.False);
-        Assert.That(configuration.Core.ShowRootEntitiesCount, Is.False);
-        Assert.That(configuration.Core.ShowFps, Is.False);
-        Assert.That(configuration.Core.ShowFrameTime, Is.False);
-        Assert.That(configuration.Core.ShowTotalFrames, Is.False);
-        Assert.That(configuration.Core.ShowTotalTime, Is.False);
-        Assert.That(configuration.Core.ShowGameLoopStatistics, Is.False);
-        Assert.That(configuration.Core.StartUpScene, Is.Empty);
-        Assert.That(configuration.Core.StartUpSceneBehavior, Is.Empty);
-
-        Assert.That(configuration.Physics.Substeps, Is.EqualTo(1));
-        Assert.That(configuration.Physics.VelocityIterations, Is.EqualTo(4));
-        Assert.That(configuration.Physics.PositionIterations, Is.EqualTo(4));
-        Assert.That(configuration.Physics.PenetrationTolerance, Is.EqualTo(0.01));
-        Assert.That(configuration.Physics.RenderCollisionGeometry, Is.False);
-
-        Assert.That(configuration.Rendering.EnableVSync, Is.False);
-        Assert.That(configuration.Rendering.ScreenHeight, Is.EqualTo(720));
-        Assert.That(configuration.Rendering.ScreenWidth, Is.EqualTo(1280));
-        Assert.That(configuration.Rendering.ShowRenderingStatistics, Is.False);
-        Assert.That(configuration.Rendering.SortingLayersOrder, Is.EqualTo(new[] { RenderingConfiguration.DefaultSortingLayerName }));
     }
 }
