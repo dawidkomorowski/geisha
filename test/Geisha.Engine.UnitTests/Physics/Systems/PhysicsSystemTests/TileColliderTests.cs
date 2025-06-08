d255a9b5ce7c64fee273371c6856baa54e8576c3
Dawid Komorowski <dawidkomorowski@outlook.com>
2025-06-08 18:06:26 +0200

    Add basic test scenario for tile collider solving ghost collisions

--- test/Geisha.Engine.UnitTests/Physics/Systems/PhysicsSystemTests/TileColliderTests.cs
+++ test/Geisha.Engine.UnitTests/Physics/Systems/PhysicsSystemTests/TileColliderTests.cs
@@ -1,5 +1,9 @@
-﻿using Geisha.Engine.Core.Math;
+﻿using System;
+using Geisha.Engine.Core;
+using Geisha.Engine.Core.Components;
+using Geisha.Engine.Core.Math;
 using Geisha.Engine.Physics;
+using Geisha.Engine.Physics.Components;
 using NUnit.Framework;
 
 namespace Geisha.Engine.UnitTests.Physics.Systems.PhysicsSystemTests;
@@ -37,19 +41,33 @@ public class TileColliderTests : PhysicsSystemTestsBase
         Assert.That(() => GetPhysicsSystem(physicsConfiguration), Throws.Nothing);
     }
 
-    // TODO
     [Test]
-    public void TODO()
+    public void KinematicBody_ShouldAvoidGhostCollisionWithTileCollider_WhenMovingToTheRight()
     {
         // Arrange
+        GameTime.FixedDeltaTime = TimeSpan.FromSeconds(0.1);
         var physicsConfiguration = new PhysicsConfiguration
         {
-            TileSize = new SizeD(1, 1)
+            TileSize = new SizeD(10, 10),
+            RenderCollisionGeometry = true
         };
         var physicsSystem = GetPhysicsSystem(physicsConfiguration);
 
         CreateTileStaticBody(0, 0);
+        CreateTileStaticBody(10, 0);
+        var kinematicBody = CreateRectangleKinematicBody(-0.5, 9, 10, 10);
+        kinematicBody.GetComponent<KinematicRigidBody2DComponent>().EnableCollisionResponse = true;
+        kinematicBody.GetComponent<KinematicRigidBody2DComponent>().LinearVelocity = new Vector2(10, 0);
+
         // Act
+        SaveVisualOutput(physicsSystem, 0, 10);
+        physicsSystem.ProcessPhysics();
+        SaveVisualOutput(physicsSystem, 1, 10);
+        physicsSystem.ProcessPhysics();
+        SaveVisualOutput(physicsSystem, 2, 10);
+
         // Assert
+        Assert.That(kinematicBody.GetComponent<KinematicRigidBody2DComponent>().LinearVelocity, Is.EqualTo(new Vector2(10, 0)));
+        Assert.That(kinematicBody.GetComponent<Transform2DComponent>().Translation, Is.EqualTo(new Vector2(1.5, 9)));
     }
 }
