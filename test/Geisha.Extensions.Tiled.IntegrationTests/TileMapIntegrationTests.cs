57fc0bb8c389104d46a62fba03dc6259dca573b0
Dawid Komorowski <dawidkomorowski@outlook.com>
2026-02-11 19:30:07 +0100

    Add map properties test and TMX file
    Add LoadFromFile_Reads_MapProperties integration test and a new TMX fixture (map_properties.tmx) that contains various custom properties, two tilesets, an empty tile layer and an object group. The test asserts map metadata (version, tiledversion, orientation, render order, dimensions, tile size, infinite flag) and verifies custom properties (Bool, Enum, Float, Int, String, Object) and their typed values. Also rename existing tests to LoadFromFile_EmptyMap and LoadFromFile_ComplexMap and remove duplicated property entries from complex.tmx so property-specific checks use the new map_properties.tmx fixture.

--- test/Geisha.Extensions.Tiled.IntegrationTests/TileMapIntegrationTests.cs
+++ test/Geisha.Extensions.Tiled.IntegrationTests/TileMapIntegrationTests.cs
@@ -7,7 +7,66 @@ namespace Geisha.Extensions.Tiled.IntegrationTests;
 public class TileMapIntegrationTests
 {
     [Test]
-    public void LoadFromFile_Empty()
+    public void LoadFromFile_Reads_MapProperties()
+    {
+        // Arrange
+        var filePath = Path.Combine("Tiled", "TileMaps", "map_properties.tmx");
+
+        // Act
+        var tileMap = TileMap.LoadFromFile(filePath);
+
+        // Assert
+
+        // Assert map properties
+        Assert.That(tileMap.Version, Is.EqualTo("1.10"));
+        Assert.That(tileMap.TiledVersion, Is.EqualTo("1.11.2"));
+        Assert.That(tileMap.Orientation, Is.EqualTo(Orientation.Orthogonal));
+        Assert.That(tileMap.RenderOrder, Is.EqualTo(RenderOrder.RightDown));
+        Assert.That(tileMap.Width, Is.EqualTo(20));
+        Assert.That(tileMap.Height, Is.EqualTo(20));
+        Assert.That(tileMap.TileWidth, Is.EqualTo(18));
+        Assert.That(tileMap.TileHeight, Is.EqualTo(18));
+        Assert.That(tileMap.IsInfinite, Is.False);
+
+        // Assert map custom properties
+        Assert.That(tileMap.Properties["Bool Property"].Name, Is.EqualTo("Bool Property"));
+        Assert.That(tileMap.Properties["Bool Property"].Type, Is.EqualTo(PropertyType.Bool));
+        Assert.That(tileMap.Properties["Bool Property"].CustomPropertyType, Is.Empty);
+        Assert.That(tileMap.Properties["Bool Property"].Value, Is.EqualTo("true"));
+        Assert.That(tileMap.Properties["Bool Property"].BoolValue, Is.True);
+
+        Assert.That(tileMap.Properties["Enum Property"].Name, Is.EqualTo("Enum Property"));
+        Assert.That(tileMap.Properties["Enum Property"].Type, Is.EqualTo(PropertyType.String));
+        Assert.That(tileMap.Properties["Enum Property"].CustomPropertyType, Is.EqualTo("Enum Type"));
+        Assert.That(tileMap.Properties["Enum Property"].Value, Is.EqualTo("Value 2"));
+
+        Assert.That(tileMap.Properties["Float Property"].Name, Is.EqualTo("Float Property"));
+        Assert.That(tileMap.Properties["Float Property"].Type, Is.EqualTo(PropertyType.Float));
+        Assert.That(tileMap.Properties["Float Property"].CustomPropertyType, Is.Empty);
+        Assert.That(tileMap.Properties["Float Property"].Value, Is.EqualTo("3.14"));
+        Assert.That(tileMap.Properties["Float Property"].FloatValue, Is.EqualTo(3.14));
+
+        Assert.That(tileMap.Properties["Int Property"].Name, Is.EqualTo("Int Property"));
+        Assert.That(tileMap.Properties["Int Property"].Type, Is.EqualTo(PropertyType.Int));
+        Assert.That(tileMap.Properties["Int Property"].CustomPropertyType, Is.Empty);
+        Assert.That(tileMap.Properties["Int Property"].Value, Is.EqualTo("69"));
+        Assert.That(tileMap.Properties["Int Property"].IntValue, Is.EqualTo(69));
+
+        Assert.That(tileMap.Properties["String Property"].Name, Is.EqualTo("String Property"));
+        Assert.That(tileMap.Properties["String Property"].Type, Is.EqualTo(PropertyType.String));
+        Assert.That(tileMap.Properties["String Property"].CustomPropertyType, Is.Empty);
+        Assert.That(tileMap.Properties["String Property"].Value, Is.EqualTo("This is a string property"));
+        Assert.That(tileMap.Properties["String Property"].StringValue, Is.EqualTo("This is a string property"));
+
+        Assert.That(tileMap.Properties["Object Property"].Name, Is.EqualTo("Object Property"));
+        Assert.That(tileMap.Properties["Object Property"].Type, Is.EqualTo(PropertyType.Object));
+        Assert.That(tileMap.Properties["Object Property"].CustomPropertyType, Is.Empty);
+        Assert.That(tileMap.Properties["Object Property"].Value, Is.EqualTo("28"));
+        Assert.That(tileMap.Properties["Object Property"].ObjectValue, Is.EqualTo(28));
+    }
+
+    [Test]
+    public void LoadFromFile_EmptyMap()
     {
         // Arrange
         var filePath = Path.Combine("Tiled", "TileMaps", "empty.tmx");
@@ -50,7 +109,7 @@ public class TileMapIntegrationTests
     }
 
     [Test]
-    public void LoadFromFile_Complex()
+    public void LoadFromFile_ComplexMap()
     {
         // Arrange
         var filePath = Path.Combine("Tiled", "TileMaps", "complex.tmx");
@@ -60,7 +119,7 @@ public class TileMapIntegrationTests
 
         // Assert
 
-        // Assert map
+        // Assert map properties
         Assert.That(tileMap.Version, Is.EqualTo("1.10"));
         Assert.That(tileMap.TiledVersion, Is.EqualTo("1.11.2"));
         Assert.That(tileMap.Orientation, Is.EqualTo(Orientation.Orthogonal));
@@ -71,41 +130,12 @@ public class TileMapIntegrationTests
         Assert.That(tileMap.TileHeight, Is.EqualTo(18));
         Assert.That(tileMap.IsInfinite, Is.False);
 
-        // Assert map properties
-        Assert.That(tileMap.Properties["Bool Property"].Name, Is.EqualTo("Bool Property"));
-        Assert.That(tileMap.Properties["Bool Property"].Type, Is.EqualTo(PropertyType.Bool));
-        Assert.That(tileMap.Properties["Bool Property"].CustomPropertyType, Is.Empty);
-        Assert.That(tileMap.Properties["Bool Property"].Value, Is.EqualTo("true"));
-        Assert.That(tileMap.Properties["Bool Property"].BoolValue, Is.True);
-
-        Assert.That(tileMap.Properties["Enum Property"].Name, Is.EqualTo("Enum Property"));
-        Assert.That(tileMap.Properties["Enum Property"].Type, Is.EqualTo(PropertyType.String));
-        Assert.That(tileMap.Properties["Enum Property"].CustomPropertyType, Is.EqualTo("Enum Type"));
-        Assert.That(tileMap.Properties["Enum Property"].Value, Is.EqualTo("Value 2"));
-
-        Assert.That(tileMap.Properties["Float Property"].Name, Is.EqualTo("Float Property"));
-        Assert.That(tileMap.Properties["Float Property"].Type, Is.EqualTo(PropertyType.Float));
-        Assert.That(tileMap.Properties["Float Property"].CustomPropertyType, Is.Empty);
-        Assert.That(tileMap.Properties["Float Property"].Value, Is.EqualTo("3.14"));
-        Assert.That(tileMap.Properties["Float Property"].FloatValue, Is.EqualTo(3.14));
-
-        Assert.That(tileMap.Properties["Int Property"].Name, Is.EqualTo("Int Property"));
-        Assert.That(tileMap.Properties["Int Property"].Type, Is.EqualTo(PropertyType.Int));
-        Assert.That(tileMap.Properties["Int Property"].CustomPropertyType, Is.Empty);
-        Assert.That(tileMap.Properties["Int Property"].Value, Is.EqualTo("69"));
-        Assert.That(tileMap.Properties["Int Property"].IntValue, Is.EqualTo(69));
-
+        // Assert map custom properties
         Assert.That(tileMap.Properties["String Property"].Name, Is.EqualTo("String Property"));
         Assert.That(tileMap.Properties["String Property"].Type, Is.EqualTo(PropertyType.String));
         Assert.That(tileMap.Properties["String Property"].CustomPropertyType, Is.Empty);
         Assert.That(tileMap.Properties["String Property"].Value, Is.EqualTo("This is a string property"));
         Assert.That(tileMap.Properties["String Property"].StringValue, Is.EqualTo("This is a string property"));
-
-        Assert.That(tileMap.Properties["Object Property"].Name, Is.EqualTo("Object Property"));
-        Assert.That(tileMap.Properties["Object Property"].Type, Is.EqualTo(PropertyType.Object));
-        Assert.That(tileMap.Properties["Object Property"].CustomPropertyType, Is.Empty);
-        Assert.That(tileMap.Properties["Object Property"].Value, Is.EqualTo("13"));
-        Assert.That(tileMap.Properties["Object Property"].ObjectValue, Is.EqualTo(13));
 
         // Assert tile sets
         Assert.That(tileMap.TileSets, Has.Count.EqualTo(2));
