ebb6159a183a92666721e9b1c5f5b819bf7389d3
Dawid Komorowski <dawidkomorowski@outlook.com>
2026-02-19 17:07:58 +0100

    Improve AssetId XML documentation
    Refine XML documentation for AssetId: clarify it is a type-safe identifier, add remarks that it is a lightweight wrapper around Guid and that the default value is Guid.Empty, change the param description to reference the underlying Guid, add a SeeAlso to Guid, and update CreateUnique docs to mention Guid.NewGuid and collision likelihood.

--- src/Geisha.Engine/Core/Assets/AssetId.cs
+++ src/Geisha.Engine/Core/Assets/AssetId.cs
@@ -3,15 +3,22 @@
 namespace Geisha.Engine.Core.Assets;
 
 /// <summary>
-///     Represents type safe identifier of an asset.
+///     Represents a type-safe identifier of an asset.
 /// </summary>
-/// <param name="Value">Actual value of identifier.</param>
+/// <remarks>
+///     <see cref="AssetId" /> is a lightweight wrapper around <see cref="Guid" />.
+///     The default value of <see cref="AssetId" /> has <see cref="Value" /> set to <see cref="Guid.Empty" />.
+/// </remarks>
+/// <param name="Value">Underlying <see cref="Guid" /> value of the identifier.</param>
+/// <seealso cref="Guid" />
 public readonly record struct AssetId(Guid Value)
 {
     /// <summary>
-    ///     Creates new, globally unique, instance of <see cref="AssetId" />.
+    ///     Creates new instance of <see cref="AssetId" /> with a unique value.
     /// </summary>
-    /// <returns>New, globally unique, instance of <see cref="AssetId" />.</returns>
-    /// <remarks>Uniqueness of created <see cref="AssetId" /> instances is based on uniqueness of <see cref="Guid" />.</remarks>
+    /// <returns>New instance of <see cref="AssetId" /> with a unique value.</returns>
+    /// <remarks>
+    ///     Uniqueness is based on <see cref="Guid.NewGuid" /> and collisions are extremely unlikely.
+    /// </remarks>
     public static AssetId CreateUnique() => new(Guid.NewGuid());
 }
