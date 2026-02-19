7c2b8bdb600e2f78776dd0604a9d533003662313
Dawid Komorowski <dawidkomorowski@outlook.com>
2026-02-19 21:55:40 +0100

    Make AssetInfo a record struct and update tests
    Convert AssetInfo to a readonly record struct with a file-scoped namespace, simplifying implementation by removing manual Equals/GetHashCode/ToString and relying on record semantics. Preserve constructor validation for null assetFilePath and keep AssetFilePath property backed by a nullable field. Update unit tests to match the new type shape: flatten test class, add [TestFixture], remove now-unnecessary equality and ToString-specific tests, and adjust using directives.

--- src/Geisha.Engine/Core/Assets/AssetInfo.cs
+++ src/Geisha.Engine/Core/Assets/AssetInfo.cs
@@ -1,81 +1,39 @@
 ﻿using System;
 
-namespace Geisha.Engine.Core.Assets
+namespace Geisha.Engine.Core.Assets;
+
+/// <summary>
+///     Asset registration info to be used in <see cref="IAssetStore" />.
+/// </summary>
+public readonly record struct AssetInfo
 {
+    private readonly string? _assetFilePath;
+
     /// <summary>
-    ///     Asset registration info to be used in <see cref="IAssetStore" />.
+    ///     Creates new instance of <see cref="AssetInfo" />.
     /// </summary>
-    public readonly struct AssetInfo : IEquatable<AssetInfo>
+    /// <param name="assetId">Id of asset.</param>
+    /// <param name="assetType">Type of asset.</param>
+    /// <param name="assetFilePath">Path to asset file.</param>
+    public AssetInfo(AssetId assetId, AssetType assetType, string assetFilePath)
     {
-        private readonly string? _assetFilePath;
-
-        /// <summary>
-        ///     Creates new instance of <see cref="AssetInfo" />.
-        /// </summary>
-        /// <param name="assetId">Id of asset.</param>
-        /// <param name="assetType">Type of asset.</param>
-        /// <param name="assetFilePath">Path to asset file.</param>
-        public AssetInfo(AssetId assetId, AssetType assetType, string assetFilePath)
-        {
-            AssetId = assetId;
-            AssetType = assetType;
-            _assetFilePath = assetFilePath ?? throw new ArgumentNullException(nameof(assetFilePath));
-        }
-
-        /// <summary>
-        ///     Id of asset.
-        /// </summary>
-        public AssetId AssetId { get; }
-
-        /// <summary>
-        ///     Type of asset.
-        /// </summary>
-        public AssetType AssetType { get; }
-
-        /// <summary>
-        ///     Path to asset file.
-        /// </summary>
-        public string AssetFilePath => _assetFilePath ?? string.Empty;
-
-        /// <summary>
-        ///     Converts the value of the current <see cref="AssetInfo" /> object to its equivalent string representation.
-        /// </summary>
-        /// <returns>A string representation of the value of the current <see cref="AssetInfo" /> object.</returns>
-        public override string ToString() => $"{nameof(AssetId)}: {AssetId}, {nameof(AssetType)}: {AssetType}, {nameof(AssetFilePath)}: {AssetFilePath}";
-
-        #region Equality members
-
-        /// <inheritdoc />
-        public bool Equals(AssetInfo other) => AssetId.Equals(other.AssetId) && AssetType.Equals(other.AssetType) && AssetFilePath == other.AssetFilePath;
-
-        /// <inheritdoc />
-        public override bool Equals(object? obj) => obj is AssetInfo other && Equals(other);
-
-        /// <inheritdoc />
-        public override int GetHashCode() => HashCode.Combine(AssetId, AssetType, AssetFilePath);
-
-        /// <summary>
-        ///     Determines whether two specified instances of <see cref="AssetInfo" /> are equal.
-        /// </summary>
-        /// <param name="left">The first object to compare.</param>
-        /// <param name="right">The second object to compare.</param>
-        /// <returns>
-        ///     <c>true</c> if <paramref name="left" /> and <paramref name="right" /> represent the same
-        ///     <see cref="AssetInfo" />; otherwise, <c>false</c>.
-        /// </returns>
-        public static bool operator ==(AssetInfo left, AssetInfo right) => left.Equals(right);
-
-        /// <summary>
-        ///     Determines whether two specified instances of <see cref="AssetInfo" /> are not equal.
-        /// </summary>
-        /// <param name="left">The first object to compare.</param>
-        /// <param name="right">The second object to compare.</param>
-        /// <returns>
-        ///     <c>true</c> if <paramref name="left" /> and <paramref name="right" /> do not represent the same
-        ///     <see cref="AssetInfo" />; otherwise, <c>false</c>.
-        /// </returns>
-        public static bool operator !=(AssetInfo left, AssetInfo right) => !left.Equals(right);
-
-        #endregion
+        AssetId = assetId;
+        AssetType = assetType;
+        _assetFilePath = assetFilePath ?? throw new ArgumentNullException(nameof(assetFilePath));
     }
+
+    /// <summary>
+    ///     Id of asset.
+    /// </summary>
+    public AssetId AssetId { get; }
+
+    /// <summary>
+    ///     Type of asset.
+    /// </summary>
+    public AssetType AssetType { get; }
+
+    /// <summary>
+    ///     Path to asset file.
+    /// </summary>
+    public string AssetFilePath => _assetFilePath ?? string.Empty;
 }
--- test/Geisha.Engine.UnitTests/Core/Assets/AssetInfoTests.cs
+++ test/Geisha.Engine.UnitTests/Core/Assets/AssetInfoTests.cs
@@ -1,98 +1,47 @@
-﻿using System;
-using Geisha.Engine.Core.Assets;
-using Geisha.TestUtils;
+﻿using Geisha.Engine.Core.Assets;
 using NUnit.Framework;
 
-namespace Geisha.Engine.UnitTests.Core.Assets
+namespace Geisha.Engine.UnitTests.Core.Assets;
+
+[TestFixture]
+public class AssetInfoTests
 {
-    [TestFixture]
-    public class AssetInfoTests
+    [Test]
+    public void Constructor_ShouldCreateDefaultAssetInfo_GivenNoParameters()
     {
-        [Test]
-        public void Constructor_ShouldCreateDefaultAssetInfo_GivenNoParameters()
-        {
-            // Arrange
-            // Act
-            var actual = new AssetInfo();
+        // Arrange
+        // Act
+        var actual = new AssetInfo();
 
-            // Assert
-            Assert.That(actual.AssetId, Is.EqualTo(default(AssetId)));
-            Assert.That(actual.AssetType, Is.EqualTo(default(AssetType)));
-            Assert.That(actual.AssetFilePath, Is.Empty);
-        }
+        // Assert
+        Assert.That(actual.AssetId, Is.EqualTo(default(AssetId)));
+        Assert.That(actual.AssetType, Is.EqualTo(default(AssetType)));
+        Assert.That(actual.AssetFilePath, Is.Empty);
+    }
 
-        [Test]
-        public void Constructor_ShouldThrowArgumentNullException_GivenNullAssetFilePath()
-        {
-            // Arrange
-            // Act
-            // Assert
-            Assert.That(() => new AssetInfo(default, default, null!), Throws.ArgumentNullException);
-        }
+    [Test]
+    public void Constructor_ShouldThrowArgumentNullException_GivenNullAssetFilePath()
+    {
+        // Arrange
+        // Act
+        // Assert
+        Assert.That(() => new AssetInfo(default, default, null!), Throws.ArgumentNullException);
+    }
 
-        [Test]
-        public void Constructor_ShouldCreateAssetInfo_GivenAllParameters()
-        {
-            // Arrange
-            var assetId = AssetId.CreateUnique();
-            var assetType = new AssetType("Asset Type");
-            const string assetFilePath = "some file path";
+    [Test]
+    public void Constructor_ShouldCreateAssetInfo_GivenAllParameters()
+    {
+        // Arrange
+        var assetId = AssetId.CreateUnique();
+        var assetType = new AssetType("Asset Type");
+        const string assetFilePath = "some file path";
 
-            // Act
-            var actual = new AssetInfo(assetId, assetType, assetFilePath);
+        // Act
+        var actual = new AssetInfo(assetId, assetType, assetFilePath);
 
-            // Assert
-            Assert.That(actual.AssetId, Is.EqualTo(assetId));
-            Assert.That(actual.AssetType, Is.EqualTo(assetType));
-            Assert.That(actual.AssetFilePath, Is.EqualTo(assetFilePath));
-        }
-
-        [Test]
-        public void ToString_ShouldReturn_AssetId_AssetType_AssetFilePath()
-        {
-            // Arrange
-            var assetId = AssetId.Parse("E0598EF9-A13E-4915-8DE3-AD5FB7036EF5");
-            var assetType = new AssetType("Asset Type");
-            const string assetFilePath = "some file path";
-
-            var assetInfo = new AssetInfo(assetId, assetType, assetFilePath);
-
-            // Act
-            var actual = assetInfo.ToString();
-
-            // Assert
-            Assert.That(actual,
-                Is.EqualTo("AssetId: AssetId { Value = e0598ef9-a13e-4915-8de3-ad5fb7036ef5 }, AssetType: Asset Type, AssetFilePath: some file path"));
-        }
-
-        [TestCase("345E30DC-5F18-472C-B539-15ECE44B6B60", "Asset Type 1", "some file path",
-            "345E30DC-5F18-472C-B539-15ECE44B6B60", "Asset Type 1", "some file path", true)]
-        [TestCase("345E30DC-5F18-472C-B539-15ECE44B6B60", "Asset Type 1", "some file path",
-            "345E30DC-5F18-472C-B539-15ECE44B6B60", "Asset Type 1", "other file path", false)]
-        [TestCase("345E30DC-5F18-472C-B539-15ECE44B6B60", "Asset Type 1", "some file path",
-            "345E30DC-5F18-472C-B539-15ECE44B6B60", "Asset Type 2", "some file path", false)]
-        [TestCase("345E30DC-5F18-472C-B539-15ECE44B6B60", "Asset Type 1", "some file path",
-            "C7CF6FFC-FF65-48D8-BF1B-041E51F8E1C4", "Asset Type 1", "some file path", false)]
-        public void EqualityMembers_ShouldEqualAssetInfo_When_AssetId_And_AssetType_And_AssetFilePath_IsEqual(string assetIdString1, string assetTypeString1,
-            string assetFilePath1, string assetIdString2, string assetTypeString2, string assetFilePath2, bool expectedIsEqual)
-        {
-            // Arrange
-            var assetId1 = AssetId.Parse(assetIdString1);
-            var assetId2 = AssetId.Parse(assetIdString2);
-
-            var assetType1 = new AssetType(assetTypeString1);
-            var assetType2 = new AssetType(assetTypeString2);
-
-            var assetInfo1 = new AssetInfo(assetId1, assetType1, assetFilePath1);
-            var assetInfo2 = new AssetInfo(assetId2, assetType2, assetFilePath2);
-
-            // Act
-            // Assert
-            AssertEqualityMembers
-                .ForValues(assetInfo1, assetInfo2)
-                .UsingEqualityOperator((x, y) => x == y)
-                .UsingInequalityOperator((x, y) => x != y)
-                .EqualityIsExpectedToBe(expectedIsEqual);
-        }
+        // Assert
+        Assert.That(actual.AssetId, Is.EqualTo(assetId));
+        Assert.That(actual.AssetType, Is.EqualTo(assetType));
+        Assert.That(actual.AssetFilePath, Is.EqualTo(assetFilePath));
     }
 }
