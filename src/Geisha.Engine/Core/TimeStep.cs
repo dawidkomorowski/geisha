5386e701df12a26f576f1f2e692ec7e423897d2f
Dawid Komorowski <dawidkomorowski@outlook.com>
2026-04-02 19:26:11 +0200

    Add TimeStep and TimeSystem.FixedDeltaTime
    Introduce TimeStep record struct to represent scaled and unscaled delta times (TimeSpan) along with time scale and convenient Seconds properties. Add TimeSystem static class exposing a global FixedDeltaTime (internal setter) used by TimeStep.FixedDeltaTime and FixedDeltaTimeSeconds. Enables consistent handling of frame and fixed-step time values across the engine.

---
+++ src/Geisha.Engine/Core/TimeStep.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Geisha.Engine.Core;
+
+public readonly record struct TimeStep
+{
+    public TimeStep(TimeSpan unscaledDeltaTime, double timeScale)
+    {
+        DeltaTime = unscaledDeltaTime * timeScale;
+        UnscaledDeltaTime = unscaledDeltaTime;
+        TimeScale = timeScale;
+    }
+
+    public TimeSpan DeltaTime { get; init; }
+    public TimeSpan UnscaledDeltaTime { get; init; }
+    public double TimeScale { get; init; }
+
+    public double DeltaTimeSeconds => DeltaTime.TotalSeconds;
+    public double UnscaledDeltaTimeSeconds => UnscaledDeltaTime.TotalSeconds;
+
+    public static TimeSpan FixedDeltaTime => TimeSystem.FixedDeltaTime;
+    public static double FixedDeltaTimeSeconds => FixedDeltaTime.TotalSeconds;
+}
---
+++ src/Geisha.Engine/Core/TimeSystem.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Geisha.Engine.Core;
+
+public static class TimeSystem
+{
+    public static TimeSpan FixedDeltaTime { get; internal set; }
+}
