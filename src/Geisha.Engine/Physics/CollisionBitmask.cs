04107c456078fb0e3e290ae053d38f0ca5f003fc
Dawid Komorowski <dawidkomorowski@outlook.com>
2026-05-13 22:19:35 +0200

    Introduce CollisionBitmask record and tests
    Change CollisionBitmask from a readonly struct to a record struct and add construction helpers and predefined masks. Added a constructor, FromUInt, FromBits (builds mask from bit indices), and static None/All properties. A HasBit method is present but not yet implemented. Added unit tests (CollisionBitmaskTests) covering FromUInt and several FromBits cases. Note: FromBits uses params int[] and includes a TODO about potential allocation and a future ReadOnlySpan-based API when evolving to newer C#/.NET.

--- src/Geisha.Engine/Physics/CollisionBitmask.cs
+++ src/Geisha.Engine/Physics/CollisionBitmask.cs
@@ -1,6 +1,32 @@
 ﻿namespace Geisha.Engine.Physics;
 
-public readonly struct CollisionBitmask
+public readonly record struct CollisionBitmask
 {
+    public CollisionBitmask(uint value)
+    {
+        Value = value;
+    }
+
     public uint Value { get; init; }
+
+    public bool HasBit(int bit) => false;
+
+    public static CollisionBitmask None { get; } = new(uint.MinValue);
+    public static CollisionBitmask All { get; } = new(uint.MaxValue);
+
+    public static CollisionBitmask FromUInt(uint value) => new(value);
+
+    // TODO: This implementation may force the caller to allocate.
+    //       When migrated to .NET 9 (C# 13) params collection feature could be used to accept ReadOnlySpan.
+    public static CollisionBitmask FromBits(params int[] bits)
+    {
+        uint value = 0;
+
+        foreach (var bit in bits)
+        {
+            value |= 1u << bit;
+        }
+
+        return new CollisionBitmask(value);
+    }
 }
---
+++ test/Geisha.Engine.UnitTests/Physics/CollisionBitmaskTests.cs
@@ -0,0 +1,37 @@
+﻿using Geisha.Engine.Physics;
+using NUnit.Framework;
+
+namespace Geisha.Engine.UnitTests.Physics;
+
+[TestFixture]
+public class CollisionBitmaskTests
+{
+    [Test]
+    public void FromValue_Test()
+    {
+        // Arrange
+        const uint value = 123;
+
+        // Act
+        var actual = CollisionBitmask.FromUInt(value);
+
+        // Assert
+        Assert.That(actual.Value, Is.EqualTo(value));
+    }
+
+    // TODO: Add more test cases.
+    [TestCase(new int[0], 0b_00000000_00000000_00000000_00000000u)]
+    [TestCase(new[] { 0 }, 0b_00000000_00000000_00000000_00000001u)]
+    [TestCase(new[] { 1 }, 0b_00000000_00000000_00000000_00000010u)]
+    [TestCase(new[] { 30 }, 0b_01000000_00000000_00000000_00000000u)]
+    [TestCase(new[] { 31 }, 0b_10000000_00000000_00000000_00000000u)]
+    public void FromBits_Test(int[] bits, uint value)
+    {
+        // Arrange
+        // Act
+        var actual = CollisionBitmask.FromBits(bits);
+
+        // Assert
+        Assert.That(actual.Value, Is.EqualTo(value));
+    }
+}
