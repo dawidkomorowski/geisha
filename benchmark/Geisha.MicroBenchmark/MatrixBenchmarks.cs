14ac97ca950a234859094615fadf9233228974c5
Dawid Komorowski <dawidkomorowski@outlook.com>
2026-05-23 15:16:18 +0200

    Add MatrixBenchmarks and update Program
    Introduce MatrixBenchmarks (benchmark/Geisha.MicroBenchmark/MatrixBenchmarks.cs) using BenchmarkDotNet with MemoryDiagnoser and DisassemblyDiagnoser to measure Matrix3x3.CreateRotation performance over a precomputed angles array. The legacy benchmark is left commented for reference. Update Program.cs to run MatrixBenchmarks instead of PhysicsSystemBenchmarks.

---
+++ benchmark/Geisha.MicroBenchmark/MatrixBenchmarks.cs
@@ -0,0 +1,43 @@
+using BenchmarkDotNet.Attributes;
+using Geisha.Engine.Core.Math;
+
+namespace Geisha.MicroBenchmark;
+
+[MemoryDiagnoser]
+[DisassemblyDiagnoser]
+public class MatrixBenchmarks
+{
+    private readonly double[] _angles = new double[10000];
+
+    public MatrixBenchmarks()
+    {
+        for (int i = 0; i < _angles.Length; i++)
+        {
+            _angles[i] = i * 0.001d;
+        }
+    }
+
+    //[Benchmark(Baseline = true)]
+    //public Matrix3x3 Matrix3x3_CreateRotation_Legacy()
+    //{
+    //    var r = Matrix3x3.Identity;
+    //    for (int i = 0; i < _angles.Length; i++)
+    //    {
+    //        r = Matrix3x3.CreateRotation_Legacy(_angles[i]);
+    //    }
+
+    //    return r;
+    //}
+
+    [Benchmark]
+    public Matrix3x3 Matrix3x3_CreateRotation()
+    {
+        var r = Matrix3x3.Identity;
+        for (int i = 0; i < _angles.Length; i++)
+        {
+            r = Matrix3x3.CreateRotation(_angles[i]);
+        }
+
+        return r;
+    }
+}
