eaa075f4d1f9506af31f429793f8e8f21844bca9
Dawid Komorowski <dawidkomorowski@outlook.com>
2026-06-18 00:50:08 +0200

    Add IUnmanaged interface and Test struct
    Introduce IUnmanaged<T> marker interface constrained to unmanaged types (placeholder for future unmanaged memory handling) and add an internal Test struct in PhysicsEngine2D that implements it with a single Value property. IUnmanaged has a TODO for documentation.

---
+++ src/Geisha.Engine/Core/Memory/IUnmanaged.cs
@@ -0,0 +1,6 @@
+﻿namespace Geisha.Engine.Core.Memory;
+
+// TODO: Add documentation.
+public interface IUnmanaged<T> where T : unmanaged
+{
+}
---
+++ src/Geisha.Engine/Physics/PhysicsEngine2D/Internal/Test.cs
@@ -0,0 +1,8 @@
+﻿using Geisha.Engine.Core.Memory;
+
+namespace Geisha.Engine.Physics.PhysicsEngine2D.Internal;
+
+internal struct Test : IUnmanaged<Test>
+{
+    public int Value { get; set; }
+}
