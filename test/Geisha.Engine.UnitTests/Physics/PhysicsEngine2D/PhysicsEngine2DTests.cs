e287fadadb09c30dacd8cc7fd00c6bb809ae5199
Dawid Komorowski <dawidkomorowski@outlook.com>
2026-06-29 21:47:12 +0200

    Update PhysicsEngine2DTests.cs

--- test/Geisha.Engine.UnitTests/Physics/PhysicsEngine2D/PhysicsEngine2DTests.cs
+++ test/Geisha.Engine.UnitTests/Physics/PhysicsEngine2D/PhysicsEngine2DTests.cs
@@ -4,9 +4,10 @@ using NUnit.Framework;
 
 namespace Geisha.Engine.UnitTests.Physics.PhysicsEngine2D;
 
-// These tests cover a minimal set of internal parts of the low level physics engine (PhysicsEngine2D). The main
-// tests for the physics functionality are provided by the physics system tests. Physics system is a higher level
-// component of the whole game engine that integrates the low level physics engine with the rest of the game engine.
+// This test class covers a minimal set of internal physics engine behaviors that are not easily observable
+// through higher-level APIs. The main test coverage for physics is provided by physics system tests, where
+// the physics system is a higher-level component of the game engine that integrates the low-level physics
+// engine with the rest of the game engine.
 [TestFixture]
 public class PhysicsEngine2DTests
 {
