769db9c76026a0c270215db920f882634b36a198
Dawid Komorowski <dawidkomorowski@outlook.com>
2026-07-01 21:33:59 +0200

    Update AABB2D.cs

--- src/Geisha.Engine/Core/Math/AABB2D.cs
+++ src/Geisha.Engine/Core/Math/AABB2D.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 namespace Geisha.Engine.Core.Math;
 
@@ -10,6 +11,9 @@ public readonly record struct AABB2D
 {
     public AABB2D(in Vector2 min, in Vector2 max)
     {
+        Debug.Assert(min.X <= max.X, "Min.X must be less than or equal to Max.X.");
+        Debug.Assert(min.Y <= max.Y, "Min.Y must be less than or equal to Max.Y.");
+
         Min = min;
         Max = max;
     }
@@ -27,6 +31,7 @@ public readonly record struct AABB2D
     public double Height => Max.Y - Min.Y;
 
     public bool Contains(in Vector2 point) => Min.X <= point.X && point.X <= Max.X && Min.Y <= point.Y && point.Y <= Max.Y;
+    public bool Contains(in AABB2D other) => false;
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public bool Overlaps(in AABB2D other) => Max.X >= other.Min.X && Min.X <= other.Max.X && Max.Y >= other.Min.Y && Min.Y <= other.Max.Y;
