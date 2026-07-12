4aedc33e00b9ec52845e86149b58c47bf2f77d82
Dawid Komorowski <dawidkomorowski@outlook.com>
2026-07-12 11:58:55 +0200

    Add cell size to SpatialGrid scaffold
    SpatialGrid now accepts a SizeD in its constructor and exposes it via a read-only CellSize property. Added a unit test to verify the constructor stores and returns the provided cell size.

--- src/Geisha.Engine/Core/Spatial/SpatialGrid.cs
+++ src/Geisha.Engine/Core/Spatial/SpatialGrid.cs
@@ -1,6 +1,14 @@
-﻿namespace Geisha.Engine.Core.Spatial;
+﻿using Geisha.Engine.Core.Math;
+
+namespace Geisha.Engine.Core.Spatial;
 
 // TODO: Scaffolding for spatial grid prototyping.
 public sealed class SpatialGrid
 {
+    public SpatialGrid(SizeD cellSize)
+    {
+        CellSize = cellSize;
+    }
+
+    public SizeD CellSize { get; }
 }
---
+++ test/Geisha.Engine.UnitTests/Core/Spatial/SpatialGridTests.cs
@@ -0,0 +1,22 @@
+﻿using Geisha.Engine.Core.Math;
+using Geisha.Engine.Core.Spatial;
+using NUnit.Framework;
+
+namespace Geisha.Engine.UnitTests.Core.Spatial;
+
+[TestFixture]
+internal class SpatialGridTests
+{
+    [Test]
+    public void Constructor_ShouldCreateGridWithSpecifiedSize()
+    {
+        // Arrange
+        var size = new SizeD(20, 10);
+
+        // Act
+        var grid = new SpatialGrid(size);
+
+        // Assert
+        Assert.That(grid.CellSize, Is.EqualTo(size));
+    }
+}
