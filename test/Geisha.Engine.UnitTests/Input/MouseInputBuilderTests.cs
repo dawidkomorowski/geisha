1fd1812a72717f38c700285b4ca17349985ff187
Dawid Komorowski <dawidkomorowski@outlook.com>
2026-08-30 14:06:25 +0200

    #746 Refactor MouseInputBuilderTests to use deterministic input values (#753)

--- test/Geisha.Engine.UnitTests/Input/MouseInputBuilderTests.cs
+++ test/Geisha.Engine.UnitTests/Input/MouseInputBuilderTests.cs
@@ -1,40 +1,40 @@
-﻿using Geisha.Engine.Input;
-using Geisha.TestUtils;
+﻿using Geisha.Engine.Core.Math;
+using Geisha.Engine.Input;
 using NUnit.Framework;
 
-namespace Geisha.Engine.UnitTests.Input
+namespace Geisha.Engine.UnitTests.Input;
+
+[TestFixture]
+public class MouseInputBuilderTests
 {
-    [TestFixture]
-    public class MouseInputBuilderTests
+    [TestCase(true, false, true, false, true)]
+    [TestCase(false, true, false, true, false)]
+    public void Build_CreatesMouseInputWithStateSetAsSpecified(bool leftButton, bool middleButton, bool rightButton, bool xButton1, bool xButton2)
     {
-        [Test]
-        public void Build_CreatesMouseInputWithStateSetAsSpecified()
+        // Arrange
+        var builder = new MouseInputBuilder
         {
-            // Arrange
-            var builder = new MouseInputBuilder
-            {
-                Position = Utils.RandomVector2(),
-                PositionDelta = Utils.RandomVector2(),
-                LeftButton = Utils.Random.NextBool(),
-                MiddleButton = Utils.Random.NextBool(),
-                RightButton = Utils.Random.NextBool(),
-                XButton1 = Utils.Random.NextBool(),
-                XButton2 = Utils.Random.NextBool(),
-                ScrollDelta = Utils.Random.Next()
-            };
+            Position = new Vector2(12, 34),
+            PositionDelta = new Vector2(5, 6),
+            LeftButton = leftButton,
+            MiddleButton = middleButton,
+            RightButton = rightButton,
+            XButton1 = xButton1,
+            XButton2 = xButton2,
+            ScrollDelta = 120
+        };
 
-            // Act
-            var mouseInput = builder.Build();
+        // Act
+        var mouseInput = builder.Build();
 
-            // Assert
-            Assert.That(mouseInput.Position, Is.EqualTo(builder.Position));
-            Assert.That(mouseInput.PositionDelta, Is.EqualTo(builder.PositionDelta));
-            Assert.That(mouseInput.LeftButton, Is.EqualTo(builder.LeftButton));
-            Assert.That(mouseInput.MiddleButton, Is.EqualTo(builder.MiddleButton));
-            Assert.That(mouseInput.RightButton, Is.EqualTo(builder.RightButton));
-            Assert.That(mouseInput.XButton1, Is.EqualTo(builder.XButton1));
-            Assert.That(mouseInput.XButton2, Is.EqualTo(builder.XButton2));
-            Assert.That(mouseInput.ScrollDelta, Is.EqualTo(builder.ScrollDelta));
-        }
+        // Assert
+        Assert.That(mouseInput.Position, Is.EqualTo(builder.Position));
+        Assert.That(mouseInput.PositionDelta, Is.EqualTo(builder.PositionDelta));
+        Assert.That(mouseInput.LeftButton, Is.EqualTo(builder.LeftButton));
+        Assert.That(mouseInput.MiddleButton, Is.EqualTo(builder.MiddleButton));
+        Assert.That(mouseInput.RightButton, Is.EqualTo(builder.RightButton));
+        Assert.That(mouseInput.XButton1, Is.EqualTo(builder.XButton1));
+        Assert.That(mouseInput.XButton2, Is.EqualTo(builder.XButton2));
+        Assert.That(mouseInput.ScrollDelta, Is.EqualTo(builder.ScrollDelta));
     }
 }
