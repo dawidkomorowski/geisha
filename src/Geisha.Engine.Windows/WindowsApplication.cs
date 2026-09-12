2a9b0cb208c81b043a923e9eca0cee5a6080cba3
Dawid Komorowski <dawidkomorowski@outlook.com>
2026-09-13 00:55:09 +0200

    Add resize-related rendering TODOs
    Add TODO notes around rendering surface sizing. One marks the need to detect window client-size changes and resize rendering resources in the Windows render loop, and another flags outdated `IRenderingContext2D` size documentation now that fullscreen behavior differs.

--- src/Geisha.Engine.Windows/WindowsApplication.cs
+++ src/Geisha.Engine.Windows/WindowsApplication.cs
@@ -80,6 +80,9 @@ namespace Geisha.Engine.Windows
 
                 RenderLoop.Run(form, () =>
                 {
+                    // TODO: Track the client size and if changed resize rendering resources.
+                    var clientSize = form.ClientSize;
+
                     // ReSharper disable AccessToDisposedClosure
                     engine.Update();
 
